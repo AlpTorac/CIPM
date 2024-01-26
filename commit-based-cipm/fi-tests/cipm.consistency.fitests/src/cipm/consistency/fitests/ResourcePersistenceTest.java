967ad833e7e92542aaa797f3e6379266d4dcba2e
AlpTorac <a.torac@hotmail.com>
2024-01-26 11:26:36 +0100

    Add Java-Model copying to unit test

--- commit-based-cipm/fi-tests/cipm.consistency.fitests/src/cipm/consistency/fitests/ResourcePersistenceTest.java
+++ commit-based-cipm/fi-tests/cipm.consistency.fitests/src/cipm/consistency/fitests/ResourcePersistenceTest.java
@@ -4,8 +4,9 @@ import cipm.consistency.commitintegration.diff.util.JavaModelComparator;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
-import java.util.stream.Stream;
 
 import org.apache.log4j.ConsoleAppender;
 import org.apache.log4j.Level;
@@ -17,13 +18,25 @@ import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 public class ResourcePersistenceTest {
 	private static final Logger LOGGER = Logger.getLogger("cipm." + ResourcePersistenceTest.class.getSimpleName());
 	
+	private static final File TARGET_DIR = new File("target").getAbsoluteFile();
+	
+	private static final String WORKING_RESOURCES_PATH = TARGET_DIR.getParentFile().getAbsolutePath() + File.separator + "tmp";
+	private static final String WORKING_TEAMMATES_PATH = WORKING_RESOURCES_PATH + File.separator + "TeammatesTest";
+	
 	@BeforeEach
-	public void setUp() {
+	public void setUp() throws IOException {
+		this.setUpLogger();
+//		this.cleanModels(WORKING_RESOURCES_PATH);
+		this.copyModels(TARGET_DIR, WORKING_RESOURCES_PATH);
+	}
+	
+	protected void setUpLogger() {
 		Logger logger = Logger.getLogger("cipm");
 		logger.setLevel(Level.ALL);
 		logger = Logger.getLogger("jamopp");
@@ -35,11 +48,80 @@ public class ResourcePersistenceTest {
 		logger.addAppender(ap);
 	}
 	
+	/**
+	 * Recursively cleans models, which have been used in tests.
+	 * 
+	 * @param path The path to the directory of the said models
+	 */
+	protected void cleanModels(String path) throws IOException {
+		var file = new File(path);
+		
+		if (file.exists()) {
+			if (file.isFile()) {
+				file.delete();
+				return;
+			}
+			
+			if (file.isDirectory()) {
+				var children = file.listFiles();
+				
+				if (children != null) {
+					for (File cf : children) {
+						this.cleanModels(cf.getAbsolutePath());
+					}
+				}
+				
+				file.delete();
+			}
+		}
+	}
+	
+	/**
+	 * Recursively copies Java-Model files from the given parent parameter to
+	 * the path given via copyPath. Replaces model files, which already exist.
+	 * 
+	 * This spares having to generate a separate model after each
+	 * test case, which modifies them.
+	 * 
+	 * @param parent The directory, under which the original Java-Models can be found
+	 * @param copyPath The path to the directory, where the content from parent will be copied to
+	 */
+	protected void copyModels(File parent, String copyPath) throws IOException {
+		LOGGER.debug("Copying the contents of " + parent.getAbsolutePath() + " into " + copyPath);
+		for (File f : parent.listFiles()) {
+			var fileName = f.getName();
+			
+			if (f.isDirectory()) {
+				LOGGER.debug("Directory found: " + fileName);
+				String newCopyAddress = copyPath + File.separator + fileName;
+				LOGGER.debug("Copy address changed to " + newCopyAddress);
+				File tmpDir = new File(newCopyAddress);
+				
+				this.copyModels(f, tmpDir.getAbsolutePath());
+			}
+			if (f.isFile()) {
+				LOGGER.debug("File found: " + fileName);
+				File tmpFile = new File(copyPath + File.separator + fileName);
+				
+				if (tmpFile.exists()) {
+					LOGGER.debug("Existing file will be replaced");
+				} else {
+					LOGGER.debug("Creating file");
+					tmpFile.mkdirs();
+					LOGGER.debug("Created file");	
+				}
+				
+				LOGGER.debug("Copying original file into new file");
+				Files.copy(f.toPath(), tmpFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+				LOGGER.debug("Copied original file into new file: " + fileName);
+			}
+		}
+		LOGGER.debug("Parent directory " + parent.getName() + " has been copied");
+	}
+	
 	@Test
 	public void modelLoadingTest() throws IOException {
-		var currentPath = new File("target").getAbsoluteFile();
-		var teammatesPath = currentPath + File.separator + "TeammatesTest";
-		var res1Path = teammatesPath + File.separator + "JavaModel-test0.javaxmi";
+		var res1Path = WORKING_TEAMMATES_PATH + File.separator + "JavaModel-test0.javaxmi";
 		
 		var resURI = URI.createFileURI(res1Path);
 		var resSet = new ResourceSetImpl();
@@ -51,12 +133,11 @@ public class ResourcePersistenceTest {
 		res.unload();
 	}
 	
+	@Disabled("Takes a long time to complete")
 	@Test
 	public void modelComparingTest() throws IOException {
-		var currentPath = new File("target").getAbsoluteFile();
-		var teammatesPath = currentPath + File.separator + "TeammatesTest";
-		var res1Path = teammatesPath + File.separator + "JavaModel-test0.javaxmi";
-		var res2Path = teammatesPath + File.separator + "JavaModel-test1.javaxmi";
+		var res1Path = WORKING_TEAMMATES_PATH + File.separator + "JavaModel-test0.javaxmi";
+		var res2Path = WORKING_TEAMMATES_PATH + File.separator + "JavaModel-test1.javaxmi";
 		
 		var res1URI = URI.createFileURI(res1Path);
 		var res1Set = new ResourceSetImpl();
@@ -153,9 +234,5 @@ public class ResourcePersistenceTest {
 					
 					return result;
 				})));
-	}
-	
-	protected String getTargetPath() {
-		return "target";
 	}
 }
