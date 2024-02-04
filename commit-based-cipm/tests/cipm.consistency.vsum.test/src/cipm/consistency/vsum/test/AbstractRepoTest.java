e1e1dcb741179d445b344a64a6471dfd3d53c8d9
AlpTorac <a.torac@hotmail.com>
2024-02-04 18:18:32 +0100

    Remove AbstractRepoSettings, add commentary

--- commit-based-cipm/tests/cipm.consistency.vsum.test/src/cipm/consistency/vsum/test/AbstractRepoTest.java
+++ commit-based-cipm/tests/cipm.consistency.vsum.test/src/cipm/consistency/vsum/test/AbstractRepoTest.java
@@ -8,8 +8,8 @@ import cipm.consistency.commitintegration.JavaParserAndPropagatorUtils;
 import tools.vitruv.framework.propagation.ChangePropagationSpecification;
 
 public abstract class AbstractRepoTest extends AbstractCITest implements HasRepoSettings {
-	protected AbstractRepoSettings repoSettings;
-	
+	protected HasRepoSettings repoSettings;
+
 	@BeforeEach
 	@Override
 	public void setUp() throws Exception {
@@ -17,22 +17,22 @@ public abstract class AbstractRepoTest extends AbstractCITest implements HasRepo
 		this.setJavaParserAndPropagatorUtilsConfig();
 		super.setUp();
 	}
-	
+
 	@Override
 	protected ChangePropagationSpecification getJavaPCMSpecification() {
 		return this.getJavaPCMSpec();
 	}
-	
+
 	@Override
-	public AbstractRepoSettings getRepoSettings() {
+	public HasRepoSettings getRepoSettings() {
 		return this.repoSettings;
 	}
-	
+
 	@Override
 	protected String getTestPath() {
 		return "target" + File.separator + this.getTestType();
 	}
-	
+
 	@Override
 	protected String getRepositoryPath() {
 		return this.getRepositoryAddress();
@@ -42,26 +42,34 @@ public abstract class AbstractRepoTest extends AbstractCITest implements HasRepo
 	protected String getSettingsPath() {
 		return this.getSettingsAddress();
 	}
-	
+
 	@Override
 	protected String getTestType() {
 		return this.getTestGroup();
 	}
-	
+
+	/**
+	 * Sets {@link JavaParserAndPropagatorUtils} configuration, if necessary.
+	 */
 	protected void setJavaParserAndPropagatorUtilsConfig() {
 		var compDetectionStrategy = this.getComponentDetectionStrategy();
-		
+
 		if (compDetectionStrategy != null) {
 			JavaParserAndPropagatorUtils.setConfiguration(new JavaParserAndPropagatorUtils.Configuration(
-					this.getJavaParserAndPropagatorUtilsResolveAll(),
-					compDetectionStrategy));
+					this.getJavaParserAndPropagatorUtilsResolveAll(), compDetectionStrategy));
 		}
 	}
-	
+
+	/**
+	 * @return resolveAll parameter of
+	 *         {@link JavaParserAndPropagatorUtils.Configuration#Configuration(boolean, cipm.consistency.commitintegration.detection.ComponentDetectionStrategy...)}
+	 */
 	protected boolean getJavaParserAndPropagatorUtilsResolveAll() {
 		return false;
 	}
-	
-	protected abstract AbstractRepoSettings initRepoSettings();
-	
+
+	/**
+	 * @return The {@link HasRepoSettings} instance to be used during unit tests
+	 */
+	protected abstract HasRepoSettings initRepoSettings();
 }
