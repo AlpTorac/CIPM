37ac530da85c7710a8082a6eeea4d5cc42d76d54
AlpTorac <a.torac@hotmail.com>
2024-02-04 18:23:07 +0100

    Add commentary, format files

--- commit-based-cipm/tests/cipm.consistency.vsum.test/src/cipm/consistency/vsum/test/AbstractFITestResourceGenerator.java
+++ commit-based-cipm/tests/cipm.consistency.vsum.test/src/cipm/consistency/vsum/test/AbstractFITestResourceGenerator.java
@@ -4,16 +4,17 @@ public abstract class AbstractFITestResourceGenerator extends AbstractRepoTest {
 	/**
 	 * The object responsible for generating Java-Models.
 	 * 
-	 * Requires to be instantiated 
+	 * Has to be instantiated via
+	 * {@link FITestResourceGenerator#init(String, String)}
 	 */
 	private FITestResourceGenerator resGen = null;
-	
+
 	@Override
 	public void tearDown() throws Exception {
 		super.tearDown();
 		this.resGen.cleanUp();
 	}
-	
+
 	/**
 	 * Instantiates a {@link FITestResourceGenerator} but does not initialise it.
 	 * Make sure to call {@link #initResGen(Object)} at the start of each test to
@@ -22,27 +23,26 @@ public abstract class AbstractFITestResourceGenerator extends AbstractRepoTest {
 	protected FITestResourceGenerator createResGen() {
 		return new FITestResourceGenerator();
 	}
-	
+
 	/**
-	 * Computes a unique suffix for the model to be generated based on
-	 * the given parameter, so that generating multiple models is allowed.
+	 * Computes a unique suffix for the model to be generated based on the given
+	 * parameter, so that generating multiple models is allowed.
 	 * 
 	 * @param commitHashIdentifier see {@link HasRepoSettings#getCommitHash(Object)}
 	 * 
 	 * @return The suffix of the file name (excluding extension)
 	 */
 	protected abstract String getTestModelFileIdentifier(Object commitHashIdentifier);
-	
+
 	/**
 	 * @param commitHashIdentifier see {@link HasRepoSettings#getCommitHash(Object)}
 	 * 
 	 * @return The full file name (excluding extension)
 	 */
 	protected String getTestModelFileNameWOExtension(Object commitHashIdentifier) {
-		return this.getTestModelFileNamePrefix() +
-				this.getTestModelFileIdentifier(commitHashIdentifier);
+		return this.getTestModelFileNamePrefix() + this.getTestModelFileIdentifier(commitHashIdentifier);
 	}
-	
+
 	/**
 	 * Initialises {@link #resGen}
 	 * 
@@ -52,19 +52,26 @@ public abstract class AbstractFITestResourceGenerator extends AbstractRepoTest {
 		if (this.resGen != null) {
 			this.resGen.cleanUp();
 		}
-		
+
 		this.resGen = this.createResGen();
-		this.resGen.init(this.getTestType(), ".javaxmi");
+		this.resGen.init(this.getTestType(), this.getTestModelFileExtension());
 		this.resGen.addModelGenerationHook(this.getTestModelFileNameWOExtension(commitHashIdentifier));
 	}
-	
+
 	/**
-	 * @return The common prefix of the name of the model file to be generated.
+	 * @return The extension of the model file to be generated
+	 */
+	protected String getTestModelFileExtension() {
+		return ".javaxmi";
+	}
+
+	/**
+	 * @return The common prefix of the name of the model file to be generated
 	 */
 	protected String getTestModelFileNamePrefix() {
 		return "JavaModel-test";
 	}
-	
+
 	/**
 	 * Attempts to generate a model for the commit hash with the given identifier
 	 * 
@@ -72,7 +79,7 @@ public abstract class AbstractFITestResourceGenerator extends AbstractRepoTest {
 	 */
 	protected void generateResourcesFor(Object commitHashIdentifier) throws Exception {
 		// Only the last commit is relevant here
-		this.resGen.generateResourceWhile(() -> executePropagationAndEvaluation(null,
-				this.getCommitHash(commitHashIdentifier), 0));
+		this.resGen.generateResourceWhile(
+				() -> executePropagationAndEvaluation(null, this.getCommitHash(commitHashIdentifier), 0));
 	}
 }
