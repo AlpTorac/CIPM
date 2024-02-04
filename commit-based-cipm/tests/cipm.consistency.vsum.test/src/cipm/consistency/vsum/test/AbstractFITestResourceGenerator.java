0d3a31e280ecc376b044eb3ba8be8d36aceea034
AlpTorac <a.torac@hotmail.com>
2024-02-04 15:05:26 +0100

    Extract super class
    Add new super class between AbstractResoTest and FirstInstance test resource generating tests

---
+++ commit-based-cipm/tests/cipm.consistency.vsum.test/src/cipm/consistency/vsum/test/AbstractFITestResourceGenerator.java
@@ -0,0 +1,78 @@
+package cipm.consistency.vsum.test;
+
+public abstract class AbstractFITestResourceGenerator extends AbstractRepoTest {
+	/**
+	 * The object responsible for generating Java-Models.
+	 * 
+	 * Requires to be instantiated 
+	 */
+	private FITestResourceGenerator resGen = null;
+	
+	@Override
+	public void tearDown() throws Exception {
+		super.tearDown();
+		this.resGen.cleanUp();
+	}
+	
+	/**
+	 * Instantiates a {@link FITestResourceGenerator} but does not initialise it.
+	 * Make sure to call {@link #initResGen(Object)} at the start of each test to
+	 * initialise the generated instance.
+	 */
+	protected FITestResourceGenerator createResGen() {
+		return new FITestResourceGenerator();
+	}
+	
+	/**
+	 * Computes a unique suffix for the model to be generated based on
+	 * the given parameter, so that generating multiple models is allowed.
+	 * 
+	 * @param commitHashIdentifier see {@link HasRepoSettings#getCommitHash(Object)}
+	 * 
+	 * @return The suffix of the file name (excluding extension)
+	 */
+	protected abstract String getTestModelFileIdentifier(Object commitHashIdentifier);
+	
+	/**
+	 * @param commitHashIdentifier see {@link HasRepoSettings#getCommitHash(Object)}
+	 * 
+	 * @return The full file name (excluding extension)
+	 */
+	protected String getTestModelFileNameWOExtension(Object commitHashIdentifier) {
+		return this.getTestModelFileNamePrefix() +
+				this.getTestModelFileIdentifier(commitHashIdentifier);
+	}
+	
+	/**
+	 * Initialises {@link #resGen}
+	 * 
+	 * @param commitHashIdentifier see {@link HasRepoSettings#getCommitHash(Object)}
+	 */
+	protected void initResGen(Object commitHashIdentifier) {
+		if (this.resGen != null) {
+			this.resGen.cleanUp();
+		}
+		
+		this.resGen = this.createResGen();
+		this.resGen.init(this.getTestType(), ".javaxmi");
+		this.resGen.addModelGenerationHook(this.getTestModelFileNameWOExtension(commitHashIdentifier));
+	}
+	
+	/**
+	 * @return The common prefix of the name of the model file to be generated.
+	 */
+	protected String getTestModelFileNamePrefix() {
+		return "JavaModel-test";
+	}
+	
+	/**
+	 * Attempts to generate a model for the commit hash with the given identifier
+	 * 
+	 * @param commitHashIdentifier see {@link HasRepoSettings#getCommitHash(Object)}
+	 */
+	protected void generateResourcesFor(Object commitHashIdentifier) throws Exception {
+		// Only the last commit is relevant here
+		this.resGen.generateResourceWhile(() -> executePropagationAndEvaluation(null,
+				this.getCommitHash(commitHashIdentifier), 0));
+	}
+}
--- commit-based-cipm/tests/cipm.consistency.vsum.test/src/cipm/consistency/vsum/test/FITeammatesTestResourceGenerator.java
+++ commit-based-cipm/tests/cipm.consistency.vsum.test/src/cipm/consistency/vsum/test/FITeammatesTestResourceGenerator.java
@@ -9,41 +9,44 @@ import org.junit.jupiter.api.TestMethodOrder;
 
 /**
  * A test case that is to be used to generate test resources required by FirstInstance tests. It
- * functions similar to {@link TEAMMATESCITest} but generates an additional Java-Model per test
- * and saves it at the corresponding path in the target directory of FirstInstance tests (fitests).
+ * is meant to generate test resources for FirstInstance tests (fitests) and save them at the
+ * corresponding path in the target directory of FirstInstance tests fitests.
+ * <br><br>
+ * Use the {@link #generateResourcesFor(Object)} method from the super class to generate the said
+ * test resources. Pass the index of the last commit hash to the aforementioned method, as the resource
+ * will only be generated with that commit in mind.
  * <br><br>
  * <b>Note that the size of generated test resources may slightly vary, which is to be expected</b>
  * 
  * @author atora
  */
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-public class FITeammatesTestResourceGenerator extends AbstractTEAMMATESCITest {
+public class FITeammatesTestResourceGenerator extends AbstractFITestResourceGenerator {
 	private static final Logger LOGGER = Logger.getLogger("cipm." + FITeammatesTestResourceGenerator.class.getSimpleName());
-	private FITestResourceGenerator resGen;
-	
-	protected String getTestModelFileName(int commitHashIndex) {
-		return "JavaModel-test" + commitHashIndex;
-	}
 	
 	@Disabled("Enable to generate resource")
 	@Order(0)
 	@Test
 	public void testTeammatesIntegration() throws Exception {
-		this.resGen = new FITestResourceGenerator();
-		this.resGen.init(this.getTestType(), this.getTestModelFileName(0), true);
-		
-		this.resGen.generateResourceWhile(() -> executePropagationAndEvaluation(null, this.getCommitHash(0), 0));
-		this.resGen.finalise();
+		this.initResGen(0);
+		this.generateResourcesFor(0);
 	}
 	
 	@Disabled("Enable to generate resource")
 	@Order(1)
 	@Test
 	public void testTeammatesFirstPropagation() throws Exception {
-		this.resGen = new FITestResourceGenerator();
-		this.resGen.init(this.getTestType(), this.getTestModelFileName(1), true);
-		
-		this.resGen.generateResourceWhile(() -> executePropagationAndEvaluation(null, this.getCommitHash(1), 0));
-		this.resGen.finalise();
+		this.initResGen(1);
+		this.generateResourcesFor(1);
+	}
+
+	@Override
+	protected String getTestModelFileIdentifier(Object commitHashIdentifier) {
+		return String.valueOf(commitHashIdentifier);
+	}
+
+	@Override
+	protected AbstractRepoSettings initRepoSettings() {
+		return new TeammatesRepoSettings();
 	}
 }
