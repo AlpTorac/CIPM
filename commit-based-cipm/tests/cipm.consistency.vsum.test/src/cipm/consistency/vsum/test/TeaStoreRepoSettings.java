ab89a9abb7a6bd1e99cae84143e233699a573ca2
AlpTorac <a.torac@hotmail.com>
2024-02-09 13:38:17 +0100

    Refactor and modify setUp for TeaStore tests
    It now iterates over all java directories under the target directory

--- commit-based-cipm/tests/cipm.consistency.vsum.test/src/cipm/consistency/vsum/test/TeaStoreRepoSettings.java
+++ commit-based-cipm/tests/cipm.consistency.vsum.test/src/cipm/consistency/vsum/test/TeaStoreRepoSettings.java
@@ -1,8 +1,12 @@
 package cipm.consistency.vsum.test;
 
 import java.io.File;
+import java.io.FilenameFilter;
+import java.io.IOException;
+
 import java.nio.file.Files;
 import java.nio.file.StandardCopyOption;
+
 import java.util.EnumMap;
 import java.util.Map;
 
@@ -79,21 +83,6 @@ public class TeaStoreRepoSettings implements HasRepoSettings {
 		return this.getExecFilesAddress() + File.separator + "settings.properties";
 	}
 	
-	@Override
-	public String getExecFilesAddress() {
-		return "teastore-exec-files";
-	}
-	
-	@Override
-	public ChangePropagationSpecification getJavaPCMSpec() {
-		return new CommitIntegrationJavaPCMChangePropagationSpecification();
-	}
-
-	@Override
-	public HasRepoSettings getRepoSettings() {
-		return this;
-	}
-	
 	public String getExternalCallTargetPairsFileName() {
 		return "external-call-target-pairs.json";
 	}
@@ -109,6 +98,25 @@ public class TeaStoreRepoSettings implements HasRepoSettings {
 	public String getModuleConfigsAddress() {
 		return this.getExecFilesAddress() + File.separator + this.getModuleConfigsFileName();
 	}
+
+	public String getExecFilesAddress() {
+		return "teastore-exec-files";
+	}
+	
+	public String getJavaDirName() {
+		return "java";
+	}
+	
+	@Override
+	public ChangePropagationSpecification getJavaPCMSpec() {
+		return new CommitIntegrationJavaPCMChangePropagationSpecification();
+	}
+
+	@Override
+	public HasRepoSettings getRepoSettings() {
+		return this;
+	}
+	
 	/**
 	 * {@inheritDoc}
 	 * <br><br>
@@ -116,13 +124,56 @@ public class TeaStoreRepoSettings implements HasRepoSettings {
 	 */
 	@Override
 	public void performTestSpecificSetUp(Object[] params) throws Exception {
+		this.copyExecFilesIntoAll((String) params[0]);
+	}
+	
+	/**
+	 * Copies the exec-files required to skip UI prompts during test runs
+	 * into all "java" directories under the given target directory path.
+	 * 
+	 * @param pathToTargetDir Path to the target directory
+	 */
+	protected void copyExecFilesIntoAll(String pathToTargetDir) throws IOException {
 		File targetPairsFile = new File(this.getExternalCallTargetPairsAddress());
 		File moduleConfigsFile = new File(this.getModuleConfigsAddress());
+		File targetDir = new File(pathToTargetDir);
+		File[] testDirs = null;
 		
-		String javaDirPath = this.makeJavaDir((String) params[0]);
+		if (targetDir.exists()) {
+			testDirs = targetDir.listFiles(new FilenameFilter() {
+				@Override
+				public boolean accept(File dir, String name) {
+					return dir.getName().contains(getTestGroup());
+				}
+			});
+		}
 		
-		File copyTargetPairsFile = new File(javaDirPath + File.separator + this.getExternalCallTargetPairsFileName());
-		File copyModuleConfigsFile = new File(javaDirPath + File.separator + this.getModuleConfigsFileName());
+		if (testDirs != null && testDirs.length > 0) {
+			for (File td : testDirs) {
+				
+				File javaDir = this.makeJavaDir(td.getPath());
+				this.copyExecFilesInto(targetPairsFile, moduleConfigsFile, javaDir.getPath());
+			}
+		} else {
+			targetDir.mkdirs();
+			
+			File tgd = this.makeTestGroupDir(targetDir.getPath());
+			File javaDir = this.makeJavaDir(tgd.getPath());
+			
+			this.copyExecFilesInto(targetPairsFile, moduleConfigsFile, javaDir.getPath());
+		}
+	}
+	
+	/**
+	 * Copies the given files into the directory at copyToPath
+	 * 
+	 * @param targetPairsFile File at {@link getExternalCallTargetPairsAddress}
+	 * @param moduleConfigsFile File at {@link #getModuleConfigsAddress()}
+	 * @param copyToPath Path to the directory, where the files will be copied into
+	 */
+	protected void copyExecFilesInto(File targetPairsFile, File moduleConfigsFile, String copyToPath) throws IOException {
+		File copyTargetPairsFile = new File(copyToPath + File.separator + this.getExternalCallTargetPairsFileName());
+		File copyModuleConfigsFile = new File(copyToPath + File.separator + this.getModuleConfigsFileName());
 		
 		copyTargetPairsFile.createNewFile();
 		copyModuleConfigsFile.createNewFile();
@@ -132,16 +183,25 @@ public class TeaStoreRepoSettings implements HasRepoSettings {
 	}
 	
 	/**
-	 * Creates the TestGroup/java directory under the given target directory path. 
+	 * Creates the TestGroup directory under the given target directory path. 
+	 * Creates all necessary directories along the way, if needed and possible.
+	 * 
+	 * @return The path to the newly created TestGroup directory
+	 */
+	protected File makeTestGroupDir(String pathToTargetDir) {
+		File targetDir = new File(pathToTargetDir + File.separator + this.getTestGroup());
+		targetDir.mkdirs();
+		return targetDir;
+	}
+	/**
+	 * Creates the TestGroup directory under the given target/TestGroup directory path. 
 	 * Creates all necessary directories along the way, if needed and possible.
 	 * 
 	 * @return The path to the newly created java directory
 	 */
-	protected String makeJavaDir(String pathToTargetDir) {
-		String pathToTargetTestType = pathToTargetDir + File.separator + this.getTestGroup();
-		
-		File javaDir = new File(pathToTargetTestType + File.separator + "java");
+	protected File makeJavaDir(String pathToTestGroupDir) {
+		File javaDir = new File(pathToTestGroupDir + File.separator + this.getJavaDirName());
 		javaDir.mkdirs();
-		return javaDir.getPath();
+		return javaDir;
 	}
 }
