dc7a8ccc5470394b8d3e1e904b6fbe6f9a071880
AlpTorac <a.torac@hotmail.com>
2024-04-01 10:40:33 +0200

    (WIP) Extract inner switches
    in the Java-related similarity checking
    
    each inner switch now has its own file

---
+++ commit-based-cipm/bundles/fi/org.splevo.jamopp.diffing/src/org/splevo/jamopp/diffing/similarity/switches/GenericsSimilaritySwitch.java
@@ -0,0 +1,57 @@
+package org.splevo.jamopp.diffing.similarity.switches;
+
+import org.emftext.language.java.generics.ExtendsTypeArgument;
+import org.emftext.language.java.generics.QualifiedTypeArgument;
+import org.emftext.language.java.generics.SuperTypeArgument;
+import org.emftext.language.java.generics.TypeParameter;
+import org.emftext.language.java.generics.UnknownTypeArgument;
+import org.emftext.language.java.generics.util.GenericsSwitch;
+import org.splevo.jamopp.diffing.similarity.ISimilaritySwitch;
+
+/**
+ * Similarity decisions for the generic elements.
+ */
+public class GenericsSimilaritySwitch extends GenericsSwitch<Boolean> {
+	private final ISimilaritySwitch similaritySwitch;
+
+	/**
+	 * @param similaritySwitch
+	 */
+	public GenericsSimilaritySwitch(ISimilaritySwitch similaritySwitch) {
+		this.similaritySwitch = similaritySwitch;
+	}
+
+	@Override
+	public Boolean caseQualifiedTypeArgument(QualifiedTypeArgument qta1) {
+		QualifiedTypeArgument qta2 = (QualifiedTypeArgument) this.similaritySwitch.getCompareElement();
+		return this.similaritySwitch.isSimilar(qta1.getTypeReference(), qta2.getTypeReference());
+	}
+	
+	@Override
+	public Boolean caseSuperTypeArgument(SuperTypeArgument sta1) {
+		SuperTypeArgument sta2 = (SuperTypeArgument) this.similaritySwitch.getCompareElement();
+		return this.similaritySwitch.isSimilar(sta1.getSuperType(), sta2.getSuperType());
+	}
+	
+	@Override
+	public Boolean caseExtendsTypeArgument(ExtendsTypeArgument eta1) {
+		ExtendsTypeArgument eta2 = (ExtendsTypeArgument) this.similaritySwitch.getCompareElement();
+		return this.similaritySwitch.isSimilar(eta1.getExtendType(), eta2.getExtendType());
+	}
+	
+	@Override
+	public Boolean caseUnknownTypeArgument(UnknownTypeArgument arg) {
+		return Boolean.TRUE;
+	}
+	
+    @Override
+    public Boolean caseTypeParameter(TypeParameter param1) {
+    	TypeParameter param2 = (TypeParameter) this.similaritySwitch.getCompareElement();
+    	
+    	if (!param1.getName().equals(param2.getName())) {
+    		return Boolean.FALSE;
+    	}
+    	
+    	return this.similaritySwitch.areSimilar(param1.getExtendTypes(), param2.getExtendTypes());
+    }
+}
---
+++ commit-based-cipm/bundles/fi/org.splevo.jamopp.diffing/src/org/splevo/jamopp/diffing/similarity/switches/LiteralsSimilaritySwitch.java
@@ -0,0 +1,139 @@
+package org.splevo.jamopp.diffing.similarity.switches;
+
+import org.eclipse.emf.ecore.EObject;
+import org.emftext.language.java.literals.BinaryIntegerLiteral;
+import org.emftext.language.java.literals.BinaryLongLiteral;
+import org.emftext.language.java.literals.BooleanLiteral;
+import org.emftext.language.java.literals.CharacterLiteral;
+import org.emftext.language.java.literals.DecimalDoubleLiteral;
+import org.emftext.language.java.literals.DecimalFloatLiteral;
+import org.emftext.language.java.literals.DecimalIntegerLiteral;
+import org.emftext.language.java.literals.DecimalLongLiteral;
+import org.emftext.language.java.literals.HexDoubleLiteral;
+import org.emftext.language.java.literals.HexFloatLiteral;
+import org.emftext.language.java.literals.HexIntegerLiteral;
+import org.emftext.language.java.literals.HexLongLiteral;
+import org.emftext.language.java.literals.OctalIntegerLiteral;
+import org.emftext.language.java.literals.OctalLongLiteral;
+import org.emftext.language.java.literals.util.LiteralsSwitch;
+import org.splevo.jamopp.diffing.similarity.ISimilaritySwitch;
+
+/**
+ * Similarity decisions for literal elements.
+ */
+public class LiteralsSimilaritySwitch extends LiteralsSwitch<Boolean> {
+
+    /**
+	 * 
+	 */
+	private final ISimilaritySwitch similaritySwitch;
+
+	/**
+	 * @param similaritySwitch
+	 */
+	public LiteralsSimilaritySwitch(ISimilaritySwitch similaritySwitch) {
+		this.similaritySwitch = similaritySwitch;
+	}
+
+	@Override
+    public Boolean caseBooleanLiteral(BooleanLiteral boolean1) {
+        BooleanLiteral boolean2 = (BooleanLiteral) this.similaritySwitch.getCompareElement();
+        return (boolean1.isValue() == boolean2.isValue());
+    }
+
+    @Override
+    public Boolean caseCharacterLiteral(CharacterLiteral char1) {
+        CharacterLiteral char2 = (CharacterLiteral) this.similaritySwitch.getCompareElement();
+        return char1.getValue().equals(char2.getValue());
+    }
+
+    @Override
+    public Boolean caseDecimalFloatLiteral(DecimalFloatLiteral float1) {
+        DecimalFloatLiteral float2 = (DecimalFloatLiteral) this.similaritySwitch.getCompareElement();
+        return compareDouble(float1.getDecimalValue(), float2.getDecimalValue());
+    }
+
+    @Override
+    public Boolean caseHexFloatLiteral(HexFloatLiteral float1) {
+        HexFloatLiteral float2 = (HexFloatLiteral) this.similaritySwitch.getCompareElement();
+        return compareDouble(float1.getHexValue(), float2.getHexValue());
+    }
+
+    @Override
+    public Boolean caseDecimalDoubleLiteral(DecimalDoubleLiteral double1) {
+        DecimalDoubleLiteral double2 = (DecimalDoubleLiteral) this.similaritySwitch.getCompareElement();
+        return compareDouble(double1.getDecimalValue(), double2.getDecimalValue());
+    }
+
+    @Override
+    public Boolean caseHexDoubleLiteral(HexDoubleLiteral double1) {
+        HexDoubleLiteral double2 = (HexDoubleLiteral) this.similaritySwitch.getCompareElement();
+        return compareDouble(double1.getHexValue(), double2.getHexValue());
+    }
+    
+    private boolean compareDouble(double d1, double d2) {
+    	return d1 == d2 || Double.isNaN(d1) && Double.isNaN(d2);
+    }
+
+    @Override
+    public Boolean caseDecimalIntegerLiteral(DecimalIntegerLiteral int1) {
+        DecimalIntegerLiteral int2 = (DecimalIntegerLiteral) this.similaritySwitch.getCompareElement();
+        return (int1.getDecimalValue().equals(int2.getDecimalValue()));
+    }
+
+    @Override
+    public Boolean caseHexIntegerLiteral(HexIntegerLiteral int1) {
+        HexIntegerLiteral int2 = (HexIntegerLiteral) this.similaritySwitch.getCompareElement();
+        return (int1.getHexValue().equals(int2.getHexValue()));
+    }
+
+    @Override
+    public Boolean caseOctalIntegerLiteral(OctalIntegerLiteral int1) {
+        OctalIntegerLiteral int2 = (OctalIntegerLiteral) this.similaritySwitch.getCompareElement();
+        return (int1.getOctalValue().equals(int2.getOctalValue()));
+    }
+
+    @Override
+    public Boolean caseDecimalLongLiteral(DecimalLongLiteral long1) {
+        DecimalLongLiteral long2 = (DecimalLongLiteral) this.similaritySwitch.getCompareElement();
+        return (long1.getDecimalValue().equals(long2.getDecimalValue()));
+    }
+
+    @Override
+    public Boolean caseHexLongLiteral(HexLongLiteral long1) {
+        HexLongLiteral long2 = (HexLongLiteral) this.similaritySwitch.getCompareElement();
+        return (long1.getHexValue().equals(long2.getHexValue()));
+    }
+
+    @Override
+    public Boolean caseOctalLongLiteral(OctalLongLiteral long1) {
+        OctalLongLiteral long2 = (OctalLongLiteral) this.similaritySwitch.getCompareElement();
+        return (long1.getOctalValue().equals(long2.getOctalValue()));
+    }
+    
+    @Override
+    public Boolean caseBinaryLongLiteral(BinaryLongLiteral long1) {
+    	BinaryLongLiteral long2 = (BinaryLongLiteral) this.similaritySwitch.getCompareElement();
+    	return long1.getBinaryValue().equals(long2.getBinaryValue());
+    }
+    
+    @Override
+    public Boolean caseBinaryIntegerLiteral(BinaryIntegerLiteral int1) {
+    	BinaryIntegerLiteral int2 = (BinaryIntegerLiteral) this.similaritySwitch.getCompareElement();
+    	return int1.getBinaryValue().equals(int2.getBinaryValue());
+    }
+
+    /**
+     * Check null literal similarity.<br>
+     * 
+     * Null literals are always assumed to be similar.
+     * 
+     * @param object
+     *            The literal to compare with the compare element.
+     * @return True As null always means null.
+     */
+    @Override
+    public Boolean defaultCase(EObject object) {
+        return Boolean.TRUE;
+    }
+}
