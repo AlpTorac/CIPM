b7cde08b1de198803681fe2e5e91a4eed54abf83
AlpTorac <a.torac@hotmail.com>
2024-04-07 20:38:47 +0200

    Work on similarity tests

--- commit-based-cipm/fi-tests/cipm.consistency.fitests/src/cipm/consistency/fitests/similarity/DummySimilaritySwitch.java
+++ commit-based-cipm/fi-tests/cipm.consistency.fitests/src/cipm/consistency/fitests/similarity/DummySimilaritySwitch.java
@@ -1,39 +1,45 @@
 package cipm.consistency.fitests.similarity;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.eclipse.emf.ecore.util.Switch;
 import org.splevo.jamopp.diffing.similarity.SimilaritySwitch;
 
 public class DummySimilaritySwitch extends SimilaritySwitch {
-	private List<Switch<Boolean>> switchesToAdd = null;
+	private final List<Switch<Boolean>> switchesToAdd = new ArrayList<Switch<Boolean>>();
 
 	public DummySimilaritySwitch(DummySimilarityComparer sc) {
-		super(sc);
+		this(sc, null);
 	}
 	
 	public DummySimilaritySwitch(DummySimilarityComparer sc,
 			List<Switch<Boolean>> switchesToAdd) {
 		super(sc);
-		this.switchesToAdd = switchesToAdd;
 		
-		if (this.switchesToAdd != null) {
-			this.switchesToAdd.forEach((s) -> this.addSwitch(s));
-		}
+		this.replaceSwitches(switchesToAdd);
 	}
 	
 	@Override
-	protected void initInnerSwitches() {
-		// ToDo: Make this flexible in SimilaritySwitch by separating it from constructor
-		// Maybe overload this method with a variant that gives control over which inner
-		// switches will be used
+	public void addSwitch(Switch<Boolean> sw) {
+		this.switchesToAdd.add(sw);
+		super.addSwitch(sw);
+	}
+	
+	@Override
+	public void removeSwitch(Switch<Boolean> sw) {
+		super.removeSwitch(sw);
+		this.switchesToAdd.remove(sw);
 	}
 	
 	public void replaceSwitches(List<Switch<Boolean>> switches) {
-		if (this.switchesToAdd != null) {
-			this.switchesToAdd.forEach((s) -> this.removeSwitch(s));
+		if (switches != null) {
+			this.clearSwitches();
+			switches.forEach((s) -> this.addSwitch(s));
 		}
-		
-		switches.forEach((s) -> this.addSwitch(s));
+	}
+	
+	public void clearSwitches() {
+		this.switchesToAdd.forEach((s) -> this.removeSwitch(s));
 	}
 }
