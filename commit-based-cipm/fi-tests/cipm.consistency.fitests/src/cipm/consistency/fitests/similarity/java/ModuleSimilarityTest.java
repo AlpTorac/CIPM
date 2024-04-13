8771445187b345e27e6b3f69c46253e868f86cd7
AlpTorac <a.torac@hotmail.com>
2024-04-13 16:18:05 +0200

    Work on tests
    Add package similarity tests, refactor test code and initialisers

--- commit-based-cipm/fi-tests/cipm.consistency.fitests/src/cipm/consistency/fitests/similarity/java/ModuleSimilarityTest.java
+++ commit-based-cipm/fi-tests/cipm.consistency.fitests/src/cipm/consistency/fitests/similarity/java/ModuleSimilarityTest.java
@@ -7,15 +7,26 @@ import java.util.Map;
 
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.util.Switch;
+import org.emftext.language.java.containers.Origin;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-
 import org.splevo.jamopp.diffing.similarity.switches.ContainersSimilaritySwitch;
 
 import cipm.consistency.fitests.similarity.java.initialiser.ModuleInitialiser;
 
-public class ModuleSimilarityTest extends AbstractSimilarityTest implements IModuleTest {
+public class ModuleSimilarityTest extends AbstractSimilarityTest implements IModuleTest, IPackageTest {
+	private IJavaModelConstructor ctor = new IJavaModelConstructor() {
+		@Override
+		public void fillResource(Resource res, Map<ResourceParameters, Object> params) {
+			var initialiser = new ModuleInitialiser();
+			
+			initialiser.setResource(res);
+			initialiser.build(params);
+		}
+	};
+	
 	@BeforeEach
 	@Override
 	public void setUp() {
@@ -32,22 +43,119 @@ public class ModuleSimilarityTest extends AbstractSimilarityTest implements IMod
 	}
 	
 	@Test
-	public void test() throws IOException {
-		var ctor = new IJavaModelConstructor() {
-			@Override
-			public void fillResource(Resource res, Map<ResourceParameters, Object> params) {
-				res.getContents().add(new ModuleInitialiser().build(params));
-			}
-		};
+	public void testModuleNameEquality() throws IOException {
+		var param = this.makeMinimalModuleParam("mod1");
 		
+		var resOne = this.createResource("modRes1", ctor, param);
+		var resTwo = this.createResource("modRes2", ctor, param);
+		
+		Assertions.assertTrue(this.areSimilar(resOne.getContents(), resTwo.getContents()));
+	}
+	
+	@Test
+	public void testModuleNameUnequality() throws IOException {
 		var params1 = this.makeMinimalModuleParam("mod1");
 		var params2 = this.makeMinimalModuleParam("mod2");
 		
-		var resOne = this.createResource("modResOne", ctor, params1);
-		var resTwo = this.createResource("modResTwo", ctor, params2);
-		var resThree = this.createResource("modResThree", ctor, params2);
+		var resOne = this.createResource("modRes1", ctor, params1);
+		var resTwo = this.createResource("modRes2", ctor, params2);
 		
-		Assertions.assertTrue(this.areSimilar(resTwo.getContents(), resThree.getContents()));
+		Assertions.assertFalse(this.areSimilar(resOne.getContents(), resTwo.getContents()));
+	}
+	
+	@Test
+	public void testModuleStructureEquality() throws IOException {
+		var pacNamespaces = new String[] {"ns1", "ns2", "ns3"};
+		
+		@SuppressWarnings("unchecked")
+		Map<ResourceParameters, Object> params1 = this.makeMinimalModuleWithPackagesParam("mod",
+				new Map[] {
+						this.makePackageParam("pac1", Origin.BINDING, pacNamespaces),
+						this.makePackageParam("pac2", Origin.BINDING, pacNamespaces)
+		});
+		
+		@SuppressWarnings("unchecked")
+		Map<ResourceParameters, Object> params2 = this.makeMinimalModuleWithPackagesParam("mod",
+				new Map[] {
+						this.makePackageParam("pac1", Origin.BINDING, pacNamespaces),
+						this.makePackageParam("pac2", Origin.BINDING, pacNamespaces)
+		});
+		
+		var resOne = this.createResource("modRes11", ctor, params1);
+		var resTwo = this.createResource("modRes12", ctor, params2);
+		
+		Assertions.assertTrue(this.areSimilar(resOne.getContents(), resTwo.getContents()));
+	}
+	
+	/**
+	 * Package names are currently not compared, disabled until further clarification
+	 */
+	@Disabled
+	@Test
+	public void testModulePackagesUnequality() throws IOException {
+		var pacNamespaces = new String[] {"ns1", "ns2", "ns3"};
+		
+		@SuppressWarnings("unchecked")
+		Map<ResourceParameters, Object> params1 = this.makeMinimalModuleWithPackagesParam("mod",
+				new Map[] {
+						this.makePackageParam("pac1", Origin.BINDING, pacNamespaces)
+		});
+		
+		@SuppressWarnings("unchecked")
+		Map<ResourceParameters, Object> params2 = this.makeMinimalModuleWithPackagesParam("mod",
+				new Map[] {
+						this.makePackageParam("pac2", Origin.BINDING, pacNamespaces)
+		});
+		
+		var resOne = this.createResource("pacRes21", ctor, params1);
+		var resTwo = this.createResource("pacRes22", ctor, params2);
+		
+		Assertions.assertFalse(this.areSimilar(resOne.getContents(), resTwo.getContents()));
+	}
+	
+	@Test
+	public void testModulePackagesSecondSubset() throws IOException {
+		var pacNamespaces = new String[] {"ns1", "ns2", "ns3"};
+		
+		@SuppressWarnings("unchecked")
+		Map<ResourceParameters, Object> params1 = this.makeMinimalModuleWithPackagesParam("mod",
+				new Map[] {
+						this.makePackageParam("pac1", Origin.BINDING, pacNamespaces),
+						this.makePackageParam("pac2", Origin.BINDING, pacNamespaces)
+		});
+		
+		@SuppressWarnings("unchecked")
+		Map<ResourceParameters, Object> params2 = this.makeMinimalModuleWithPackagesParam("mod",
+				new Map[] {
+						this.makePackageParam("pac2", Origin.BINDING, pacNamespaces)
+		});
+		
+		var resOne = this.createResource("pacRes21", ctor, params1);
+		var resTwo = this.createResource("pacRes22", ctor, params2);
+		
+		Assertions.assertFalse(this.areSimilar(resOne.getContents(), resTwo.getContents()));
+	}
+	
+	@Test
+	public void testModulePackagesFirstSubset() throws IOException {
+		var pacNamespaces = new String[] {"ns1", "ns2", "ns3"};
+		
+		@SuppressWarnings("unchecked")
+		Map<ResourceParameters, Object> params1 = this.makeMinimalModuleWithPackagesParam("mod",
+				new Map[] {
+						this.makePackageParam("pac1", Origin.BINDING, pacNamespaces)
+		});
+		
+		@SuppressWarnings("unchecked")
+		Map<ResourceParameters, Object> params2 = this.makeMinimalModuleWithPackagesParam("mod",
+				new Map[] {
+						this.makePackageParam("pac1", Origin.BINDING, pacNamespaces),
+						this.makePackageParam("pac2", Origin.BINDING, pacNamespaces)
+		});
+		
+		var resOne = this.createResource("pacRes21", ctor, params1);
+		var resTwo = this.createResource("pacRes22", ctor, params2);
+		
 		Assertions.assertFalse(this.areSimilar(resOne.getContents(), resTwo.getContents()));
 	}
 }
--- commit-based-cipm/fi-tests/cipm.consistency.fitests/src/cipm/consistency/fitests/similarity/java/initialiser/ModuleInitialiser.java
+++ commit-based-cipm/fi-tests/cipm.consistency.fitests/src/cipm/consistency/fitests/similarity/java/initialiser/ModuleInitialiser.java
@@ -2,12 +2,15 @@ package cipm.consistency.fitests.similarity.java.initialiser;
 
 import java.util.Map;
 
+import org.eclipse.emf.ecore.resource.Resource;
 import org.emftext.language.java.containers.ContainersFactory;
 import org.emftext.language.java.containers.Module;
 
 import cipm.consistency.fitests.similarity.java.ResourceParameters;
 
 public class ModuleInitialiser implements IModuleInitialiser {
+	private Resource res;
+	
 	@Override
 	public boolean isSetDefaultName() {
 		return true;
@@ -22,5 +25,15 @@ public class ModuleInitialiser implements IModuleInitialiser {
 	public Module instantiateModule(Map<ResourceParameters, Object> modParam) {
 		var fac = ContainersFactory.eINSTANCE;
 		return fac.createModule();
+	}
+	
+	@Override
+	public void setResource(Resource res) {
+		this.res = res;
+	}
+
+	@Override
+	public Resource getResource() {
+		return this.res;
 	}
 }
