65db8ecfbfd6ea3e53f1af316247e961d653e301
AlpTorac <a.torac@hotmail.com>
2024-04-14 21:33:06 +0200

    Add test class
    The new test class tests the control flow of the similarity checking, until instance-specific checking is reached
    
    Change the way parameters are provided

--- commit-based-cipm/fi-tests/cipm.consistency.fitests/src/cipm/consistency/fitests/similarity/java/NamedElementSimilarityTest.java
+++ commit-based-cipm/fi-tests/cipm.consistency.fitests/src/cipm/consistency/fitests/similarity/java/NamedElementSimilarityTest.java
@@ -1,18 +1,16 @@
 package cipm.consistency.fitests.similarity.java;
 
 import java.util.List;
-import java.util.stream.Stream;
 
 import org.emftext.language.java.commons.NamedElement;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ArgumentsSource;
 
-import cipm.consistency.fitests.similarity.java.initialiser.CompilationUnitInitialiser;
 import cipm.consistency.fitests.similarity.java.initialiser.INamedElementInitialiser;
-import cipm.consistency.fitests.similarity.java.initialiser.ModuleInitialiser;
+
+// TODO: Clarify whether Package-instances' names (not namespace) should be compared.
 
 public class NamedElementSimilarityTest extends AbstractSimilarityTest {
 	private final String name11 = "name11";
@@ -28,14 +26,6 @@ public class NamedElementSimilarityTest extends AbstractSimilarityTest {
 		super.setUp();
 	}
 	
-	private static Stream<Arguments> generateNameSimilarityParams() {
-		return Stream.of(
-					Arguments.of(new ModuleInitialiser()),
-//					Arguments.of(new PackageInitialiser()),
-					Arguments.of(new CompilationUnitInitialiser())
-				);
-	}
-	
 	protected NamedElement initElement(INamedElementInitialiser initialiser, String name) {
 		var result = initialiser.instantiate();
 		initialiser.initialiseName(result, name);
@@ -43,7 +33,7 @@ public class NamedElementSimilarityTest extends AbstractSimilarityTest {
 	}
 	
 	@ParameterizedTest
-	@MethodSource("generateNameSimilarityParams")
+	@ArgumentsSource(InitialiserProvider.class)
 	public void testSameReference(INamedElementInitialiser initialiser) {
 		this.setResourceFileTestIdentifier("testSameReference");
 		
@@ -54,12 +44,12 @@ public class NamedElementSimilarityTest extends AbstractSimilarityTest {
 	}
 	
 	@ParameterizedTest
-	@MethodSource("generateNameSimilarityParams")
+	@ArgumentsSource(InitialiserProvider.class)
 	public void testSameName(INamedElementInitialiser initialiser) {
 		this.setResourceFileTestIdentifier("testSameName");
 		
 		var objOne = this.initElement(initialiser, name11);
-		var objTwo = this.initElement(initialiser, name11);
+		var objTwo = initialiser.clone(objOne);
 		
 		var resOne = this.createResource(List.of(objOne));
 		var resTwo = this.createResource(List.of(objTwo));
@@ -68,7 +58,7 @@ public class NamedElementSimilarityTest extends AbstractSimilarityTest {
 	}
 	
 	@ParameterizedTest
-	@MethodSource("generateNameSimilarityParams")
+	@ArgumentsSource(InitialiserProvider.class)
 	public void testDifferentName(INamedElementInitialiser initialiser) {
 		this.setResourceFileTestIdentifier("testDifferentName");
 		
@@ -80,32 +70,17 @@ public class NamedElementSimilarityTest extends AbstractSimilarityTest {
 		
 		Assertions.assertFalse(this.areSimilar(resOne.getContents(), resTwo.getContents()));
 	}
-	
+
 	@ParameterizedTest
-	@MethodSource("generateNameSimilarityParams")
-	public void testListSameReference(INamedElementInitialiser initialiser) {
-		this.setResourceFileTestIdentifier("testListSameReference");
-		
-		var obj11 = this.initElement(initialiser, name11);
-		var obj12 = this.initElement(initialiser, name12);
-		
-		var list1 = List.of(obj11, obj12);
-		
-		var resOne = this.createResource(list1);
-		
-		Assertions.assertTrue(this.areSimilar(resOne.getContents(), resOne.getContents()));
-	}
-	
-	@ParameterizedTest
-	@MethodSource("generateNameSimilarityParams")
+	@ArgumentsSource(InitialiserProvider.class)
 	public void testListSameNamesSameOrder(INamedElementInitialiser initialiser) {
 		this.setResourceFileTestIdentifier("testListSameNamesSameOrder");
 		
 		var obj11 = this.initElement(initialiser, name11);
-		var obj21 = this.initElement(initialiser, name11);
+		var obj21 = initialiser.clone(obj11);
 		
 		var obj12 = this.initElement(initialiser, name12);
-		var obj22 = this.initElement(initialiser, name12);
+		var obj22 = initialiser.clone(obj12);
 		
 		var list1 = List.of(obj11, obj12);
 		var list2 = List.of(obj21, obj22);
@@ -123,15 +98,15 @@ public class NamedElementSimilarityTest extends AbstractSimilarityTest {
 	 * to be compared pairwise.
 	 */
 	@ParameterizedTest
-	@MethodSource("generateNameSimilarityParams")
+	@ArgumentsSource(InitialiserProvider.class)
 	public void testListSameNamesDifferentOrder(INamedElementInitialiser initialiser) {
 		this.setResourceFileTestIdentifier("testListSameNamesDifferentOrder");
 		
 		var obj11 = this.initElement(initialiser, name11);
-		var obj21 = this.initElement(initialiser, name11);
+		var obj21 = initialiser.clone(obj11);
 		
 		var obj12 = this.initElement(initialiser, name12);
-		var obj22 = this.initElement(initialiser, name12);
+		var obj22 = initialiser.clone(obj12);
 		
 		var list1 = List.of(obj11, obj12);
 		var list2 = List.of(obj22, obj21);
@@ -143,7 +118,7 @@ public class NamedElementSimilarityTest extends AbstractSimilarityTest {
 	}
 	
 	@ParameterizedTest
-	@MethodSource("generateNameSimilarityParams")
+	@ArgumentsSource(InitialiserProvider.class)
 	public void testListDifferentNames(INamedElementInitialiser initialiser) {
 		this.setResourceFileTestIdentifier("testListDifferentNames");
 		
@@ -155,44 +130,6 @@ public class NamedElementSimilarityTest extends AbstractSimilarityTest {
 		
 		var list1 = List.of(obj11, obj12);
 		var list2 = List.of(obj21, obj22);
-		
-		var resOne = this.createResource(list1);
-		var resTwo = this.createResource(list2);
-		
-		Assertions.assertFalse(this.areSimilar(resOne.getContents(), resTwo.getContents()));
-	}
-	
-	@ParameterizedTest
-	@MethodSource("generateNameSimilarityParams")
-	public void testListLeftSubset(INamedElementInitialiser initialiser) {
-		this.setResourceFileTestIdentifier("testListLeftSubset");
-		
-		var obj11 = this.initElement(initialiser, name11);
-		var obj21 = this.initElement(initialiser, name11);
-		
-		var obj12 = this.initElement(initialiser, name12);
-		
-		var list1 = List.of(obj11);
-		var list2 = List.of(obj21, obj12);
-		
-		var resOne = this.createResource(list1);
-		var resTwo = this.createResource(list2);
-		
-		Assertions.assertFalse(this.areSimilar(resOne.getContents(), resTwo.getContents()));
-	}
-	
-	@ParameterizedTest
-	@MethodSource("generateNameSimilarityParams")
-	public void testListRightSubset(INamedElementInitialiser initialiser) {
-		this.setResourceFileTestIdentifier("testListRightSubset");
-		
-		var obj11 = this.initElement(initialiser, name11);
-		var obj21 = this.initElement(initialiser, name11);
-		
-		var obj12 = this.initElement(initialiser, name12);
-		
-		var list1 = List.of(obj11, obj12);
-		var list2 = List.of(obj21);
 		
 		var resOne = this.createResource(list1);
 		var resTwo = this.createResource(list2);
