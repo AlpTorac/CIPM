d63a79c9630fb53ea6162240803be6ac53a7b4a6
AlpTorac <a.torac@hotmail.com>
2024-04-23 18:44:21 +0200

    Implement initialisers
    Implemented more initialisers corresponding to the elements in the hierarchy tree of EObject and integrated them to the existing initialisers

---
+++ commit-based-cipm/fi-tests/cipm.consistency.fitests/src/cipm/consistency/fitests/similarity/java/initialiser/IAnnotableAndModifiableInitialiser.java
@@ -0,0 +1,38 @@
+package cipm.consistency.fitests.similarity.java.initialiser;
+
+import org.emftext.language.java.annotations.AnnotationInstance;
+import org.emftext.language.java.modifiers.AnnotableAndModifiable;
+import org.emftext.language.java.modifiers.Modifier;
+
+public interface IAnnotableAndModifiableInitialiser extends ICommentableInitialiser {
+	@Override
+	public AnnotableAndModifiable instantiate();
+	
+	public default void addModifier(AnnotableAndModifiable aam, Modifier modif) {
+		if (modif != null) {
+			aam.addModifier(modif);
+			assert aam.getAnnotationsAndModifiers().contains(modif);
+			assert aam.hasModifier(modif.getClass());
+			assert aam.getModifiers().contains(modif);
+		}
+	}
+	
+	public default void addAnnotationInstance(AnnotableAndModifiable aam, AnnotationInstance ai) {
+		if (aam != null) {
+			aam.getAnnotationInstances().add(ai);
+			assert aam.getAnnotationInstances().contains(ai);
+		}
+	}
+	
+	public default void makePrivate(AnnotableAndModifiable aam) {
+		aam.makePrivate();
+	}
+	
+	public default void makeProtected(AnnotableAndModifiable aam) {
+		aam.makeProtected();
+	}
+	
+	public default void makePublic(AnnotableAndModifiable aam) {
+		aam.makePublic();
+	}
+}
