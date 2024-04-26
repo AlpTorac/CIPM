e86d3f72405963b1dc720e6ff5504f5c4d24715f
AlpTorac <a.torac@hotmail.com>
2024-04-26 18:57:41 +0200

    Remove unused imports

--- commit-based-cipm/fi-tests/cipm.consistency.fitests/src/cipm/consistency/fitests/similarity/java/initialiser/impl/AnnotationInstanceInitialiser.java
+++ commit-based-cipm/fi-tests/cipm.consistency.fitests/src/cipm/consistency/fitests/similarity/java/initialiser/impl/AnnotationInstanceInitialiser.java
@@ -1,14 +1,10 @@
 package cipm.consistency.fitests.similarity.java.initialiser.impl;
 
 import org.emftext.language.java.annotations.AnnotationsFactory;
-import org.emftext.language.java.annotations.AnnotationAttributeSetting;
-import org.emftext.language.java.annotations.AnnotationValue;
-import org.emftext.language.java.expressions.ExpressionsFactory;
 
 import cipm.consistency.fitests.similarity.java.initialiser.IAnnotationInstanceInitialiser;
 
 import org.emftext.language.java.annotations.AnnotationInstance;
-import org.emftext.language.java.annotations.AnnotationParameter;
 
 public class AnnotationInstanceInitialiser implements IAnnotationInstanceInitialiser, IInitialiser {
 	@Override
