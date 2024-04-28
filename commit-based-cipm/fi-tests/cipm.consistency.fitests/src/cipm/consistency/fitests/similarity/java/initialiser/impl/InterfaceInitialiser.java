8168fb3e5f0a71d213e0aab509ba72c037f56026
AlpTorac <a.torac@hotmail.com>
2024-04-28 14:30:17 +0200

    Make IInitialiser generic

--- commit-based-cipm/fi-tests/cipm.consistency.fitests/src/cipm/consistency/fitests/similarity/java/initialiser/impl/InterfaceInitialiser.java
+++ commit-based-cipm/fi-tests/cipm.consistency.fitests/src/cipm/consistency/fitests/similarity/java/initialiser/impl/InterfaceInitialiser.java
@@ -5,7 +5,7 @@ import org.emftext.language.java.classifiers.Interface;
 
 import cipm.consistency.fitests.similarity.java.initialiser.IInterfaceInitialiser;
 
-public class InterfaceInitialiser implements IInterfaceInitialiser, IInitialiser {
+public class InterfaceInitialiser implements IInterfaceInitialiser, IInitialiser<Interface> {
 	@Override
 	public Interface instantiate() {
 		var fac = ClassifiersFactory.eINSTANCE;
