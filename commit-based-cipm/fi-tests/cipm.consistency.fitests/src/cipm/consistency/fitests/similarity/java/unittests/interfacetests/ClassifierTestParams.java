0d904eb612cdf68bf245a83d28eac8dd3a37bffd
AlpTorac <a.torac@hotmail.com>
2024-06-01 16:09:49 +0200

    Remove TypeParameter
    From classifier test parameters

--- commit-based-cipm/fi-tests/cipm.consistency.fitests/src/cipm/consistency/fitests/similarity/java/unittests/interfacetests/ClassifierTestParams.java
+++ commit-based-cipm/fi-tests/cipm.consistency.fitests/src/cipm/consistency/fitests/similarity/java/unittests/interfacetests/ClassifierTestParams.java
@@ -6,17 +6,19 @@ import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.ArgumentsProvider;
 
+import cipm.consistency.fitests.similarity.java.initialiser.generics.ITypeParameterInitialiser;
 import cipm.consistency.fitests.similarity.java.initialiser.testable.IClassifierInitialiser;
 import cipm.consistency.fitests.similarity.java.params.InitialiserParameters;
 
 public class ClassifierTestParams implements ArgumentsProvider {
-
 	@Override
 	public Stream<? extends Arguments> provideArguments(ExtensionContext arg0) throws Exception {
 		return new InitialiserParameters()
 				.getInitialisersBySuper(IClassifierInitialiser.class)
 				.stream()
-//				.filter((i) -> !ITypeParameterInitialiser.class.isInstance(i))
+				
+				// Classifiers are only tested for their import methods, TypeParameter does not use them
+				.filter((i) -> !ITypeParameterInitialiser.class.isInstance(i))
 				.map((i) -> Arguments.of(i));
 	}
 }
