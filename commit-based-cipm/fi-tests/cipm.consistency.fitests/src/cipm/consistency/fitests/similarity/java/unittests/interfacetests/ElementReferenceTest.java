85511de414cbc50b45524d741cbd261d59dfcffd
AlpTorac <a.torac@hotmail.com>
2024-06-01 16:13:08 +0200

    (WIP) Attempt to fix test
    still failing

--- commit-based-cipm/fi-tests/cipm.consistency.fitests/src/cipm/consistency/fitests/similarity/java/unittests/interfacetests/ElementReferenceTest.java
+++ commit-based-cipm/fi-tests/cipm.consistency.fitests/src/cipm/consistency/fitests/similarity/java/unittests/interfacetests/ElementReferenceTest.java
@@ -6,16 +6,25 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ArgumentsSource;
 
 import cipm.consistency.fitests.similarity.java.EObjectSimilarityTest;
+import cipm.consistency.fitests.similarity.java.initialiser.annotations.AnnotationInstanceInitialiser;
 import cipm.consistency.fitests.similarity.java.initialiser.testable.IElementReferenceInitialiser;
+import cipm.consistency.fitests.similarity.java.unittests.UsesAnnotationInstances;
+import cipm.consistency.fitests.similarity.java.unittests.UsesAnnotationParameters;
 import cipm.consistency.fitests.similarity.java.unittests.UsesConcreteClassifiers;
 
-public class ElementReferenceTest extends EObjectSimilarityTest implements UsesConcreteClassifiers {
-	protected ElementReference initElement(IElementReferenceInitialiser init, ReferenceableElement cTarget,
-			ReferenceableElement target) {
+public class ElementReferenceTest extends EObjectSimilarityTest implements UsesConcreteClassifiers,
+UsesAnnotationInstances, UsesAnnotationParameters {
+	protected ElementReference initElement(IElementReferenceInitialiser init,
+			ReferenceableElement target, ReferenceableElement cTarget) {
 		ElementReference result = init.instantiate();
 		init.minimalInitialisation(result);
 		init.setTarget(result, target);
-		init.setContainedTarget(result, target);
+		init.setContainedTarget(result, cTarget);
+		
+		var aiInit = new AnnotationInstanceInitialiser();
+		var ai = this.createMinimalAI(new String[] {"nsai"}, "an");
+		aiInit.setAnnotationParameter(ai, this.createSingleAnnoParam(result));
+		
 		return result;
 	}
 	
@@ -24,8 +33,11 @@ public class ElementReferenceTest extends EObjectSimilarityTest implements UsesC
 	public void testTarget(IElementReferenceInitialiser init) {
 		this.setResourceFileTestIdentifier("testTarget");
 		
-		var objOne = this.initElement(init, this.createMinimalClass("cls1"), null);
-		var objTwo = this.initElement(init, this.createMinimalClass("cls2"), null);
+		var cls1 = this.createMinimalClassWithPac("cls1", new String[] {"ns1"});
+		var cls2 = this.createMinimalClassWithPac("cls2", new String[] {"ns2"});
+		
+		var objOne = this.initElement(init, cls1.getPackage(), cls1);
+		var objTwo = this.initElement(init, cls2.getPackage(), cls2);
 		
 		this.testX(objOne, objTwo, false);
 	}
