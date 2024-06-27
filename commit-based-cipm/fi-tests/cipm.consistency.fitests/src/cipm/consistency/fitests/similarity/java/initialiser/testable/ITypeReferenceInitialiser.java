7adf4737a6de9e2527df71f31ee0ea60f2da2e72
AlpTorac <a.torac@hotmail.com>
2024-06-27 17:18:37 +0200

    Add assertions for setTarget method

--- commit-based-cipm/fi-tests/cipm.consistency.fitests/src/cipm/consistency/fitests/similarity/java/initialiser/testable/ITypeReferenceInitialiser.java
+++ commit-based-cipm/fi-tests/cipm.consistency.fitests/src/cipm/consistency/fitests/similarity/java/initialiser/testable/ITypeReferenceInitialiser.java
@@ -6,10 +6,12 @@ import org.emftext.language.java.types.TypeReference;
 import cipm.consistency.fitests.similarity.java.initialiser.IArrayTypeableInitialiser;
 
 public interface ITypeReferenceInitialiser extends IArrayTypeableInitialiser {
+	/**
+	 * Does not change tref, unless it is an instance of {@link NamespaceClassifierReference}.
+	 */
 	public default void setTarget(TypeReference tref, Classifier cls) {
 		if (cls != null) {
 			tref.setTarget(cls);
-			// TODO: Write assertion
 		}
 	}
 }
