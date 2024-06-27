92b01ca23c1e6d0373b5512bcd7b216be34f57ac
AlpTorac <a.torac@hotmail.com>
2024-06-27 15:47:42 +0200

    Remove unused abstract class

--- commit-based-cipm/fi-tests/cipm.consistency.fitests/src/cipm/consistency/fitests/similarity/java/initialiser/parameters/ReceiverParameterInitialiser.java
+++ commit-based-cipm/fi-tests/cipm.consistency.fitests/src/cipm/consistency/fitests/similarity/java/initialiser/parameters/ReceiverParameterInitialiser.java
@@ -3,22 +3,14 @@ package cipm.consistency.fitests.similarity.java.initialiser.parameters;
 import org.emftext.language.java.parameters.ParametersFactory;
 import org.emftext.language.java.parameters.ReceiverParameter;
 
-import cipm.consistency.fitests.similarity.java.initialiser.ParameterInitialiser;
-import cipm.consistency.fitests.similarity.java.initialiser.testable.IParametrizableInitialiser;
-
-public class ReceiverParameterInitialiser extends ParameterInitialiser implements IReceiverParameterInitialiser {
+public class ReceiverParameterInitialiser implements IReceiverParameterInitialiser {
 	@Override
 	public ReceiverParameter instantiate() {
 		return ParametersFactory.eINSTANCE.createReceiverParameter();
 	}
-	
-	@Override
-	public ReceiverParameterInitialiser withPInit(IParametrizableInitialiser pInit) {
-		return (ReceiverParameterInitialiser) super.withPInit(pInit);
-	}
 
 	@Override
-	public ParameterInitialiser newInitialiser() {
+	public IReceiverParameterInitialiser newInitialiser() {
 		return new ReceiverParameterInitialiser();
 	}
 }
