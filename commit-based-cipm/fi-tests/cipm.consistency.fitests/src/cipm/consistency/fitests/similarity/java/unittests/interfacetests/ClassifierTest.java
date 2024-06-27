700fdc8f05e2e156c4470a0b7603a0dcf97e05ec
AlpTorac <a.torac@hotmail.com>
2024-06-27 16:26:35 +0200

    Enable and adapt ClassifierTest

--- commit-based-cipm/fi-tests/cipm.consistency.fitests/src/cipm/consistency/fitests/similarity/java/unittests/interfacetests/ClassifierTest.java
+++ commit-based-cipm/fi-tests/cipm.consistency.fitests/src/cipm/consistency/fitests/similarity/java/unittests/interfacetests/ClassifierTest.java
@@ -1,8 +1,9 @@
 package cipm.consistency.fitests.similarity.java.unittests.interfacetests;
 
 import org.emftext.language.java.classifiers.Classifier;
-import org.emftext.language.java.classifiers.ClassifiersPackage;
+import org.emftext.language.java.containers.CompilationUnit;
 import org.emftext.language.java.imports.Import;
+import org.emftext.language.java.imports.ImportsPackage;
 import org.emftext.language.java.imports.PackageImport;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ArgumentsSource;
@@ -29,33 +30,33 @@ public class ClassifierTest extends EObjectSimilarityTest implements UsesImports
 	
 	//TODO: Clean up, if these tests prove to be useless.
 	
-//	@ParameterizedTest
-//	@ArgumentsSource(ClassifierTestParams.class)
-//	public void testImports(IClassifierInitialiser initialiser) {
-//		this.setResourceFileTestIdentifier("testImports");
-//		
-//		var objOne = this.initElement(initialiser,
-//				new Import[] {this.createMinimalClsImport("cls1")}, null);
-//		var objTwo = this.initElement(initialiser,
-//				new Import[] {this.createMinimalClsImport("cls2")}, null);
-//		
-//		this.testX(objOne, objTwo, ClassifiersPackage.Literals);
-//	}
-//	
-//	/**
-//	 * Package import differences do not break similarity
-//	 */
-//	@ParameterizedTest
-//	@ArgumentsSource(ClassifierTestParams.class)
-//	public void testPackageImports(IClassifierInitialiser initialiser) {
-//		this.setResourceFileTestIdentifier("testPackageImports");
-//		
-//		var objOne = this.initElement(initialiser,
-//				null, new PackageImport[] {
-//						this.createMinimalPackageImport(new String[] {"ns1", "ns2"})});
-//		var objTwo = this.initElement(initialiser,
-//				null, new PackageImport[] {this.createMinimalPackageImport(new String[] {"ns3", "ns4"})});
-//		
-//		this.testX(objOne, objTwo, false);
-//	}
+	@ParameterizedTest
+	@ArgumentsSource(ClassifierTestParams.class)
+	public void testImports(IClassifierInitialiser initialiser) {
+		this.setResourceFileTestIdentifier("testImports");
+		
+		var objOne = this.initElement(initialiser,
+				new Import[] {this.createMinimalClsImport("cls1")}, null);
+		var objTwo = this.initElement(initialiser,
+				new Import[] {this.createMinimalClsImport("cls2")}, null);
+		
+		this.testX(objOne, objTwo, CompilationUnit.class, ImportsPackage.Literals.IMPORTING_ELEMENT__IMPORTS);
+	}
+	
+	/**
+	 * Package import differences do not break similarity
+	 */
+	@ParameterizedTest
+	@ArgumentsSource(ClassifierTestParams.class)
+	public void testPackageImports(IClassifierInitialiser initialiser) {
+		this.setResourceFileTestIdentifier("testPackageImports");
+		
+		var objOne = this.initElement(initialiser,
+				null, new PackageImport[] {
+						this.createMinimalPackageImport(new String[] {"ns1", "ns2"})});
+		var objTwo = this.initElement(initialiser,
+				null, new PackageImport[] {this.createMinimalPackageImport(new String[] {"ns3", "ns4"})});
+		
+		this.testX(objOne, objTwo, CompilationUnit.class, ImportsPackage.Literals.IMPORTING_ELEMENT__IMPORTS);
+	}
 }
