60fe80928c7191481c9933474b0174022c760c3f
AlpTorac <a.torac@hotmail.com>
2024-06-28 09:52:40 +0200

    Remove unused dummy and helper classes

--- commit-based-cipm/fi-tests/cipm.consistency.fitests/src/cipm/consistency/fitests/similarity/java/utils/InnerSwitchFactory.java
+++
@@ -1,9 +0,0 @@
-package cipm.consistency.fitests.similarity.java.utils;
-
-import java.util.List;
-
-import org.eclipse.emf.ecore.util.Switch;
-
-public interface InnerSwitchFactory {
-	public List<Switch<Boolean>> createSwitchesFor(DummySimilaritySwitch dss);
-}
