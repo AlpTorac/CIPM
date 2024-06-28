b3ab2212ee1bdd249e0580e847825905cc6862a3
AlpTorac <a.torac@hotmail.com>
2024-06-28 20:12:10 +0200

    Remove TODO
    Add commentary

--- commit-based-cipm/fi-tests/cipm.consistency.fitests/src/cipm/consistency/fitests/similarity/java/unittests/interfacetests/TypeReferenceTest.java
+++ commit-based-cipm/fi-tests/cipm.consistency.fitests/src/cipm/consistency/fitests/similarity/java/unittests/interfacetests/TypeReferenceTest.java
@@ -1,25 +1,14 @@
 package cipm.consistency.fitests.similarity.java.unittests.interfacetests;
 
-//import org.emftext.language.java.references.ReferencesPackage;
-//import org.emftext.language.java.types.TypesPackage;
-//import org.junit.jupiter.params.ParameterizedTest;
-//import org.junit.jupiter.params.provider.ArgumentsSource;
-
 import cipm.consistency.fitests.similarity.java.EObjectSimilarityTest;
-//import cipm.consistency.fitests.similarity.java.initialiser.testable.ITypeReferenceInitialiser;
 import cipm.consistency.fitests.similarity.java.unittests.UsesTypeReferences;
 
+/**
+ * {@link TypeReference} has no attributes to be tested. Left here for possible
+ * future tests.
+ * 
+ * @author atora
+ */
 public class TypeReferenceTest extends EObjectSimilarityTest implements UsesTypeReferences {
-	// TODO: Clean up if useless
-	
-//	@ParameterizedTest
-//	@ArgumentsSource(TypeReferenceTestParams.class)
-//	public void testTarget(ITypeReferenceInitialiser init) {
-//		this.setResourceFileTestIdentifier("testTarget");
-//		
-//		var objOne = this.createMinimalClsRef(init, "cls1");
-//		var objTwo = this.createMinimalClsRef(init, "cls2");
-//		
-//		this.testX(objOne, objTwo, false);
-//	}
+
 }
