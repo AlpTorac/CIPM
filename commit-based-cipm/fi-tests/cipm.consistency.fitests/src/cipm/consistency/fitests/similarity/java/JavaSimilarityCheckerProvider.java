ee33e0d15975a9f1eb5d1511b84e652f0bfb21a1
AlpTorac <a.torac@hotmail.com>
2024-06-28 09:51:43 +0200

    Extract SimilarityChecker construction

---
+++ commit-based-cipm/fi-tests/cipm.consistency.fitests/src/cipm/consistency/fitests/similarity/java/JavaSimilarityCheckerProvider.java
@@ -0,0 +1,22 @@
+package cipm.consistency.fitests.similarity.java;
+
+import org.splevo.jamopp.diffing.similarity.JavaSimilarityChecker;
+import org.splevo.jamopp.diffing.similarity.JavaSimilarityToolboxBuilder;
+import org.splevo.jamopp.diffing.similarity.base.ISimilarityChecker;
+import org.splevo.jamopp.diffing.similarity.base.MapSimilarityToolboxFactory;
+
+public class JavaSimilarityCheckerProvider implements ISimilarityCheckerProvider {
+	@Override
+	public ISimilarityChecker createSC() {
+        var builder = new JavaSimilarityToolboxBuilder();
+        builder.setSimilarityToolboxFactory(new MapSimilarityToolboxFactory());
+        
+        var toolbox = builder.instantiate()
+        	.buildNewSimilaritySwitchHandler()
+        	.buildNormalizationHandlers()
+        	.buildComparisonHandlers()
+        	.build();
+		
+		return new JavaSimilarityChecker(toolbox);
+	}
+}
