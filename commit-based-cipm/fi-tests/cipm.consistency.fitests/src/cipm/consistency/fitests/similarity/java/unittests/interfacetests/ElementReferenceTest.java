3787565f587555bf316ef859632541eb4276e653
AlpTorac <a.torac@hotmail.com>
2024-06-29 19:17:05 +0200

    Simplify test

--- commit-based-cipm/fi-tests/cipm.consistency.fitests/src/cipm/consistency/fitests/similarity/java/unittests/interfacetests/ElementReferenceTest.java
+++ commit-based-cipm/fi-tests/cipm.consistency.fitests/src/cipm/consistency/fitests/similarity/java/unittests/interfacetests/ElementReferenceTest.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ArgumentsSource;
 
 import cipm.consistency.fitests.similarity.java.EObjectSimilarityTest;
-import cipm.consistency.fitests.similarity.java.initialiser.annotations.AnnotationInstanceInitialiser;
 import cipm.consistency.fitests.similarity.java.initialiser.references.IElementReferenceInitialiser;
 import cipm.consistency.fitests.similarity.java.unittests.UsesAnnotationInstances;
 import cipm.consistency.fitests.similarity.java.unittests.UsesAnnotationParameters;
@@ -23,10 +22,6 @@ UsesAnnotationInstances, UsesAnnotationParameters {
 		Assertions.assertTrue(init.setTarget(result, target));
 		Assertions.assertTrue(init.setContainedTarget(result, cTarget));
 		
-		var aiInit = new AnnotationInstanceInitialiser();
-		var ai = this.createMinimalAI(new String[] {"nsai"}, "an");
-		Assertions.assertTrue(aiInit.setAnnotationParameter(ai, this.createSingleAnnoParam(result)));
-		
 		return result;
 	}
 	
@@ -35,11 +30,8 @@ UsesAnnotationInstances, UsesAnnotationParameters {
 	public void testTarget(IElementReferenceInitialiser init) {
 		this.setResourceFileTestIdentifier("testTarget");
 		
-		var cls1 = this.createMinimalClassWithPac("cls1", new String[] {"ns1"});
-		var cls2 = this.createMinimalClassWithPac("cls2", new String[] {"ns2"});
-		
-		var objOne = this.initElement(init, cls1.getPackage(), cls1);
-		var objTwo = this.initElement(init, cls2.getPackage(), cls2);
+		var objOne = this.initElement(init, this.createMinimalClass("cls1"), null);
+		var objTwo = this.initElement(init, this.createMinimalClass("cls2"), null);
 		
 		this.testX(objOne, objTwo, ReferencesPackage.Literals.ELEMENT_REFERENCE__TARGET);
 	}
@@ -49,8 +41,8 @@ UsesAnnotationInstances, UsesAnnotationParameters {
 	public void testContainedTarget(IElementReferenceInitialiser init) {
 		this.setResourceFileTestIdentifier("testContainedTarget");
 		
-		var objOne = this.initElement(init, null, this.createMinimalClassWithPac("cls1", new String[] {"ns1"}));
-		var objTwo = this.initElement(init, null, this.createMinimalClassWithPac("cls2", new String[] {"ns2"}));
+		var objOne = this.initElement(init, null, this.createMinimalClass("cls1"));
+		var objTwo = this.initElement(init, null, this.createMinimalClass("cls2"));
 		
 		this.testX(objOne, objTwo, ReferencesPackage.Literals.ELEMENT_REFERENCE__CONTAINED_TARGET);
 	}
