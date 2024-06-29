04ad85678a92f2659c79649b874f4ba9caf9268f
AlpTorac <a.torac@hotmail.com>
2024-06-29 21:19:58 +0200

    Implement LambdaExpressionTest as implementation test

--- commit-based-cipm/fi-tests/cipm.consistency.fitests/src/cipm/consistency/fitests/similarity/java/unittests/impltests/LambdaExpressionTest.java
+++ commit-based-cipm/fi-tests/cipm.consistency.fitests/src/cipm/consistency/fitests/similarity/java/unittests/impltests/LambdaExpressionTest.java
@@ -1,22 +1,21 @@
-package cipm.consistency.fitests.similarity.java.unittests.interfacetests;
+package cipm.consistency.fitests.similarity.java.unittests.impltests;
 
 import org.emftext.language.java.expressions.ExpressionsPackage;
 import org.emftext.language.java.expressions.LambdaBody;
 import org.emftext.language.java.expressions.LambdaExpression;
 import org.emftext.language.java.expressions.LambdaParameters;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ArgumentsSource;
+import org.junit.jupiter.api.Test;
 
 import cipm.consistency.fitests.similarity.java.EObjectSimilarityTest;
-import cipm.consistency.fitests.similarity.java.initialiser.expressions.ILambdaExpressionInitialiser;
+import cipm.consistency.fitests.similarity.java.initialiser.expressions.LambdaExpressionInitialiser;
 import cipm.consistency.fitests.similarity.java.unittests.UsesLambdaParameters;
 import cipm.consistency.fitests.similarity.java.unittests.UsesStatements;
 
 public class LambdaExpressionTest extends EObjectSimilarityTest
 	implements UsesStatements, UsesLambdaParameters {
-	protected LambdaExpression initElement(ILambdaExpressionInitialiser init,
-			LambdaBody body, LambdaParameters param) {
+	protected LambdaExpression initElement(LambdaBody body, LambdaParameters param) {
+		var init = new LambdaExpressionInitialiser();
 		LambdaExpression result = init.instantiate();
 		Assertions.assertTrue(init.minimalInitialisation(result));
 		Assertions.assertTrue(init.setBody(result, body));
@@ -24,24 +23,22 @@ public class LambdaExpressionTest extends EObjectSimilarityTest
 		return result;
 	}
 	
-	@ParameterizedTest
-	@ArgumentsSource(LambdaExpressionTestParams.class)
-	public void testBody(ILambdaExpressionInitialiser init) {
+	@Test
+	public void testBody() {
 		this.setResourceFileTestIdentifier("testBody");
 		
-		var objOne = this.initElement(init, this.createMinimalBlockWithNullReturn(), null);
-		var objTwo = this.initElement(init, this.createMinimalBlockWithTrivialAssert(), null);
+		var objOne = this.initElement(this.createMinimalBlockWithNullReturn(), null);
+		var objTwo = this.initElement(this.createMinimalBlockWithTrivialAssert(), null);
 		
 		this.testX(objOne, objTwo, ExpressionsPackage.Literals.LAMBDA_EXPRESSION__BODY);
 	}
 	
-	@ParameterizedTest
-	@ArgumentsSource(LambdaExpressionTestParams.class)
-	public void testParameters(ILambdaExpressionInitialiser init) {
+	@Test
+	public void testParameters() {
 		this.setResourceFileTestIdentifier("testParameters");
 		
-		var objOne = this.initElement(init, null, this.createMinimalETLP("p1", "c1"));
-		var objTwo = this.initElement(init, null, this.createMinimalETLP("p2", "c2"));
+		var objOne = this.initElement(null, this.createMinimalETLP("p1", "c1"));
+		var objTwo = this.initElement(null, this.createMinimalETLP("p2", "c2"));
 		
 		this.testX(objOne, objTwo, ExpressionsPackage.Literals.LAMBDA_EXPRESSION__PARAMETERS);
 	}
