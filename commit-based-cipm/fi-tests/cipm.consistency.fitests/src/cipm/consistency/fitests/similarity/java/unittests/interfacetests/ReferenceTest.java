6b45ac62af8a732a1a943fcc7e76b0153ae559db
AlpTorac <a.torac@hotmail.com>
2024-06-29 21:41:25 +0200

    Add important FIXME

---
+++ commit-based-cipm/fi-tests/cipm.consistency.fitests/src/cipm/consistency/fitests/similarity/java/unittests/interfacetests/ReferenceTest.java
@@ -0,0 +1,7 @@
+package cipm.consistency.fitests.similarity.java.unittests.interfacetests;
+
+import cipm.consistency.fitests.similarity.java.EObjectSimilarityTest;
+
+public class ReferenceTest extends EObjectSimilarityTest {
+	// FIXME: Implement
+}
