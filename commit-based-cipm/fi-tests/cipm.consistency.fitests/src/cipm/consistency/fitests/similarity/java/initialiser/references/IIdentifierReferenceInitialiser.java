0457173b8827b0c9ae35eeeec67c3e88d6628acf
AlpTorac <a.torac@hotmail.com>
2024-07-01 11:39:41 +0200

    Override method in IIdentifierReferenceInitialiser

--- commit-based-cipm/fi-tests/cipm.consistency.fitests/src/cipm/consistency/fitests/similarity/java/initialiser/references/IIdentifierReferenceInitialiser.java
+++ commit-based-cipm/fi-tests/cipm.consistency.fitests/src/cipm/consistency/fitests/similarity/java/initialiser/references/IIdentifierReferenceInitialiser.java
@@ -1,11 +1,30 @@
 package cipm.consistency.fitests.similarity.java.initialiser.references;
 
+import org.eclipse.emf.ecore.EObject;
+import org.emftext.language.java.references.IdentifierReference;
+
 import cipm.consistency.fitests.similarity.java.initialiser.annotations.IAnnotableInitialiser;
 import cipm.consistency.fitests.similarity.java.initialiser.arrays.IArrayTypeableInitialiser;
+import cipm.consistency.fitests.similarity.java.initialiser.instantiations.ExplicitConstructorCallInitialiser;
 
 public interface IIdentifierReferenceInitialiser extends IAnnotableInitialiser,
 	IArrayTypeableInitialiser,
 	IElementReferenceInitialiser
 {
-
+	@Override
+	public default EObject minimalInitialisationWithContainer(EObject obj) {
+		// FIXME: See if this can go to a helper interface
+		var castedO = (IdentifierReference) obj;
+		IAnnotableInitialiser.super.minimalInitialisation(castedO);
+		
+		if (castedO.eContainer() == null) {
+			var insInit = new ExplicitConstructorCallInitialiser();
+			var ecc = insInit.instantiate();
+			insInit.minimalInitialisation(ecc);
+			insInit.addArgument(ecc, castedO);
+			castedO.eContainer().equals(ecc);
+		}
+		
+		return castedO.eContainer();
+	}
 }
