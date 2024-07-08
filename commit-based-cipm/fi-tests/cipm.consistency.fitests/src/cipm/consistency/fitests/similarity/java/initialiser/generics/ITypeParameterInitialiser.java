455e3e61e408c2eece585a284ba02ed693adf43b
AlpTorac <a.torac@hotmail.com>
2024-07-08 13:43:05 +0200

    Override import adding in ITypeParameterInitialiser

--- commit-based-cipm/fi-tests/cipm.consistency.fitests/src/cipm/consistency/fitests/similarity/java/initialiser/generics/ITypeParameterInitialiser.java
+++ commit-based-cipm/fi-tests/cipm.consistency.fitests/src/cipm/consistency/fitests/similarity/java/initialiser/generics/ITypeParameterInitialiser.java
@@ -1,6 +1,8 @@
 package cipm.consistency.fitests.similarity.java.initialiser.generics;
 
+import org.emftext.language.java.classifiers.Classifier;
 import org.emftext.language.java.generics.TypeParameter;
+import org.emftext.language.java.imports.Import;
 import org.emftext.language.java.types.TypeReference;
 
 import cipm.consistency.fitests.similarity.java.initialiser.ModificationMethod;
@@ -18,7 +20,31 @@ public interface ITypeParameterInitialiser extends IClassifierInitialiser, IAnno
 		}
 		return true;
 	}
-	
+    
+    // FIXME: Find out if imports can somehow be added to TypeParameter
+    
+    /**
+     * {@link TypeParameter} cannot add imports to its container, so attempting
+     * to add imports to it has no effect.
+     * 
+     * This is caused by the inconsistency in the {@link Classifier} sub-hierarchy.
+     */
+    @Override
+    public default boolean addImport(Classifier cls, Import imp) {
+    	return true;
+    }
+    
+    /**
+     * {@link TypeParameter} cannot add imports to its container, so attempting
+     * to add imports to it has no effect.
+     * 
+     * This is caused by the inconsistency in the {@link Classifier} sub-hierarchy.
+     */
+    @Override
+    public default boolean addPackageImport(Classifier cls, Import imp) {
+    	return true;
+    }
+    
 	public default boolean addExtendTypes(TypeParameter tp, TypeReference[] trefs) {
 		return this.addXs(tp, trefs, this::addExtendType);
 	}
