cb078c8870657553d78f9cf88e109670429c3b59
AlpTorac <a.torac@hotmail.com>
2024-07-08 13:34:59 +0200

    Add initElement method
    to make object creation more visible

--- commit-based-cipm/fi-tests/cipm.consistency.fitests/src/cipm/consistency/fitests/similarity/java/unittests/interfacetests/ImportingElementTest.java
+++ commit-based-cipm/fi-tests/cipm.consistency.fitests/src/cipm/consistency/fitests/similarity/java/unittests/interfacetests/ImportingElementTest.java
@@ -1,8 +1,10 @@
 package cipm.consistency.fitests.similarity.java.unittests.interfacetests;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ArgumentsSource;
-
+import org.emftext.language.java.imports.Import;
+import org.emftext.language.java.imports.ImportingElement;
 import org.emftext.language.java.imports.ImportsPackage;
 
 import cipm.consistency.fitests.similarity.java.EObjectSimilarityTest;
@@ -10,13 +12,21 @@ import cipm.consistency.fitests.similarity.java.initialiser.imports.IImportingEl
 import cipm.consistency.fitests.similarity.java.unittests.UsesImportingElements;
 
 public class ImportingElementTest extends EObjectSimilarityTest implements UsesImportingElements {
+	protected ImportingElement initElement(IImportingElementInitialiser init,
+			Import[] imps) {
+		ImportingElement result = init.instantiate();
+		Assertions.assertTrue(init.initialise(result));
+		Assertions.assertTrue(init.addImports(result, imps));
+		return result;
+	}
+	
 	@ParameterizedTest
 	@ArgumentsSource(ImportingElementTestParams.class)
 	public void testImports(IImportingElementInitialiser init) {
 		this.setResourceFileTestIdentifier("testImports");
 		
-		var objOne = this.createMinimalImportingElement(init, "cls1");
-		var objTwo = this.createMinimalImportingElement(init, "cls2");
+		var objOne = this.initElement(init, new Import[] {this.createMinimalClsImport("cls1")});
+		var objTwo = this.initElement(init, new Import[] {this.createMinimalClsImport("cls2")});
 		
 		this.testX(objOne, objTwo, ImportsPackage.Literals.IMPORTING_ELEMENT__IMPORTS);
 	}
