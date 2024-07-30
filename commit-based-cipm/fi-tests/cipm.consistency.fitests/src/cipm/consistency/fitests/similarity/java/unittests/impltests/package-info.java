5c0471c1b6c055e37c2fe0d3b4e705ab3efc622d
AlpTorac <a.torac@hotmail.com>
2024-07-30 17:56:11 +0200

    Add package documentations

---
+++ commit-based-cipm/fi-tests/cipm.consistency.fitests/src/cipm/consistency/fitests/similarity/java/unittests/impltests/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * Contains unit tests for {@link EObject} classes/interfaces, which are
+ * terminal in the {@link Commentable} type hierarchy, i.e. they have no further
+ * sub-classes/-interfaces. In the said tests, {@link EObject} instances are
+ * constructed programmatically and are checked for similarity. <br>
+ * <br>
+ * It is highly recommended to make the construction of the "main"
+ * {@link EObject} instances as obvious and visible as possible, as their
+ * construction can get complicated and not knowing all the construction steps
+ * can lead to tests not fulfilling their goal.
+ */
+package cipm.consistency.fitests.similarity.java.unittests.impltests;
