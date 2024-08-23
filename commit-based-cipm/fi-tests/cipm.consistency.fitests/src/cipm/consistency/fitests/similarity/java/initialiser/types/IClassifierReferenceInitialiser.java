7fbb1919427861b72f9e20a7ed663f881e74e68d
AlpTorac <a.torac@hotmail.com>
2024-08-23 18:02:12 +0200

    Fix and refactor TypeReference initialisers

--- commit-based-cipm/fi-tests/cipm.consistency.fitests/src/cipm/consistency/fitests/similarity/java/initialiser/types/IClassifierReferenceInitialiser.java
+++ commit-based-cipm/fi-tests/cipm.consistency.fitests/src/cipm/consistency/fitests/similarity/java/initialiser/types/IClassifierReferenceInitialiser.java
@@ -1,6 +1,7 @@
 package cipm.consistency.fitests.similarity.java.initialiser.types;
 
 import org.emftext.language.java.types.ClassifierReference;
+import org.emftext.language.java.types.TypeReference;
 
 import cipm.consistency.fitests.similarity.java.initialiser.annotations.IAnnotableInitialiser;
 import cipm.consistency.fitests.similarity.java.initialiser.generics.ITypeArgumentableInitialiser;
@@ -9,4 +10,9 @@ public interface IClassifierReferenceInitialiser
 		extends ITypeArgumentableInitialiser, IAnnotableInitialiser, ITypeReferenceInitialiser {
 	@Override
 	public ClassifierReference instantiate();
+
+	@Override
+	public default boolean canSetTarget(TypeReference tref) {
+		return true;
+	}
 }
