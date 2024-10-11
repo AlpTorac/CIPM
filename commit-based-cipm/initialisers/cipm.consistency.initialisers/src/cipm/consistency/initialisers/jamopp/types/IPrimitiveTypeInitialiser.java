fc23f272edf2471e35eff62783d708a894fc4aff
AlpTorac <a.torac@hotmail.com>
2024-10-11 19:21:58 +0200

    Add commentary to IPrimitiveTypeInitialiser
    Override method to provide commentary

--- commit-based-cipm/initialisers/cipm.consistency.initialisers/src/cipm/consistency/initialisers/jamopp/types/IPrimitiveTypeInitialiser.java
+++ commit-based-cipm/initialisers/cipm.consistency.initialisers/src/cipm/consistency/initialisers/jamopp/types/IPrimitiveTypeInitialiser.java
@@ -1,5 +1,6 @@
 package cipm.consistency.initialisers.jamopp.types;
 
+import org.emftext.language.java.classifiers.Classifier;
 import org.emftext.language.java.types.PrimitiveType;
 import org.emftext.language.java.types.TypeReference;
 
@@ -8,7 +9,29 @@ import cipm.consistency.initialisers.jamopp.annotations.IAnnotableInitialiser;
 public interface IPrimitiveTypeInitialiser extends IAnnotableInitialiser, ITypeInitialiser, ITypeReferenceInitialiser {
 	@Override
 	public PrimitiveType instantiate();
-	
+
+	/**
+	 * {@inheritDoc} <br>
+	 * <br>
+	 * In the case of {@link PrimitiveType}, this method does nothing, since the
+	 * target attribute of primitive types ({@code tref.getTarget()}) are (as their
+	 * name imply) primitives and thus not adjustable. <br>
+	 * <br>
+	 * This method is overridden to provide commentary, it introduces no changes to
+	 * its super version in {@link ITypeReferenceInitialiser}. <br>
+	 * <br>
+	 */
+	@Override
+	public default boolean setTarget(TypeReference tref, Classifier target) {
+		return ITypeReferenceInitialiser.super.setTarget(tref, target);
+	}
+
+	/**
+	 * {@inheritDoc} <br>
+	 * <br>
+	 * Since the target attribute of primitive types ({@code tref.getTarget()}) are
+	 * (as their name imply) primitives and thus not adjustable.
+	 */
 	@Override
 	public default boolean canSetTarget(TypeReference tref) {
 		return false;
