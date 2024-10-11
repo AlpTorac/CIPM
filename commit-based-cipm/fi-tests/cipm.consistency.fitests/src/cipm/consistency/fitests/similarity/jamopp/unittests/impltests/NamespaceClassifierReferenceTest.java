cf0d5e778a542c606fe1576ecc9e27b96c42628c
AlpTorac <a.torac@hotmail.com>
2024-10-11 19:07:45 +0200

    Add missing test methods to NamespaceClassifierReference
    Adjust object construction method
    
    Adapt other test methods

--- commit-based-cipm/fi-tests/cipm.consistency.fitests/src/cipm/consistency/fitests/similarity/jamopp/unittests/impltests/NamespaceClassifierReferenceTest.java
+++ commit-based-cipm/fi-tests/cipm.consistency.fitests/src/cipm/consistency/fitests/similarity/jamopp/unittests/impltests/NamespaceClassifierReferenceTest.java
@@ -1,5 +1,6 @@
 package cipm.consistency.fitests.similarity.jamopp.unittests.impltests;
 
+import org.emftext.language.java.classifiers.Classifier;
 import org.emftext.language.java.types.ClassifierReference;
 import org.emftext.language.java.types.NamespaceClassifierReference;
 import org.emftext.language.java.types.TypesPackage;
@@ -8,20 +9,38 @@ import org.junit.jupiter.api.Test;
 
 import cipm.consistency.fitests.similarity.jamopp.AbstractJaMoPPSimilarityTest;
 import cipm.consistency.fitests.similarity.jamopp.unittests.UsesTypeReferences;
+import cipm.consistency.initialisers.jamopp.types.ClassifierReferenceInitialiser;
 import cipm.consistency.initialisers.jamopp.types.NamespaceClassifierReferenceInitialiser;
 
 public class NamespaceClassifierReferenceTest extends AbstractJaMoPPSimilarityTest implements UsesTypeReferences {
-	protected NamespaceClassifierReference initElement(ClassifierReference[] clsRefs) {
+	protected NamespaceClassifierReference initElement(Classifier target, ClassifierReference[] clsRefs) {
 		var ncrInit = new NamespaceClassifierReferenceInitialiser();
 		var ncr = ncrInit.instantiate();
+		Assertions.assertEquals(ncrInit.canSetTargetTo(ncr, target), ncrInit.setTarget(ncr, target));
 		Assertions.assertTrue(ncrInit.addClassifierReferences(ncr, clsRefs));
 		return ncr;
 	}
 
 	@Test
+	public void testTarget() {
+		var objOne = this.initElement(this.createMinimalClassWithCU("cls1"), null);
+		var objTwo = this.initElement(this.createMinimalClassWithCU("cls2"), null);
+
+		this.testSimilarity(objOne, objTwo, NamespaceClassifierReference.class,
+				TypesPackage.Literals.CLASSIFIER_REFERENCE__TARGET);
+	}
+
+	@Test
+	public void testTargetNullCheck() {
+		this.testSimilarityNullCheck(this.initElement(this.createMinimalClassWithCU("cls1"), null),
+				new ClassifierReferenceInitialiser(), false, NamespaceClassifierReference.class,
+				TypesPackage.Literals.CLASSIFIER_REFERENCE__TARGET);
+	}
+
+	@Test
 	public void testClassifierReference() {
-		var objOne = this.initElement(new ClassifierReference[] { this.createMinimalClsRef("cls1") });
-		var objTwo = this.initElement(new ClassifierReference[] { this.createMinimalClsRef("cls2") });
+		var objOne = this.initElement(null, new ClassifierReference[] { this.createMinimalClsRef("cls1") });
+		var objTwo = this.initElement(null, new ClassifierReference[] { this.createMinimalClsRef("cls2") });
 
 		this.testSimilarity(objOne, objTwo,
 				TypesPackage.Literals.NAMESPACE_CLASSIFIER_REFERENCE__CLASSIFIER_REFERENCES);
@@ -29,9 +48,9 @@ public class NamespaceClassifierReferenceTest extends AbstractJaMoPPSimilarityTe
 
 	@Test
 	public void testClassifierReferenceSize() {
-		var objOne = this.initElement(
+		var objOne = this.initElement(null,
 				new ClassifierReference[] { this.createMinimalClsRef("cls1"), this.createMinimalClsRef("cls2") });
-		var objTwo = this.initElement(new ClassifierReference[] { this.createMinimalClsRef("cls1") });
+		var objTwo = this.initElement(null, new ClassifierReference[] { this.createMinimalClsRef("cls1") });
 
 		this.testSimilarity(objOne, objTwo,
 				TypesPackage.Literals.NAMESPACE_CLASSIFIER_REFERENCE__CLASSIFIER_REFERENCES);
@@ -39,7 +58,8 @@ public class NamespaceClassifierReferenceTest extends AbstractJaMoPPSimilarityTe
 
 	@Test
 	public void testClassifierReferenceNullCheck() {
-		this.testSimilarityNullCheck(this.initElement(new ClassifierReference[] { this.createMinimalClsRef("cls1") }),
+		this.testSimilarityNullCheck(
+				this.initElement(null, new ClassifierReference[] { this.createMinimalClsRef("cls1") }),
 				new NamespaceClassifierReferenceInitialiser(), false,
 				TypesPackage.Literals.NAMESPACE_CLASSIFIER_REFERENCE__CLASSIFIER_REFERENCES);
 	}
