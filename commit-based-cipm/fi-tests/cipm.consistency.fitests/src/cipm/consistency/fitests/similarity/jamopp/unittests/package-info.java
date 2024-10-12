420e93c54811a6276d35ab5a168e515617f33f45
AlpTorac <a.torac@hotmail.com>
2024-10-12 20:20:01 +0200

    Fix and improve commentary

--- commit-based-cipm/fi-tests/cipm.consistency.fitests/src/cipm/consistency/fitests/similarity/jamopp/unittests/package-info.java
+++ commit-based-cipm/fi-tests/cipm.consistency.fitests/src/cipm/consistency/fitests/similarity/jamopp/unittests/package-info.java
@@ -1,21 +1,21 @@
 /**
  * Contains interfaces that can be implemented by tests, where initialisers are
- * used to instantiate {@link EObject} implementors present in EMFText. Each
+ * used to instantiate {@link EObject} implementors present in JaMoPP. Each
  * initialiser in this package offers default methods that help encapsulate the
- * creation of such {@link EObject} instances, which are needed in tests. <br>
+ * creation of such EObject instances, which are needed in tests. <br>
  * <br>
  * It is recommended to only use the interfaces in this package to instantiate
- * {@link EObject} instances that are needed to setup the "main" {@link EObject}
- * instances under test (i.e. instances used in similarity checking methods).
- * The creation of the "main" EObject instances should be done within the
- * corresponding test class explicitly via initialisers, in order to avoid
- * implicit steps as much as possible. <br>
+ * EObject instances that are needed to setup the "main" EObject instances under
+ * test (i.e. instances used in similarity checking methods). The creation of
+ * the "main" EObject instances should be done within the corresponding test
+ * class explicitly via initialisers, in order to avoid implicit steps as much
+ * as possible. <br>
  * <br>
  * While adding further interfaces to this package, it is suggested to check the
  * existing interfaces and to extend them, if possible. Doing so will help keep
- * consistency across test cases and may spare re-inventing the wheel. <br>
+ * consistency across test cases. <br>
  * <br>
- * The sub-packages of this package contain unit tests for {@link EObject}
+ * The sub-packages of this package contain unit tests for EObject
  * classes/interfaces using initialisers. For each attribute X of EObject
  * implementor/interface EO, there are currently up to 3 test categories. Let
  * eo1 and eo2 be instances of type EO, then test categories do the following:
@@ -27,9 +27,10 @@
  * of one eo contains 2 elements elem1 and elem2 (elem1 and elem2 are NOT
  * similar), whereas the X of other eo only contains elem1. These test methods
  * make sure that array-valued attributes are compared accordingly.
- * <li><b>testXNullCheck</b>: Same as testX, where eo1's X is set and eo2's X is
- * not. These test methods ensure that no exceptions are thrown while performing
- * similarity checking on EObject instances that are not initialised properly.
+ * <li><b>testXNullCheck</b>: Same as testX, where eo1's X attribute is set and
+ * eo2's X attribute is not. These test methods ensure that no exceptions are
+ * thrown while performing similarity checking on EObject instances that are not
+ * initialised properly.
  * </ul>
  * <b>Note that multiple similarity checks are performed in each test, in order
  * to ensure that similarity checking is symmetric and that similarity checking
