d8936216519ea7c6ca02f773d9185c9bc5ba61aa
AlpTorac <a.torac@hotmail.com>
2024-10-13 19:26:43 +0200

    Adjust pre-conditions of adding statements to statement list containers
    Add commentary

--- commit-based-cipm/initialisers/cipm.consistency.initialisers/src/cipm/consistency/initialisers/jamopp/statements/IStatementListContainerInitialiser.java
+++ commit-based-cipm/initialisers/cipm.consistency.initialisers/src/cipm/consistency/initialisers/jamopp/statements/IStatementListContainerInitialiser.java
@@ -5,25 +5,38 @@ import org.emftext.language.java.statements.StatementListContainer;
 
 import cipm.consistency.initialisers.jamopp.commons.ICommentableInitialiser;
 
+/**
+ * An interface meant to be implemented by initialisers, which are supposed to
+ * create {@link StatementListContainer} instances. <br>
+ * <br>
+ * Not all implementors of {@link StatementListContainer} allow adding
+ * statements via {@code .getStatements().add(...)} as expected. Attempting to
+ * add statements that way can result in exceptions under circumstances. The
+ * method {@link #canContainStatements(StatementListContainer)} can be used to
+ * determine, whether this is possible.
+ * 
+ * @author Alp Torac Genc
+ */
 public interface IStatementListContainerInitialiser extends ICommentableInitialiser {
 	@Override
 	public StatementListContainer instantiate();
 
 	/**
-	 * Attempts to add the given {@link Statement} to the given
-	 * {@link StatementListContainer}. <br>
+	 * Attempts to add the given {@link Statement} st to the given
+	 * {@link StatementListContainer} slc. Uses {@code slc.getStatements().add(st)}
+	 * to do so. <br>
 	 * <br>
-	 * <b>Note: Not all implementors of {@link StatementListContainer} allow adding
-	 * statements via this method.</b>
+	 * Note: Attempting to add a null statement will still return true, since there
+	 * is no modification to be performed.
 	 * 
-	 * @return True, if the given {@link Statement} was added successfully.
+	 * @see {@link IStatementListContainerInitialiser}
 	 * @see {@link #addStatementAssertion(StatementListContainer, Statement)}
 	 */
 	public default boolean addStatement(StatementListContainer slc, Statement st) {
-		if (!this.canContainStatements(slc)) {
-			return false;
-		}
 		if (st != null) {
+			if (!this.canContainStatements(slc)) {
+				return false;
+			}
 			slc.getStatements().add(st);
 			return slc.getStatements().contains(st);
 		}
@@ -33,10 +46,10 @@ public interface IStatementListContainerInitialiser extends ICommentableInitiali
 	/**
 	 * Extracted from {@link #addStatement(StatementListContainer, Statement)}
 	 * because there are some implementors of {@link StatementListContainer}, which
-	 * throw {@link NullPointerException} if {@code slc.getStatements()} is called
-	 * without proper initialisation. Extracting this method allows such
-	 * implementors to indicate, whether {@link Statement}s can be added to them
-	 * without issues.
+	 * throw {@link NullPointerException} if {@code slc.getStatements().add(...)} is
+	 * called without proper initialisation. Extracting this method allows such
+	 * implementors to indicate, whether {@link Statement}s can be added to them via
+	 * {@link #addStatement(StatementListContainer, Statement)} without issues.
 	 * 
 	 * @return Whether {@link Statement}s can be added to the given
 	 *         {@link StatementListContainer} via
