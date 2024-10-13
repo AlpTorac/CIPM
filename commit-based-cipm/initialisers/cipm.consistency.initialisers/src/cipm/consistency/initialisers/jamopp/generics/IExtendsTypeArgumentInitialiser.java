c849640520d6fddbf04970eaf042be9b70ca81d8
AlpTorac <a.torac@hotmail.com>
2024-10-13 19:24:35 +0200

    Add commentary

--- commit-based-cipm/initialisers/cipm.consistency.initialisers/src/cipm/consistency/initialisers/jamopp/generics/IExtendsTypeArgumentInitialiser.java
+++ commit-based-cipm/initialisers/cipm.consistency.initialisers/src/cipm/consistency/initialisers/jamopp/generics/IExtendsTypeArgumentInitialiser.java
@@ -5,10 +5,25 @@ import org.emftext.language.java.types.TypeReference;
 
 import cipm.consistency.initialisers.jamopp.annotations.IAnnotableInitialiser;
 
+/**
+ * An interface meant to be implemented by initialisers, which are supposed to
+ * create {@link ExtendsTypeArgument} instances. <br>
+ * <br>
+ * For an {@link ExtendsTypeArgument} instance eta,
+ * {@code eta.getExtendTypes().add(...)} does not modify eta.
+ * 
+ * @author Alp Torac Genc
+ */
 public interface IExtendsTypeArgumentInitialiser extends IAnnotableInitialiser, ITypeArgumentInitialiser {
 	@Override
 	public ExtendsTypeArgument instantiate();
 
+	/**
+	 * Sets the extend type of the given {@link ExtendsTypeArgument} to extType.
+	 * Uses {@code eta.setExtendType(...)} to do so.
+	 * 
+	 * @see {@link IExtendsTypeArgumentInitialiser}
+	 */
 	public default boolean setExtendType(ExtendsTypeArgument eta, TypeReference extType) {
 		if (extType != null) {
 			eta.setExtendType(extType);
