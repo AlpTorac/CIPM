d5d6dfa5d80491e0c9c6c875368dbb148cbe1f01
AlpTorac <a.torac@hotmail.com>
2024-10-13 14:47:34 +0200

    Rename parameter
    Improve commentary

--- commit-based-cipm/initialisers/cipm.consistency.initialisers/src/cipm/consistency/initialisers/IInitialiser.java
+++ commit-based-cipm/initialisers/cipm.consistency.initialisers/src/cipm/consistency/initialisers/IInitialiser.java
@@ -4,8 +4,9 @@ import java.util.function.BiFunction;
 
 /**
  * An interface to be implemented by initialisers. Initialisers are interfaces
- * or classes, which are meant to instantiate objects ({@link #instantiate()}).
- * For intuition, their names can be used to denote what they instantiate. <br>
+ * or classes, which are meant to instantiate objects (designated objects) via
+ * {@link #instantiate()}. For intuition, their names can be used to denote what
+ * they instantiate. <br>
  * <br>
  * Initialisers can also implement (default) methods that modify their
  * designated objects. It is suggested to declare the return types of such
@@ -14,30 +15,37 @@ import java.util.function.BiFunction;
  * it was meant to do. It is also possible to extract this behaviour into
  * additional assertion methods. <br>
  * <br>
- * <b>Modification methods DO NOT check, if the object that is being modified is
- * null. Attempting to modify null objects will result in EXCEPTIONS being
- * thrown. They only have null checks for the other passed parameters, which are
- * used to perform the modifications.</b> The reason behind this is that it is
- * important to know, if a null object is attempted to be modified, since this
- * should never happen.<br>
+ * <b>Modification methods DO NOT check, if the object that is being modified
+ * (modification target) is null. Attempting to modify null objects will result
+ * in EXCEPTIONS being thrown. They only have null checks for the other passed
+ * parameters, which are used to perform the modifications. If those parameters
+ * are null, no modification will be performed and the return value of the said
+ * methods will be true, since there is no modification to fail.</b> It is
+ * important to know if the modification target is null, as this could hint
+ * towards faulty code. Therefore there are no null checks for the modification
+ * target. On the other hand, there are null checks for other modification
+ * parameters. This is for the sake of sparing null checks in object
+ * construction.<br>
  * <br>
  * It is recommended to separate instantiation and initialisation (modification)
- * methods, as doing so will allow using the individual methods in
- * sub-interfaces and sub-classes. Implementing initialisers similar to the way
- * the objects are implemented, which they instantiate, may make initialisers
- * more flexible and ease implementing them. <br>
+ * methods, as doing so will allow using the individual methods in sub-types.
+ * Implementing initialisers similar to their designated objects, in terms of
+ * type hierarchy and what modifications they allow from outside, may make
+ * initialisers more flexible and ease implementing them. <br>
  * <br>
  * A further suggestion is to not declare attributes in the concrete
  * implementors, so that all functionality is present in form of methods. This
  * alleviates having to declare unnecessary attributes in sub-types and makes
- * overriding behaviour easier. If initialisation of the instantiated objects is
- * complex, consider realising it in form of initialiser adaptation strategies.
- * <br>
+ * overriding behaviour easier, since default methods can be overridden in
+ * interfaces. If initialisation of the designated objects is complex, consider
+ * realising it in form of initialiser adaptation strategies (see the links
+ * below). <br>
  * <br>
  * This interface also contains some static utility methods.
  * 
  * @author Alp Torac Genc
  * @see {@link IInitialiserBase}
+ * @see {@link IInitialiserAdapterStrategy}
  */
 public interface IInitialiser {
 	/**
@@ -65,8 +73,9 @@ public interface IInitialiser {
 	public boolean initialise(Object obj);
 
 	/**
-	 * Checks whether a given {@link IInitialiser} class directly declares any
-	 * methods that modify given object instances.
+	 * Checks whether a given {@link IInitialiser} type directly declares any
+	 * methods that modify given object instances. Returns false, if
+	 * {@code initCls == null}.
 	 */
 	public static boolean declaresModificationMethods(Class<? extends IInitialiser> initCls) {
 		if (initCls == null) {
@@ -110,16 +119,21 @@ public interface IInitialiser {
 	}
 
 	/**
+	 * An initialiser class is assumed to be able to instantiate the class objClass,
+	 * if it has a method (instantiation method), whose return type is objClass and
+	 * which has no parameters. Methods inherited by initCls will also be
+	 * inspected.<br>
+	 * <br>
+	 * For the result to be true, initCls has to be able to instantiate
+	 * <i><b>exactly</b></i> objClass, i.e. the return type of the instantiation
+	 * method has to be <i><b>exactly</b></i> objClass. <br>
+	 * <br>
+	 * Methods that are generated internally by Java (synthetic methods and bridge
+	 * methods) are excluded.
+	 * 
 	 * @return True, if initCls is an initialiser type, which is meant to
-	 *         instantiate objects of class objClass. An initialiser class is
-	 *         assumed to be able to instantiate the class objClass, if it has a
-	 *         method (instantiation method), whose return type is objClass and
-	 *         which has no parameters. Methods inherited by initCls will also be
-	 *         inspected. <br>
-	 *         <br>
-	 *         For the result to be true, initCls has to be able to instantiate
-	 *         <i><b>exactly</b></i> objClass, i.e. the return type of the
-	 *         instantiation method has to be <i><b>exactly</b></i> objClass.
+	 *         instantiate objects of class objClass (sub-types of objClass do not
+	 *         count).
 	 */
 	public static boolean isInitialiserFor(Class<? extends IInitialiser> initCls, Class<?> objClass) {
 		if (initCls == null || objClass == null) {
@@ -153,7 +167,7 @@ public interface IInitialiser {
 
 	/**
 	 * A variant of {@link #isInitialiserFor(Class, Class)}, where initCls is
-	 * extracted from init.
+	 * extracted from init. Returns false, if any parameter is null.
 	 */
 	public static boolean isInitialiserFor(IInitialiser init, Class<?> objClass) {
 		return init != null && objClass != null && isInitialiserFor(init.getClass(), objClass);
@@ -171,7 +185,7 @@ public interface IInitialiser {
 	/**
 	 * The dynamic variant of {@link #isInitialiserFor(IInitialiser, Class)}. <br>
 	 * <br>
-	 * Uses this initialiser as init.
+	 * Uses this initialiser as the initialiser parameter.
 	 */
 	public default boolean isInitialiserFor(Class<?> objClass) {
 		return objClass != null && isInitialiserFor(this, objClass);
@@ -179,26 +193,30 @@ public interface IInitialiser {
 
 	/**
 	 * A helper method for implementors, which provides them with a template for
-	 * versions of their methods, which take arrays of parameters rather than
-	 * singular ones, and perform multiple modifications. <br>
+	 * versions of their modification methods, which take arrays of parameters
+	 * rather than singular ones, and perform multiple modifications. The purpose of
+	 * this method is to help keep consistency across the said versions of the
+	 * modification methods.<br>
 	 * <br>
-	 * <b>If addFunction returns false for an element x of xs, the method will FAIL
-	 * EARLY and return false.</b> This means, addFunction WILL NOT be called for
-	 * the remaining xs once it fails. <br>
-	 * <br>
-	 * Because of this, it is important to perform modifications one by one, if
-	 * performing the said modifications is expected to fail for some x. <br>
+	 * <b>If modificationFunction returns false for an element x of xs, the method
+	 * will FAIL EARLY and return false.</b> This means, modificationFunction WILL
+	 * NOT be called for the remaining xs once it fails. Because of this, it is
+	 * important to perform modifications one by one, if performing the said
+	 * modifications is expected to fail for some x. <br>
 	 * <br>
 	 * This method is not intended to be used directly from outside.
 	 * 
-	 * @param <T>         The type of the object being modified
-	 * @param <X>         The parameter passed to the modification function
-	 *                    (addFunction)
-	 * @param obj         The object being modified. {@code obj == null} will cause
-	 *                    null pointer exceptions, if xs has at least one element.
-	 * @param xs          Array of parameters that will be passed to addFunction
-	 * @param addFunction The modification function that will be run on obj, using
-	 *                    xs as parameters (one addFunction call each x in xs)
+	 * @param <T>                  The type of the object being modified
+	 * @param <X>                  The parameter passed to the modification function
+	 *                             (modificationFunction)
+	 * @param obj                  The object being modified. {@code obj == null}
+	 *                             will cause null pointer exceptions, if xs has at
+	 *                             least one element.
+	 * @param xs                   Array of parameters that will be passed to
+	 *                             modificationFunction
+	 * @param modificationFunction The modification function that will be run on
+	 *                             obj, using xs as parameters (one
+	 *                             modificationFunction call each x in xs)
 	 * 
 	 * @return
 	 *         <ul>
@@ -214,10 +232,10 @@ public interface IInitialiser {
 	 *         </ul>
 	 */
 	public default <T extends Object, X extends Object> boolean doMultipleModifications(T obj, X[] xs,
-			BiFunction<T, X, Boolean> addFunction) {
+			BiFunction<T, X, Boolean> modificationFunction) {
 		if (xs != null) {
 			for (var x : xs) {
-				if (!addFunction.apply(obj, x))
+				if (!modificationFunction.apply(obj, x))
 					return false;
 			}
 		}
