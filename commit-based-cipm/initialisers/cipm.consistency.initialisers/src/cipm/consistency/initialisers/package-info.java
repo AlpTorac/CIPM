f90602e2d4054db24ca5b031f8f5204058c82560
AlpTorac <a.torac@hotmail.com>
2024-10-13 14:48:49 +0200

    Improve commentary

--- commit-based-cipm/initialisers/cipm.consistency.initialisers/src/cipm/consistency/initialisers/package-info.java
+++ commit-based-cipm/initialisers/cipm.consistency.initialisers/src/cipm/consistency/initialisers/package-info.java
@@ -3,24 +3,24 @@
  * {@link cipm.consistency.initialisers.IInitialiser} as well as for classes
  * that can be used to adapt them. Also has an interface
  * {@link cipm.consistency.initialisers.IInitialiserPackage}, which can be
- * implemented to access initialiser interfaces and classes easier. <br>
+ * implemented to access initialiser types and instances easier. <br>
  * <br>
  * Initialisers are classes/interfaces, whose purpose is to instantiate,
  * initialise and modify certain objects. Implementing initialisers similar to
  * their designated objects, may help making the initialisers more flexible and
  * can ease adding further initialisers later on. It is strongly recommended to
- * define atomic modification methods in initialisers and reusing them, rather
+ * define atomic modification methods in initialisers and re-using them, rather
  * than defining complex modification methods. <br>
  * <br>
- * In order to make parameterized tests using initialisers as parameters
+ * In order to make parameterised tests using initialisers as parameters
  * possible, one can use
- * {@link cipm.consistency.initialisers.IInitialiserAdapterStrategy} to augment
- * certain initialisers in ways that lets them be used in such tests without
- * throwing certain exceptions. The most common cause of such exceptions is
- * initialisers not setting any required attributes while instantiating
- * objects. In such cases, the said adapters can be used to have them set those
- * attributes, so that using the instances in tests do not throw exceptions, due
- * to their essential attributes not being set. <br>
+ * {@link cipm.consistency.initialisers.IInitialiserAdapterStrategy} to adapt
+ * certain initialisers. This way, they can be used in such tests without
+ * throwing exceptions and without the need of type checking. The most common
+ * cause of such exceptions is initialisers not setting the required attributes
+ * while instantiating objects. In such cases, the said adapters can be used to
+ * have them set those attributes, so that using the instances in tests do not
+ * throw exceptions, due to their essential attributes not being set. <br>
  * <br>
  * {@link cipm.consistency.initialisers.IInitialiserPackage} defines a nestable
  * structure that enables finding the proper initialisers as well as certain
