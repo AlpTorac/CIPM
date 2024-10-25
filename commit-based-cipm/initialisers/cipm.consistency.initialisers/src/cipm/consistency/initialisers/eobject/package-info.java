67a02ece914c66673a981c8f379787390a40f77e
AlpTorac <a.torac@hotmail.com>
2024-10-25 16:26:27 +0200

    Edit package commentary

--- commit-based-cipm/initialisers/cipm.consistency.initialisers/src/cipm/consistency/initialisers/eobject/package-info.java
+++ commit-based-cipm/initialisers/cipm.consistency.initialisers/src/cipm/consistency/initialisers/eobject/package-info.java
@@ -1,11 +1,5 @@
 /**
  * Contains the means to extend {@link IInitialiser} for {@link EObject}
- * implementors introduced with JaMoPP. Sub-packages contain concrete
- * implementations and interfaces of concrete initialisers for their
- * corresponding {@link EObject} implementor. <br>
- * <br>
- * TODO Extract this plug-in and move it to its own repository in the future, as
- * it can be re-used wherever {@link EObject} implementors have to be
- * programmatically created or modified.
+ * implementors.
  */
 package cipm.consistency.initialisers.eobject;
