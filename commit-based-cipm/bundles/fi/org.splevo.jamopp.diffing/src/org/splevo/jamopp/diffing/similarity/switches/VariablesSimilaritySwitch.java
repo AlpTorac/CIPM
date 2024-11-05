04e3c931b44caeb2af4304332afb08096ff09d16
AlpTorac <a.torac@hotmail.com>
2024-11-05 16:23:03 +0100

    (WIP) Improve commentary
    Review the commentary and improve further

--- commit-based-cipm/bundles/fi/org.splevo.jamopp.diffing/src/org/splevo/jamopp/diffing/similarity/switches/VariablesSimilaritySwitch.java
+++ commit-based-cipm/bundles/fi/org.splevo.jamopp.diffing/src/org/splevo/jamopp/diffing/similarity/switches/VariablesSimilaritySwitch.java
@@ -30,16 +30,12 @@ public class VariablesSimilaritySwitch extends VariablesSwitch<Boolean>
 		this.similaritySwitch = similaritySwitch;
 	}
 
-	/**
-	 * Check variable declaration similarity.<br>
-	 * Similarity is checked by
-	 * <ul>
-	 * <li>variable name</li>
-	 * <li>variable container (name space)</li>
-	 * </ul>
+    /**
+     * Checks the similarity of 2 variables. Similarity is checked by comparing their
+     * names ({@link Variable#getName()}).
 	 * 
 	 * @param var1 The variable declaration to compare with the compare element.
-	 * @return True/False if the variable declarations are similar or not.
+	 * @return False if names are not equal, true otherwise.
 	 */
 	@Override
 	public Boolean caseVariable(Variable var1) {
@@ -58,6 +54,13 @@ public class VariablesSimilaritySwitch extends VariablesSwitch<Boolean>
 		return Boolean.TRUE;
 	}
 
+    /**
+     * Checks the similarity of 2 additional local variables. Similarity is checked by comparing their
+     * names ({@link AdditionalLocalVariable#getName()}).
+	 * 
+	 * @param var1 The additional local variable to compare with the compare element.
+	 * @return False if names are not equal, true otherwise.
+	 */
 	@Override
 	public Boolean caseAdditionalLocalVariable(AdditionalLocalVariable var1) {
 		this.logMessage("caseAdditionalLocalVariable");
