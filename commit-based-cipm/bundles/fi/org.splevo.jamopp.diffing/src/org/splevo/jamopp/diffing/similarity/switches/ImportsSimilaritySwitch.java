b2d0a633d9e0f6fbfa7a19d991f0b804009da799
AlpTorac <a.torac@hotmail.com>
2024-11-06 00:08:26 +0100

    Use areSimilar(...) instead

--- commit-based-cipm/bundles/fi/org.splevo.jamopp.diffing/src/org/splevo/jamopp/diffing/similarity/switches/ImportsSimilaritySwitch.java
+++ commit-based-cipm/bundles/fi/org.splevo.jamopp.diffing/src/org/splevo/jamopp/diffing/similarity/switches/ImportsSimilaritySwitch.java
@@ -5,7 +5,6 @@ import org.emftext.language.java.imports.PackageImport;
 import org.emftext.language.java.imports.StaticClassifierImport;
 import org.emftext.language.java.imports.StaticMemberImport;
 import org.emftext.language.java.imports.util.ImportsSwitch;
-import org.emftext.language.java.references.ReferenceableElement;
 import org.splevo.jamopp.diffing.similarity.IJavaSimilaritySwitch;
 import org.splevo.jamopp.diffing.similarity.ILoggableJavaSwitch;
 import org.splevo.jamopp.diffing.similarity.base.ISimilarityRequestHandler;
@@ -89,22 +88,9 @@ public class ImportsSimilaritySwitch extends ImportsSwitch<Boolean>
 
 		var stMems1 = import1.getStaticMembers();
 		var stMems2 = import2.getStaticMembers();
-
-		// Null check to avoid NullPointerExceptions
-		if (stMems1 == null ^ stMems2 == null) {
+		var stMemsSimilarity = this.areSimilar(stMems1, stMems2);
+		if (JaMoPPBooleanUtil.isFalse(stMemsSimilarity)) {
 			return Boolean.FALSE;
-		} else if (stMems1 != null && stMems2 != null) {
-			if (stMems1.size() != stMems2.size()) {
-				return Boolean.FALSE;
-			}
-			for (int i = 0; i < stMems1.size(); i++) {
-				ReferenceableElement member1 = stMems1.get(i);
-				ReferenceableElement member2 = stMems2.get(i);
-				Boolean similarity = this.isSimilar(member1, member2);
-				if (JaMoPPBooleanUtil.isFalse(similarity)) {
-					return Boolean.FALSE;
-				}
-			}
 		}
 
 		String namespace1 = Strings.nullToEmpty(import1.getNamespacesAsString());
