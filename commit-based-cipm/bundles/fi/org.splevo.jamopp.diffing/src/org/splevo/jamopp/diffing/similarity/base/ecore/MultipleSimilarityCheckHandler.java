6d82414c2d97d16f6ef0073b312b9ffab2f57a83
AlpTorac <a.torac@hotmail.com>
2024-11-06 00:22:22 +0100

    Use utility classes instead

--- commit-based-cipm/bundles/fi/org.splevo.jamopp.diffing/src/org/splevo/jamopp/diffing/similarity/base/ecore/MultipleSimilarityCheckHandler.java
+++ commit-based-cipm/bundles/fi/org.splevo.jamopp.diffing/src/org/splevo/jamopp/diffing/similarity/base/ecore/MultipleSimilarityCheckHandler.java
@@ -5,6 +5,8 @@ import java.util.Collection;
 import org.eclipse.emf.ecore.EObject;
 import org.splevo.jamopp.diffing.similarity.base.ISimilarityRequest;
 import org.splevo.jamopp.diffing.similarity.base.ISimilarityRequestHandler;
+import org.splevo.jamopp.diffing.util.JaMoPPBooleanUtil;
+import org.splevo.jamopp.diffing.util.JaMoPPNullCheckUtil;
 
 /**
  * A {@link ISimilarityRequestHandler} that processes
@@ -60,10 +62,10 @@ public class MultipleSimilarityCheckHandler implements ISimilarityRequestHandler
 		Collection<? extends IComposedSwitchAdapter> sss = (Collection<? extends IComposedSwitchAdapter>) params[2];
 
 		// Null check to avoid NullPointerExceptions
-		if (elements1 == elements2) {
+		if (JaMoPPNullCheckUtil.allNull(elements1, elements2)) {
 			return Boolean.TRUE;
 		}
-		else if (elements1 == null ^ elements2 == null) {
+		else if (JaMoPPNullCheckUtil.onlyOneIsNull(elements1, elements2)) {
 			return Boolean.FALSE;
 		}
 		
@@ -85,7 +87,7 @@ public class MultipleSimilarityCheckHandler implements ISimilarityRequestHandler
 			Boolean childSimilarity = (Boolean) this.srh
 					.handleSimilarityRequest(new SingleSimilarityCheckRequest(es1[i], es2[i], ssA[i]));
 
-			if (childSimilarity != Boolean.TRUE) {
+			if (JaMoPPBooleanUtil.isNotTrue(childSimilarity)) {
 				return Boolean.FALSE;
 			}
 		}
