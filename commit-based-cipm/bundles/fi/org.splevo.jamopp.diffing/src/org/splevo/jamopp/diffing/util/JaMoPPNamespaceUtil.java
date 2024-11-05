c814889bc0525515dcdc9e930f521d2227b132a8
AlpTorac <a.torac@hotmail.com>
2024-11-05 22:06:02 +0100

    Move compareNamespacesByPart(...) implementation to utility class

--- commit-based-cipm/bundles/fi/org.splevo.jamopp.diffing/src/org/splevo/jamopp/diffing/util/JaMoPPNamespaceUtil.java
+++ commit-based-cipm/bundles/fi/org.splevo.jamopp.diffing/src/org/splevo/jamopp/diffing/util/JaMoPPNamespaceUtil.java
@@ -5,6 +5,41 @@ import org.emftext.language.java.commons.NamespaceAwareElement;
 
 public class JaMoPPNamespaceUtil {
 	/**
+	 * Compares the namespaces of the given {@link NamespaceAwareElement}s part by
+	 * part.
+	 * 
+	 * @return False if namespaces have parts different parts, true if not.
+	 */
+	public static Boolean compareNamespacesByPart(NamespaceAwareElement nae1, NamespaceAwareElement nae2) {
+		// Null check to avoid NullPointerExceptions
+		if (nae1 == nae2) {
+			return true;
+		} else if (nae1 == null ^ nae2 == null) {
+			return false;
+		}
+
+		var nss1 = nae1.getNamespaces();
+		var nss2 = nae2.getNamespaces();
+
+		// Null check to avoid NullPointerExceptions
+		if (nss1 == nss2) {
+			return true;
+		} else if (nss1 == null ^ nss2 == null) {
+			return false;
+		}
+
+		if (nss1.size() != nss2.size()) {
+			return false;
+		}
+		for (int idx = 0; idx < nss1.size(); idx++) {
+			if (!nss1.get(idx).equals(nss2.get(idx))) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	/**
 	 * Build the package path for a given element. Either the element itself is
 	 * aware of it's name space or the closest aware container is used.
 	 *
