53ee0b03884f3787dae4fbc86f8194e69f8c4535
AlpTorac <a.torac@hotmail.com>
2024-11-06 17:09:02 +0100

    Extract some comparing methods to new utility class

---
+++ commit-based-cipm/bundles/fi/org.splevo.jamopp.diffing/src/org/splevo/jamopp/diffing/util/JaMoPPComparisonUtil.java
@@ -0,0 +1,21 @@
+package org.splevo.jamopp.diffing.util;
+
+import org.emftext.language.java.commons.NamedElement;
+
+import com.google.common.base.Strings;
+
+public class JaMoPPComparisonUtil {
+	public static Boolean stringsEqual(String s1, String s2) {
+		return Strings.nullToEmpty(s1).equals(Strings.nullToEmpty(s2));
+	}
+
+	public static Boolean namesEqual(NamedElement ne1, NamedElement ne2) {
+		if (JaMoPPNullCheckUtil.allNull(ne1, ne2)) {
+			return true;
+		} else if (JaMoPPNullCheckUtil.onlyOneIsNull(ne1, ne2)) {
+			return false;
+		}
+
+        return JaMoPPComparisonUtil.stringsEqual(ne1.getName(), ne2.getName());
+	}
+}
