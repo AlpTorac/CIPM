18585fb2651110fd100709b9823a7c45126ba8ec
AlpTorac <a.torac@hotmail.com>
2025-01-20 18:48:00 +0100

    Edit commentary

--- commit-based-cipm/bundles/fi/org.splevo.jamopp.diffing/src/org/splevo/jamopp/diffing/util/package-info.java
+++ commit-based-cipm/bundles/fi/org.splevo.jamopp.diffing/src/org/splevo/jamopp/diffing/util/package-info.java
@@ -1,4 +1,5 @@
 /**
- * Utility package to handle JaMoPP models.
+ * Utility package to handle JaMoPP models. The classes within contain methods
+ * that can work with null parameters.
  */
 package org.splevo.jamopp.diffing.util;
