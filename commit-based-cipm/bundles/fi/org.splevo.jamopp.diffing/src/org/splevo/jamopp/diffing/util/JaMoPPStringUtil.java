f1b2039dbc3be59bb79bcb08ce04caae36223e2c
AlpTorac <a.torac@hotmail.com>
2025-01-28 17:55:34 +0100

    Fix String checking
    Make similarity checking detect a difference upon comparing of null
    String (null) and empty String ("")

--- commit-based-cipm/bundles/fi/org.splevo.jamopp.diffing/src/org/splevo/jamopp/diffing/util/JaMoPPStringUtil.java
+++ commit-based-cipm/bundles/fi/org.splevo.jamopp.diffing/src/org/splevo/jamopp/diffing/util/JaMoPPStringUtil.java
@@ -1,7 +1,5 @@
 package org.splevo.jamopp.diffing.util;
 
-import com.google.common.base.Strings;
-
 /**
  * A utility class for comparing String instances. <br>
  * <br>
@@ -13,14 +11,16 @@ import com.google.common.base.Strings;
  */
 public class JaMoPPStringUtil {
 	/**
-	 * TODO Decide whether null Strings (null) are the same as empty strings ("")
-	 * 
-	 * Uses {@link Strings#nullToEmpty(String)} on both parameters before comparing
-	 * them to allow null parameters.
-	 * 
-	 * @return Whether the given String instances are equal.
+	 * @return Whether the given String instances are equal. Accounts for null
+	 *         parameters.
 	 */
 	public static Boolean stringsEqual(String s1, String s2) {
-		return Strings.nullToEmpty(s1).equals(Strings.nullToEmpty(s2));
+		if (JaMoPPNullCheckUtil.allNull(s1, s2)) {
+			return true;
+		} else if (JaMoPPNullCheckUtil.allNonNull(s1, s2)) {
+			return s1.equals(s2);
+		} else {
+			return false;
+		}
 	}
 }
