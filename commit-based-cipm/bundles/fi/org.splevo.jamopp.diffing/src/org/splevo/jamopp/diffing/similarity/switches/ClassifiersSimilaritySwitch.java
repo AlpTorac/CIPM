d28906bcadeea2f973499914addc30040b442aae
AlpTorac <a.torac@hotmail.com>
2025-01-28 17:59:14 +0100

    Make caseConcreteClassifier symmetric
    It now processes the objects it compares equally

--- commit-based-cipm/bundles/fi/org.splevo.jamopp.diffing/src/org/splevo/jamopp/diffing/similarity/switches/ClassifiersSimilaritySwitch.java
+++ commit-based-cipm/bundles/fi/org.splevo.jamopp.diffing/src/org/splevo/jamopp/diffing/similarity/switches/ClassifiersSimilaritySwitch.java
@@ -6,8 +6,7 @@ import org.emftext.language.java.classifiers.util.ClassifiersSwitch;
 import org.splevo.jamopp.diffing.similarity.IJavaSimilaritySwitch;
 import org.splevo.jamopp.diffing.similarity.ILoggableJavaSwitch;
 import org.splevo.jamopp.diffing.similarity.base.ISimilarityRequestHandler;
-
-import com.google.common.base.Strings;
+import org.splevo.jamopp.diffing.util.JaMoPPStringUtil;
 
 /**
  * Similarity decisions for classifier elements.
@@ -56,11 +55,10 @@ public class ClassifiersSimilaritySwitch extends ClassifiersSwitch<Boolean> impl
     	
         ConcreteClassifier classifier2 = (ConcreteClassifier) this.getCompareElement();
 
-        String name1 = Strings.nullToEmpty(classifier1.getQualifiedName());
-        name1 = Strings.nullToEmpty(this.normalizeClassifier(name1));
-        String name2 = Strings.nullToEmpty(classifier2.getQualifiedName());
+        String name1 = this.normalizeClassifier(classifier1.getQualifiedName());
+        String name2 = this.normalizeClassifier(classifier2.getQualifiedName());
 
-        return (name1.equals(name2));
+        return JaMoPPStringUtil.stringsEqual(name1, name2);
     }
     
     /**
