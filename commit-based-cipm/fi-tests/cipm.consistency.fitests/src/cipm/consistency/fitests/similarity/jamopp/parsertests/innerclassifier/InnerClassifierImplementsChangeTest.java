4413bd766542aeb76091887ed31c1f21d3e92f80
AlpTorac <a.torac@hotmail.com>
2025-02-27 17:55:40 +0100

    Fix parser tests
    Adapt affected classes

--- commit-based-cipm/fi-tests/cipm.consistency.fitests/src/cipm/consistency/fitests/similarity/jamopp/parsertests/innerclassifier/InnerClassifierImplementsChangeTest.java
+++ commit-based-cipm/fi-tests/cipm.consistency.fitests/src/cipm/consistency/fitests/similarity/jamopp/parsertests/innerclassifier/InnerClassifierImplementsChangeTest.java
@@ -31,6 +31,6 @@ public class InnerClassifierImplementsChangeTest extends AbstractJaMoPPComplexPa
 		if (super.getExpectedSimilarityResultForModelComparison(lhs, lhsSourceFilePath, rhs, rhsSourceFilePath)) {
 			return true;
 		}
-		return this.allContentSimilar(lhs, rhs);
+		return this.contentwiseSimilar(lhs, rhs);
 	}
 }
