cccb4711985accd4527cda94f4817eaebc01228b
AlpTorac <a.torac@hotmail.com>
2025-03-01 16:50:02 +0100

    (WIP) Refactor complex test

--- commit-based-cipm/fi-tests/cipm.consistency.fitests/src/cipm/consistency/fitests/similarity/jamopp/unittests/complextests/CyclicClassifierDeclarationTest.java
+++ commit-based-cipm/fi-tests/cipm.consistency.fitests/src/cipm/consistency/fitests/similarity/jamopp/unittests/complextests/CyclicClassifierDeclarationTest.java
@@ -1,12 +1,18 @@
 package cipm.consistency.fitests.similarity.jamopp.unittests.complextests;
 
 import java.util.ArrayList;
-import java.util.stream.Stream;
+import java.util.Collection;
+import java.util.List;
 
+import org.emftext.language.java.classifiers.ConcreteClassifier;
+import org.emftext.language.java.commons.CommonsPackage;
+import org.emftext.language.java.generics.TypeParameter;
 import org.emftext.language.java.imports.ImportsPackage;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
+import org.emftext.language.java.types.ClassifierReference;
+import org.junit.jupiter.api.DynamicContainer;
+import org.junit.jupiter.api.DynamicNode;
+import org.junit.jupiter.api.DynamicTest;
+import org.junit.jupiter.api.TestFactory;
 
 import cipm.consistency.fitests.similarity.jamopp.AbstractJaMoPPSimilarityTest;
 import cipm.consistency.initialisers.jamopp.classifiers.IConcreteClassifierInitialiser;
@@ -24,32 +30,6 @@ import cipm.consistency.initialisers.jamopp.types.ClassifierReferenceInitialiser
  * @author Alp Torac Genc
  */
 public class CyclicClassifierDeclarationTest extends AbstractJaMoPPSimilarityTest {
-	/**
-	 * @return All {@link ConcreteClassifier} implementor types.
-	 */
-	private static Stream<Arguments> genCls() {
-		return AbstractJaMoPPSimilarityTest.getNonAdaptedInitialiserArgumentsFor(IConcreteClassifierInitialiser.class);
-	}
-
-	/**
-	 * @return All possible combinations of {@link ConcreteClassifier} types with
-	 *         {@link Import} types.
-	 */
-	private static Stream<Arguments> genClsXImport() {
-		var res = new ArrayList<Arguments>();
-		var clsfierInits = AbstractJaMoPPSimilarityTest
-				.getNonAdaptedInitialisersFor(IConcreteClassifierInitialiser.class);
-		var impInits = AbstractJaMoPPSimilarityTest.getNonAdaptedInitialisersFor(IImportInitialiser.class);
-		for (var cInit : clsfierInits) {
-			for (var iInit : impInits) {
-				res.add(Arguments.of(cInit, iInit,
-						String.format("Classifier type: %s, Import type: %s",
-								cInit.getInstanceClassOfInitialiser().getSimpleName(),
-								iInit.getInstanceClassOfInitialiser().getSimpleName())));
-			}
-		}
-		return res.stream();
-	}
 
 	/**
 	 * Ensures that similarity checking handles classifiers with type parameters
@@ -57,130 +37,94 @@ public class CyclicClassifierDeclarationTest extends AbstractJaMoPPSimilarityTes
 	 * <br>
 	 * {@code cls1<T1 extends cls2>} <br>
 	 * {@code cls2<T2 extends cls1>}
-	 * 
-	 * @param cInit       The initialiser that will instantiate the classifiers,
-	 *                    which will be imported
-	 * @param displayName The display of this test
 	 */
-	@ParameterizedTest(name = "{1}")
-	@MethodSource("genCls")
-	public void testCyclicTypeParameter(IConcreteClassifierInitialiser cInit, String displayName) {
-		var cls1 = cInit.instantiate();
-		cInit.setName(cls1, "cls1");
-		var cls2 = cInit.instantiate();
-		cInit.setName(cls2, "cls2");
+	@TestFactory
+	public Collection<DynamicNode> testCyclicDeclaration() {
+		var tests = new ArrayList<DynamicNode>();
 
-		var clsRefInit = new ClassifierReferenceInitialiser();
-		var cls1Ref = clsRefInit.instantiate();
-		clsRefInit.setTarget(cls1Ref, cls1);
-		var cls2Ref = clsRefInit.instantiate();
-		clsRefInit.setTarget(cls2Ref, cls2);
-
+		var allInits = this.getUsedInitialiserPackage().getAllInitialiserInstances();
+		var clsInits = List.of(allInits.stream().filter((i) -> i instanceof IConcreteClassifierInitialiser)
+				.toArray(IConcreteClassifierInitialiser[]::new));
+		var impInits = List.of(
+				allInits.stream().filter((i) -> i instanceof IImportInitialiser).toArray(IImportInitialiser[]::new));
 		var tpInit = new TypeParameterInitialiser();
-		var t1 = tpInit.instantiate();
-		tpInit.addExtendType(t1, cls1Ref);
-		var t2 = tpInit.instantiate();
-		tpInit.addExtendType(t2, cls2Ref);
+		var clsRefInit = new ClassifierReferenceInitialiser();
 
-		cInit.addTypeParameter(cls1, t2);
-		cInit.addTypeParameter(cls2, t1);
+		for (var cInit : clsInits) {
+			var cls1 = cInit.instantiate();
+			cInit.setName(cls1, "cls1");
+			var cls2 = cInit.instantiate();
+			cInit.setName(cls2, "cls2");
 
-		this.testSimilarity(cls1, cls2, false);
-		this.testSimilarity(cls1Ref, cls2Ref, false);
-		this.testSimilarity(t1, t2, false);
+			var cls1Ref = clsRefInit.instantiate();
+			clsRefInit.setTarget(cls1Ref, cls1);
+			var cls2Ref = clsRefInit.instantiate();
+			clsRefInit.setTarget(cls2Ref, cls2);
+
+			var t1 = tpInit.instantiate();
+			tpInit.addExtendType(t1, cls1Ref);
+			var t2 = tpInit.instantiate();
+			tpInit.addExtendType(t2, cls2Ref);
+			cInit.addTypeParameter(cls1, t2);
+			cInit.addTypeParameter(cls2, t1);
+
+			var clsTestList = new ArrayList<DynamicNode>();
+
+			clsTestList
+					.addAll(this.testDeclaration(this.cloneEObjWithContainers(cls1), this.cloneEObjWithContainers(cls2),
+							this.cloneEObjWithContainers(cls1Ref), this.cloneEObjWithContainers(cls2Ref),
+							this.cloneEObjWithContainers(t1), this.cloneEObjWithContainers(t2)));
+
+			for (var iInit : impInits) {
+				var impTestList = new ArrayList<DynamicNode>();
+
+				impTestList.addAll(this.testImport_FromPackage(this.cloneEObjWithContainers(cls1),
+						this.cloneEObjWithContainers(cls2), iInit));
+				impTestList.addAll(this.testImport_FromCompilationUnit(this.cloneEObjWithContainers(cls1),
+						this.cloneEObjWithContainers(cls2), iInit));
+
+				var impNode = DynamicContainer.dynamicContainer(
+						String.format("Import type: %s", iInit.getInstanceClassOfInitialiser().getSimpleName()),
+						impTestList);
+				clsTestList.add(impNode);
+			}
+			var clsNode = DynamicContainer.dynamicContainer(
+					String.format("Classifier type: %s", cInit.getInstanceClassOfInitialiser().getSimpleName()),
+					clsTestList);
+			tests.add(clsNode);
+		}
+
+		return tests;
 	}
 
 	/**
-	 * Ensures that similarity checking handles importing classifiers with type
-	 * parameters that are forced to depend on one another: <br>
-	 * <br>
-	 * {@code cls1<T1 extends cls2>} <br>
-	 * {@code cls2<T2 extends cls1>} <br>
-	 * <br>
-	 * cls1 imports cls2, cls2 imports cls1
-	 * 
-	 * @param cInit       The initialiser that will instantiate the classifiers,
-	 *                    which will be imported
-	 * @param iInit       The initialiser that will instantiate the said imports
-	 * @param displayName The display of this test
+	 * TODO Add commentary
 	 */
-	@ParameterizedTest(name = "{2}")
-	@MethodSource("genClsXImport")
-	public void testCyclicImport_WithoutContainer(IConcreteClassifierInitialiser cInit, IImportInitialiser iInit,
-			String displayName) {
-		var cls1 = cInit.instantiate();
-		cInit.setName(cls1, "cls1");
-		var cls2 = cInit.instantiate();
-		cInit.setName(cls2, "cls2");
+	public Collection<DynamicNode> testDeclaration(ConcreteClassifier cls1, ConcreteClassifier cls2,
+			ClassifierReference cls1Ref, ClassifierReference cls2Ref, TypeParameter t1, TypeParameter t2) {
+		var tests = new ArrayList<DynamicNode>();
 
-		var clsRefInit = new ClassifierReferenceInitialiser();
-		var cls1Ref = clsRefInit.instantiate();
-		clsRefInit.setTarget(cls1Ref, cls1);
-		var cls2Ref = clsRefInit.instantiate();
-		clsRefInit.setTarget(cls2Ref, cls2);
-
-		var tpInit = new TypeParameterInitialiser();
-		var t1 = tpInit.instantiate();
-		tpInit.addExtendType(t1, cls1Ref);
-		var t2 = tpInit.instantiate();
-		tpInit.addExtendType(t2, cls2Ref);
-
-		cInit.addTypeParameter(cls1, t2);
-		cInit.addTypeParameter(cls2, t1);
-
-		this.testSimilarity(cls1, cls2, false);
-		this.testSimilarity(cls1Ref, cls2Ref, false);
-		this.testSimilarity(t1, t2, false);
-
-		var imp1 = iInit.instantiate();
-		iInit.setClassifier(imp1, cls1);
-		var imp2 = iInit.instantiate();
-		iInit.setClassifier(imp2, cls2);
-		this.testSimilarity(imp1, imp2, this.getExpectedSimilarityResult(iInit.getInstanceClassOfInitialiser(),
-				ImportsPackage.Literals.IMPORT__CLASSIFIER));
+		var cyclicDefTest1 = DynamicTest.dynamicTest("Compare classifiers", () -> {
+			this.testSimilarity(cls1, cls2, false);
+		});
+		var cyclicDefTest2 = DynamicTest.dynamicTest("Compare classifier reference", () -> {
+			this.testSimilarity(cls1Ref, cls2Ref, false);
+		});
+		var cyclicDefTest3 = DynamicTest.dynamicTest("Compare Type parameter", () -> {
+			this.testSimilarity(t1, t2, false);
+		});
+		var cyclicDefTestNode = DynamicContainer.dynamicContainer("Compare declaration",
+				List.of(cyclicDefTest1, cyclicDefTest2, cyclicDefTest3));
+		tests.add(cyclicDefTestNode);
+		return tests;
 	}
 
 	/**
-	 * Ensures that similarity checking handles importing classifiers (in their own
-	 * packages) with type parameters that are forced to depend on one another: <br>
-	 * <br>
-	 * {@code cls1<T1 extends cls2>} <br>
-	 * {@code cls2<T2 extends cls1>} <br>
-	 * <br>
-	 * p1.cls1 imports p2.cls2, p2.cls2 imports p1.cls1
-	 * 
-	 * @param cInit       The initialiser that will instantiate the classifiers,
-	 *                    which will be imported
-	 * @param iInit       The initialiser that will instantiate the said imports
-	 * @param displayName The display of this test
+	 * TODO Add commentary
 	 */
-	@ParameterizedTest(name = "{2}")
-	@MethodSource("genClsXImport")
-	public void testCyclicImport_WithPackage(IConcreteClassifierInitialiser cInit, IImportInitialiser iInit,
-			String displayName) {
-		var cls1 = cInit.instantiate();
-		cInit.setName(cls1, "cls1");
-		var cls2 = cInit.instantiate();
-		cInit.setName(cls2, "cls2");
-
-		var clsRefInit = new ClassifierReferenceInitialiser();
-		var cls1Ref = clsRefInit.instantiate();
-		clsRefInit.setTarget(cls1Ref, cls1);
-		var cls2Ref = clsRefInit.instantiate();
-		clsRefInit.setTarget(cls2Ref, cls2);
-
-		var tpInit = new TypeParameterInitialiser();
-		var t1 = tpInit.instantiate();
-		tpInit.addExtendType(t1, cls1Ref);
-		var t2 = tpInit.instantiate();
-		tpInit.addExtendType(t2, cls2Ref);
-
-		cInit.addTypeParameter(cls1, t2);
-		cInit.addTypeParameter(cls2, t1);
-
-		this.testSimilarity(cls1, cls2, false);
-		this.testSimilarity(cls1Ref, cls2Ref, false);
-		this.testSimilarity(t1, t2, false);
+	public Collection<DynamicNode> testImport_FromPackage(ConcreteClassifier cls1, ConcreteClassifier cls2,
+			IImportInitialiser iInit) {
+		var tests = new ArrayList<DynamicNode>();
 
 		var pInit = new PackageInitialiser();
 		var p1 = pInit.instantiate();
@@ -190,7 +134,9 @@ public class CyclicClassifierDeclarationTest extends AbstractJaMoPPSimilarityTes
 		pInit.addNamespace(p2, "p2");
 		pInit.addClassifier(p2, cls2);
 
-		this.testSimilarity(p1, p2, false);
+		var cyclicDefWithImpTest1 = DynamicTest.dynamicTest("Compare packages (of classifiers)", () -> {
+			this.testSimilarity(p1, p2, false);
+		});
 
 		var imp1 = iInit.instantiate();
 		iInit.addNamespaces(imp1, p1.getNamespaces().toArray(String[]::new));
@@ -199,54 +145,42 @@ public class CyclicClassifierDeclarationTest extends AbstractJaMoPPSimilarityTes
 		iInit.addNamespaces(imp2, p2.getNamespaces().toArray(String[]::new));
 		iInit.setClassifier(imp2, cls2);
 
-		this.testSimilarity(imp1, imp2, false);
+		var impClassifierDoesNotMatter = this.getExpectedSimilarityResult(iInit.getInstanceClassOfInitialiser(),
+				ImportsPackage.Literals.IMPORT__CLASSIFIER);
+		var impNsDoesNotMatter = this.getExpectedSimilarityResult(iInit.getInstanceClassOfInitialiser(),
+				CommonsPackage.Literals.NAMESPACE_AWARE_ELEMENT__NAMESPACES);
+		var expectedImpComparisonResult = impClassifierDoesNotMatter && impNsDoesNotMatter;
+
+		var cyclicDefWithImpTest2 = DynamicTest.dynamicTest("Compare imports (package.classifier)", () -> {
+			this.testSimilarity(imp1, imp2, expectedImpComparisonResult);
+		});
+
+		// Test again after putting the imports into the packages
+
+		pInit.addImport(p1, imp2);
+		pInit.addImport(p2, imp1);
+
+		var cyclicDefWithImpTest3 = DynamicTest.dynamicTest("Compare packages (of classifiers) with imports", () -> {
+			this.testSimilarity(p1, p2, false);
+		});
+
+		var cyclicDefWithImpTest4 = DynamicTest
+				.dynamicTest("Compare imports (package.classifier) after adding them to packages", () -> {
+					this.testSimilarity(imp1, imp2, expectedImpComparisonResult);
+				});
+
+		var cyclicDefTestNode = DynamicContainer.dynamicContainer("Compare imports from packages",
+				List.of(cyclicDefWithImpTest1, cyclicDefWithImpTest2, cyclicDefWithImpTest3, cyclicDefWithImpTest4));
+		tests.add(cyclicDefTestNode);
+		return tests;
 	}
 
 	/**
-	 * Ensures that similarity checking handles importing classifiers (in their own
-	 * compilation unit) with type parameters that are forced to depend on one
-	 * another: <br>
-	 * <br>
-	 * {@code cls1<T1 extends cls2>} <br>
-	 * {@code cls2<T2 extends cls1>} <br>
-	 * <br>
-	 * Both classifiers cls1 and cls2 are stored in their own compilation units.
-	 * <br>
-	 * <br>
-	 * cls1 imports cls2, cls2 imports cls1
-	 * 
-	 * @param cInit       The initialiser that will instantiate the classifiers,
-	 *                    which will be imported
-	 * @param iInit       The initialiser that will instantiate the said imports
-	 * @param displayName The display of this test
+	 * TODO Add commentary
 	 */
-	@ParameterizedTest(name = "{2}")
-	@MethodSource("genClsXImport")
-	public void testCyclicImport_WithCompilationUnit(IConcreteClassifierInitialiser cInit, IImportInitialiser iInit,
-			String displayName) {
-		var cls1 = cInit.instantiate();
-		cInit.setName(cls1, "cls1");
-		var cls2 = cInit.instantiate();
-		cInit.setName(cls2, "cls2");
-
-		var clsRefInit = new ClassifierReferenceInitialiser();
-		var cls1Ref = clsRefInit.instantiate();
-		clsRefInit.setTarget(cls1Ref, cls1);
-		var cls2Ref = clsRefInit.instantiate();
-		clsRefInit.setTarget(cls2Ref, cls2);
-
-		var tpInit = new TypeParameterInitialiser();
-		var t1 = tpInit.instantiate();
-		tpInit.addExtendType(t1, cls1Ref);
-		var t2 = tpInit.instantiate();
-		tpInit.addExtendType(t2, cls2Ref);
-
-		cInit.addTypeParameter(cls1, t2);
-		cInit.addTypeParameter(cls2, t1);
-
-		this.testSimilarity(cls1, cls2, false);
-		this.testSimilarity(cls1Ref, cls2Ref, false);
-		this.testSimilarity(t1, t2, false);
+	public Collection<DynamicNode> testImport_FromCompilationUnit(ConcreteClassifier cls1, ConcreteClassifier cls2,
+			IImportInitialiser iInit) {
+		var tests = new ArrayList<DynamicNode>();
 
 		var cuInit = new CompilationUnitInitialiser();
 		var cu1 = cuInit.instantiate();
@@ -256,7 +190,9 @@ public class CyclicClassifierDeclarationTest extends AbstractJaMoPPSimilarityTes
 		cuInit.addNamespace(cu2, "cu2");
 		cuInit.addClassifier(cu2, cls2);
 
-		this.testSimilarity(cu1, cu2, false);
+		var cyclicDefWithImpTest1 = DynamicTest.dynamicTest("Compare compilation units (of classifiers)", () -> {
+			this.testSimilarity(cu1, cu2, false);
+		});
 
 		var imp1 = iInit.instantiate();
 		iInit.addNamespaces(imp1, cu1.getNamespaces().toArray(String[]::new));
@@ -265,6 +201,33 @@ public class CyclicClassifierDeclarationTest extends AbstractJaMoPPSimilarityTes
 		iInit.addNamespaces(imp2, cu2.getNamespaces().toArray(String[]::new));
 		iInit.setClassifier(imp2, cls2);
 
-		this.testSimilarity(imp1, imp2, false);
+		var impClassifierDoesNotMatter = this.getExpectedSimilarityResult(iInit.getInstanceClassOfInitialiser(),
+				ImportsPackage.Literals.IMPORT__CLASSIFIER);
+		var impNsDoesNotMatter = this.getExpectedSimilarityResult(iInit.getInstanceClassOfInitialiser(),
+				CommonsPackage.Literals.NAMESPACE_AWARE_ELEMENT__NAMESPACES);
+		var expectedImpComparisonResult = impClassifierDoesNotMatter && impNsDoesNotMatter;
+
+		var cyclicDefWithImpTest2 = DynamicTest.dynamicTest("Compare imports (cuNamespace.classifier)", () -> {
+			this.testSimilarity(imp1, imp2, expectedImpComparisonResult);
+		});
+
+		// Test again after adding imports to the compilation units
+
+		cuInit.addImport(cu1, imp2);
+		cuInit.addImport(cu2, imp1);
+
+		var cyclicDefWithImpTest3 = DynamicTest.dynamicTest("Compare compilation units (of classifiers) with imports",
+				() -> {
+					this.testSimilarity(cu1, cu2, false);
+				});
+		var cyclicDefWithImpTest4 = DynamicTest
+				.dynamicTest("Compare imports (cuNamespace.classifier) after adding them to compilation units", () -> {
+					this.testSimilarity(imp1, imp2, expectedImpComparisonResult);
+				});
+
+		var cyclicDefTestNode = DynamicContainer.dynamicContainer("Compare imports from compilation units",
+				List.of(cyclicDefWithImpTest1, cyclicDefWithImpTest2, cyclicDefWithImpTest3, cyclicDefWithImpTest4));
+		tests.add(cyclicDefTestNode);
+		return tests;
 	}
 }
