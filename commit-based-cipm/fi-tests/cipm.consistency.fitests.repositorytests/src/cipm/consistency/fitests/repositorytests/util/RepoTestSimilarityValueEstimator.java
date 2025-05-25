c77e9258027bd10856aac24dae087116f77fe89c
AlpTorac <a.torac@hotmail.com>
2025-05-25 18:25:20 +0200

    (WIP) Work on similarity value estimation

--- commit-based-cipm/fi-tests/cipm.consistency.fitests.repositorytests/src/cipm/consistency/fitests/repositorytests/util/RepoTestSimilarityValueEstimator.java
+++ commit-based-cipm/fi-tests/cipm.consistency.fitests.repositorytests/src/cipm/consistency/fitests/repositorytests/util/RepoTestSimilarityValueEstimator.java
@@ -2,6 +2,8 @@ package cipm.consistency.fitests.repositorytests.util;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.diff.DiffFormatter;
@@ -13,42 +15,57 @@ import cipm.consistency.fitests.repositorytests.util.difffilter.DiffFilter;
 public class RepoTestSimilarityValueEstimator {
 	public boolean getExpectedSimilarityValueFor(Git git, AbstractTreeIterator oldTreeIter,
 			AbstractTreeIterator newTreeIter) {
-		var osOutput = "";
-		var filter = new DiffFilter();
-		var cr = new QuickCommentRemover();
-
 		try (var os = new ByteArrayOutputStream(); var df = new DiffFormatter(os)) {
 			df.setRepository(git.getRepository());
 			df.setContext(3);
 //			df.setPathFilter(PathSuffixFilter.create(".java"));
 
-			// Include to get patches
-			df.format(oldTreeIter, newTreeIter);
+			var entries = df.scan(oldTreeIter, newTreeIter);
 
-			// Adapt all UNIX new lines to the current system
-			osOutput = os.toString().replaceAll("\\n", System.lineSeparator());
-
-//			var entries = df.scan(oldTreeIter, newTreeIter);
-//
-//			for (var e : entries) {
-//				df.format(e);
-//				osOutput += os.toString();
-//			}
-
-			df.close();
-			os.close();
+			for (var e : entries) {
+				df.format(e);
+				// Adapt all UNIX new lines to the current system
+				var code = this.getEffectiveLines(os.toString().replaceAll("\\n", System.lineSeparator()));
+				System.out.println(code);
+				var expectedSimVal = this.computeExpectedSimilarityValue(code);
+				if (!expectedSimVal)
+					return false;
+			}
 		} catch (IOException e) {
 			e.printStackTrace();
+			throw new IllegalStateException("IOException occured while computing expected similarity value", e);
 		}
 
-		var text = cr.removeComments(osOutput);
-		var lines = filter.splitLines(text);
+		return true;
+	}
+
+	public List<String> getEffectiveLines(String text) {
+		var filter = new DiffFilter();
+		var cr = new QuickCommentRemover();
+
+		var result = cr.removeComments(text);
+		var lines = filter.splitLines(result);
 		lines = filter.removeContextLines(lines);
 		lines = filter.removeNonPatchScript(lines);
 		lines = filter.removeBlankLines(lines);
-		text = lines.stream().reduce("", (l1, l2) -> String.format("%s%s%s", l1, System.lineSeparator(), l2));
 
-		System.out.println(text);
-		return text.isBlank();
+		return lines;
+	}
+
+	public boolean computeExpectedSimilarityValue(List<String> lines) {
+		var added = new ArrayList<String>();
+		var removed = new ArrayList<String>();
+
+		lines.stream().forEach((l) -> {
+			if (l.startsWith("+"))
+				added.add(l.substring(1).replaceAll("\\s", ""));
+			if (l.startsWith("-"))
+				removed.add(l.substring(1).replaceAll("\\s", ""));
+		});
+
+		var allAdded = added.stream().reduce("", (t1, t2) -> t1 + t2);
+		var allRemoved = removed.stream().reduce("", (t1, t2) -> t1 + t2);
+
+		return allAdded.equals(allRemoved);
 	}
 }
