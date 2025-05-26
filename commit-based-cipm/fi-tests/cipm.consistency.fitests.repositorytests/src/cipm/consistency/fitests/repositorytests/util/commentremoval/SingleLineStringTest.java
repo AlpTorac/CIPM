391d6bab8bbe6c4077b13ce6fb6db3c352ba3d26
AlpTorac <a.torac@hotmail.com>
2025-05-26 14:42:13 +0200

    Add tests for removing commentary

--- commit-based-cipm/fi-tests/cipm.consistency.fitests.repositorytests/src/cipm/consistency/fitests/repositorytests/util/commentremoval/SingleLineStringTest.java
+++ commit-based-cipm/fi-tests/cipm.consistency.fitests.repositorytests/src/cipm/consistency/fitests/repositorytests/util/commentremoval/SingleLineStringTest.java
@@ -7,7 +7,7 @@ import cipm.consistency.fitests.repositorytests.util.AbstractJaMoPPParserRepoUti
 
 public class SingleLineStringTest extends AbstractJaMoPPParserRepoUtilTest {
 	private ICommentRemover cr = new QuickCommentRemover();
-	
+
 	@Test
 	public void handleStringLiteral() {
 		var line1 = "\"abc\"";
@@ -33,6 +33,17 @@ public class SingleLineStringTest extends AbstractJaMoPPParserRepoUtilTest {
 	@Test
 	public void handleStringLiteral_WithMultipleEscapedQuotations() {
 		var line1 = "\"\\\"abc\\\"\"";
+
+		var text = concatLines(line1);
+
+		var filteredText = this.removeBlankLines(this.splitLines(cr.removeComments(text)));
+		Assertions.assertEquals(1, filteredText.size());
+		Assertions.assertEquals(line1, filteredText.get(0));
+	}
+
+	@Test
+	public void handleStringLiteral_MultiLineStringAsSingleLineString() {
+		var line1 = "\"\"\"//abc\"\"\"";
 
 		var text = concatLines(line1);
 
@@ -109,13 +120,41 @@ public class SingleLineStringTest extends AbstractJaMoPPParserRepoUtilTest {
 
 	@Test
 	public void handleUnclosedStringLiteral() {
-		var line1 = "\"abc";
+		var code = "\"abc";
+		var comment = "//def";
+		var line1 = code + comment;
 
 		var text = concatLines(line1);
 
 		var filteredText = this.removeBlankLines(this.splitLines(cr.removeComments(text)));
 		Assertions.assertEquals(1, filteredText.size());
-		Assertions.assertEquals(line1, filteredText.get(0));
+		Assertions.assertEquals(code, filteredText.get(0));
+	}
+
+	@Test
+	public void handleUnclosedStringLiteral_WithEscapedQuotationStart() {
+		var code = "\\\"abc";
+		var comment = "//def\"";
+		var line1 = code + comment;
+
+		var text = concatLines(line1);
+
+		var filteredText = this.removeBlankLines(this.splitLines(cr.removeComments(text)));
+		Assertions.assertEquals(1, filteredText.size());
+		Assertions.assertEquals(code, filteredText.get(0));
+	}
+
+	@Test
+	public void handleUnclosedStringLiteral_WithEscapedQuotationEnd() {
+		var code = "\"abc";
+		var comment = "//def\\\"";
+		var line1 = code + comment;
+
+		var text = concatLines(line1);
+
+		var filteredText = this.removeBlankLines(this.splitLines(cr.removeComments(text)));
+		Assertions.assertEquals(1, filteredText.size());
+		Assertions.assertEquals(code, filteredText.get(0));
 	}
 
 	@Test
@@ -152,5 +191,34 @@ public class SingleLineStringTest extends AbstractJaMoPPParserRepoUtilTest {
 		var filteredText = this.removeBlankLines(this.splitLines(cr.removeComments(text)));
 		Assertions.assertEquals(1, filteredText.size());
 		Assertions.assertEquals(start, filteredText.get(0));
+	}
+
+	@Test
+	public void handleBrokenStringLiteral_SingleLineString_AcrossTwoLines() {
+		var comment = "//abc";
+
+		var line1 = "\"" + comment;
+		var line2 = "\"";
+
+		var text = concatLines(line1, line2);
+
+		var filteredText = this.removeBlankLines(this.splitLines(cr.removeComments(text)));
+		Assertions.assertEquals(2, filteredText.size());
+		Assertions.assertEquals("\"", filteredText.get(0));
+		Assertions.assertEquals("\"", filteredText.get(1));
+	}
+
+	@Test
+	public void handleBrokenStringLiteral_SingleLineString_AcrossThreeLines() {
+		var line1 = "\"";
+		var line2 = "//abc";
+		var line3 = "\"";
+
+		var text = concatLines(line1, line2, line3);
+
+		var filteredText = this.removeBlankLines(this.splitLines(cr.removeComments(text)));
+		Assertions.assertEquals(2, filteredText.size());
+		Assertions.assertEquals("\"", filteredText.get(0));
+		Assertions.assertEquals("\"", filteredText.get(1));
 	}
 }
