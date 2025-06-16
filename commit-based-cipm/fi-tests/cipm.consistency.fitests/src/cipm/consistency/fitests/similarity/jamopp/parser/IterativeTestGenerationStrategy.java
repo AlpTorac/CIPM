41c679f4a394ece5be16f41cd7db689ec65b41dc
AlpTorac <a.torac@hotmail.com>
2025-06-16 19:12:09 +0200

    (WIP) Extract iterators from test generation strategies
    NOT FUNCTIONAL, required by parser repo tests to compute expected test results

--- commit-based-cipm/fi-tests/cipm.consistency.fitests/src/cipm/consistency/fitests/similarity/jamopp/parser/IterativeTestGenerationStrategy.java
+++ commit-based-cipm/fi-tests/cipm.consistency.fitests/src/cipm/consistency/fitests/similarity/jamopp/parser/IterativeTestGenerationStrategy.java
@@ -3,6 +3,7 @@ package cipm.consistency.fitests.similarity.jamopp.parser;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Iterator;
 
 import org.eclipse.emf.ecore.resource.Resource;
 import org.junit.jupiter.api.Assertions;
@@ -11,6 +12,35 @@ import org.junit.jupiter.api.DynamicNode;
 
 public class IterativeTestGenerationStrategy implements IJaMoPPParserTestGenerationStrategy {
 	private final static String description = "symmetric iteration strategy";
+
+	// TODO Fix and spare actually creating the multi-dimensional array
+	
+	private Iterator<int[]> getIterator(Path[] pathArr, Resource[] resArr) {
+		final int[][] idxs = new int[pathArr.length * 2][2];
+
+		for (int i = 0; i < pathArr.length; i++) {
+			idxs[i * 2] = new int[] { i, i + 1 };
+			idxs[i * 2 + 1] = new int[] { i + 1, i };
+		}
+
+		return new Iterator<int[]>() {
+			private int currentIdx = 0;
+			private int[][] indices = idxs;
+
+			@Override
+			public boolean hasNext() {
+				return currentIdx < indices.length && indices[currentIdx] != null;
+			}
+
+			@Override
+			public int[] next() {
+				var result = indices[currentIdx];
+				currentIdx++;
+				return result;
+			}
+
+		};
+	}
 
 	@Override
 	public Collection<DynamicNode> createTests(Path[] pathArr, Resource[] resArr,
@@ -22,11 +52,13 @@ public class IterativeTestGenerationStrategy implements IJaMoPPParserTestGenerat
 		var tests = new ArrayList<DynamicNode>();
 		tFacs.forEach((tf) -> {
 			var testsForModelDirs = new ArrayList<DynamicNode>();
-			for (int i = 0; i < pathArr.length - 1; i++) {
-				var path1 = pathArr[i];
-				var res1 = resArr[i];
-				var path2 = pathArr[i + 1];
-				var res2 = resArr[i + 1];
+			var it = this.getIterator(pathArr, resArr);
+			while (it.hasNext()) {
+				var idxs = it.next();
+				var path1 = pathArr[idxs[0]];
+				var res1 = resArr[idxs[0]];
+				var path2 = pathArr[idxs[1]];
+				var res2 = resArr[idxs[1]];
 				testsForModelDirs.add(tf.createTestsFor(res1, path1, res2, path2));
 				testsForModelDirs.add(tf.createTestsFor(res2, path2, res1, path1));
 			}
