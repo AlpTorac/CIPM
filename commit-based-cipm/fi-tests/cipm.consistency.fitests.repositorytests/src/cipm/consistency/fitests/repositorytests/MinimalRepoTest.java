cde6a86c1a3d35b35ee2224d65087b454df75ca7
AlpTorac <a.torac@hotmail.com>
2025-06-21 18:23:41 +0200

    Add more commits to minimal repo test
    to test transitivity of result cache
    
    Change test generation strategy

--- commit-based-cipm/fi-tests/cipm.consistency.fitests.repositorytests/src/cipm/consistency/fitests/repositorytests/MinimalRepoTest.java
+++ commit-based-cipm/fi-tests/cipm.consistency.fitests.repositorytests/src/cipm/consistency/fitests/repositorytests/MinimalRepoTest.java
@@ -11,12 +11,15 @@ import org.junit.jupiter.api.DynamicNode;
 import org.junit.jupiter.api.TestFactory;
 
 import cipm.consistency.fitests.similarity.jamopp.parser.AbstractJaMoPPParserSimilarityTestFactory;
+import cipm.consistency.fitests.similarity.jamopp.parser.CombinationTestGenerationStrategy;
 import cipm.consistency.fitests.similarity.jamopp.parser.EAllContentSimilarityTestFactory;
+import cipm.consistency.fitests.similarity.jamopp.parser.IJaMoPPParserTestGenerationStrategy;
 
 public class MinimalRepoTest extends AbstractJaMoPPParserRepoTest {
 	private static final List<String> commitIDs = List.of("364155e24b21122e980cad73da2d0b09c1c994aa",
 			"9eda56eaa52c2d5e47aade7a688e9ebda2645bde", "21ae1643905898a29470157de076942a7be05698",
-			"9ab81979f64539442b9dc5857b65d696918e4732");
+			"9ab81979f64539442b9dc5857b65d696918e4732", "1ae42af88d2702a44bd5192557e8561c18960c58",
+			"b9f1ef79249831958732241680ee1ba12dcda078");
 
 	@Override
 	protected List<String> getCommitIDs() {
@@ -26,6 +29,13 @@ public class MinimalRepoTest extends AbstractJaMoPPParserRepoTest {
 	@Override
 	protected URI getRepoURI() {
 		return URI.createFileURI("C:\\Users\\sdq-l\\OneDrive\\Desktop\\parserTestRepo");
+	}
+
+	@Override
+	protected Collection<IJaMoPPParserTestGenerationStrategy> getTestGenerationStrategies() {
+		var strats = new ArrayList<IJaMoPPParserTestGenerationStrategy>();
+		strats.add(new CombinationTestGenerationStrategy());
+		return strats;
 	}
 
 	@Override
