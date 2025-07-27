dd03470ff9ea3a46a5d2f180d15fe3c69b0d9168
AlpTorac <a.torac@hotmail.com>
2025-07-27 11:04:08 +0200

    Remove fitests.params

--- commit-based-cipm/fi-tests/cipm.consistency.fitests/src/cipm/consistency/fitests/similarity/jamopp/params/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * Contains implementations of interfaces from
- * {@link cipm.consistency.fitests.similarity.params} for JaMoPP.
- */
-package cipm.consistency.fitests.similarity.jamopp.params;
