cfeee50e5fc2ba02b1e5958f2852f26c02ed630a
AlpTorac <a.torac@hotmail.com>
2025-07-27 11:03:36 +0200

    Remove fitests.similarity.dummy

--- commit-based-cipm/fi-tests/cipm.consistency.fitests/src/cipm/consistency/fitests/similarity/base/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * Contains test classes and interfaces that depend on
- * {@link org.splevo.jamopp.diffing.similarity} and/or its sub-packages.
- */
-package cipm.consistency.fitests.similarity.base;
