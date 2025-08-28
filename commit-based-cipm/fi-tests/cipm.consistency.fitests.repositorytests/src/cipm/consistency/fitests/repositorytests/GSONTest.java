d864e4be35660735e9191805e83db4c1abd7adbb
AlpTorac <a.torac@hotmail.com>
2025-08-28 13:34:24 +0200

    (TMP) Implement a GSON loading strategy
    Tested and works on previously saved time measurement file
    
    Adapt GSONTest to work with arbitrary previously saved time measurement files

--- commit-based-cipm/fi-tests/cipm.consistency.fitests.repositorytests/src/cipm/consistency/fitests/repositorytests/GSONTest.java
+++ commit-based-cipm/fi-tests/cipm.consistency.fitests.repositorytests/src/cipm/consistency/fitests/repositorytests/GSONTest.java
@@ -2,161 +2,130 @@ package cipm.consistency.fitests.repositorytests;
 
 import java.io.File;
 import java.io.IOException;
-import java.lang.reflect.Type;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.List;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonDeserializationContext;
-import com.google.gson.JsonDeserializer;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonParseException;
-
-import cipm.consistency.fitests.similarity.jamopp.parser.timemeasurement.GSONDataStructure;
+import cipm.consistency.fitests.similarity.jamopp.parser.timemeasurement.GSONDataStructureLoadingStrategy;
 import cipm.consistency.fitests.similarity.jamopp.parser.timemeasurement.GSONPersistingStrategy;
 import cipm.consistency.fitests.similarity.jamopp.parser.timemeasurement.GeneralTimeMeasurementTag;
-//import cipm.consistency.fitests.similarity.jamopp.parser.timemeasurement.ITimeMeasurementDataStructureEntry;
+import cipm.consistency.fitests.similarity.jamopp.parser.timemeasurement.ITimeMeasurementDataStructure;
+import cipm.consistency.fitests.similarity.jamopp.parser.timemeasurement.ITimeMeasurementLoadingStrategy;
+import cipm.consistency.fitests.similarity.jamopp.parser.timemeasurement.ITimeMeasurementPersistingStrategy;
 import cipm.consistency.fitests.similarity.jamopp.parser.timemeasurement.ITimeMeasurementTag;
-import cipm.consistency.fitests.similarity.jamopp.parser.timemeasurement.ParserTestTimeMeasurementKey;
-import cipm.consistency.fitests.similarity.jamopp.parser.timemeasurement.TimeMeasurementEntry;
 
 /**
- * TODO Either remove or deal with magic strings before pushing
+ * TODO Commentary
  */
 public class GSONTest {
-	private Gson gson = null;
-	private Path newJsonPath;
+	private static final DateTimeFormatter fileContentTimePattern = DateTimeFormatter.ISO_DATE_TIME;
+	private static final Path timeMeasurementsRootPath = Path.of("target", "timeMeasurements").toAbsolutePath();
+	@SuppressWarnings("unchecked")
+	private static final ITimeMeasurementLoadingStrategy loadingStrat = new GSONDataStructureLoadingStrategy(
+			fileContentTimePattern, new Class[] { GeneralTimeMeasurementTag.class, RepoTimeMeasurementTag.class });
+	private static final ITimeMeasurementPersistingStrategy persistingStrat = new GSONPersistingStrategy(
+			fileContentTimePattern, DateTimeFormatter.ofPattern("dd-MM-yyyy_HH-mm-ss"));
+
+	private Path formerTimeMeasurementPath;
+	private Path newTimeMeasurementPath;
+
+	@BeforeEach
+	public void setUp() {
+		var timeMeasurementsRootDir = timeMeasurementsRootPath.toFile();
+		if (!timeMeasurementsRootDir.exists() || timeMeasurementsRootDir.listFiles().length == 0) {
+			Assertions.fail(
+					"There are no time measurement files to use in tests, make sure to provide at least one time measurement file in JSON format under: "
+							+ timeMeasurementsRootPath.toString());
+		} else {
+			formerTimeMeasurementPath = timeMeasurementsRootDir.listFiles()[0].toPath().toAbsolutePath();
+			newTimeMeasurementPath = new File("").toPath().toAbsolutePath()
+					.resolve(formerTimeMeasurementPath.getFileName());
+		}
+	}
 
 	@AfterEach
 	public void tearDown() {
-		if (newJsonPath != null && newJsonPath.toFile().exists()) {
+		if (newTimeMeasurementPath != null && newTimeMeasurementPath.toFile().exists()) {
 			try {
-				Files.delete(newJsonPath);
+				Files.delete(newTimeMeasurementPath);
 			} catch (IOException e) {
 				e.printStackTrace();
 				Assertions.fail(e);
 			}
 		}
-		newJsonPath = null;
+		formerTimeMeasurementPath = null;
+		newTimeMeasurementPath = null;
+	}
+
+	private String readTimeMeasurement(Path pathToTimeMeasurementToRead) {
+		String content = null;
+		try {
+			content = Files.readString(pathToTimeMeasurementToRead);
+			Assertions.assertFalse(content.isBlank());
+		} catch (IOException e) {
+			e.printStackTrace();
+			Assertions.fail(e);
+		}
+
+		return content;
+	}
+
+	private ITimeMeasurementDataStructure loadTimeMeasurement(Path pathToTimeMeasurementToLoad) {
+		return loadingStrat.load(pathToTimeMeasurementToLoad);
+	}
+
+	private void persistTimeMeasurement(ITimeMeasurementDataStructure dataStructure, Path savePath) {
+		persistingStrat.save(dataStructure, savePath);
 	}
 
 	@Test
-	public void deserialiseAndSerialiseJSON() {
-		gson = new GsonBuilder().registerTypeHierarchyAdapter(LocalDateTime.class, this.getDateDeserializer())
-//				.registerTypeHierarchyAdapter(TimeMeasurementEntry.class, getEntryDeserializer())
-				.registerTypeHierarchyAdapter(ITimeMeasurementTag.class, getTagDeserializer()).create();
+	public void loadDataStructure() {
+		var timeMeasurements = this.loadTimeMeasurement(formerTimeMeasurementPath);
 
-		var formerFile = new File(
-				"C:\\Users\\sdq-l\\CIPM2\\commit-based-cipm\\fi-tests\\cipm.consistency.fitests.repositorytests\\target\\timeMeasurements\\27-08-2025_21-07-21___27-08-2025_21-08-11.json");
+		Assertions.assertNotNull(timeMeasurements.getTimeMeasurerDescription());
 
-		String formerFileContent = null;
-		try {
-			formerFileContent = Files.readString(formerFile.toPath());
-		} catch (IOException e) {
-			e.printStackTrace();
-			Assertions.fail(e);
-		}
+		Assertions.assertNotNull(timeMeasurements.getEndTime());
 
-		var formerJson = gson.fromJson(formerFileContent, GSONDataStructure.class);
+		Assertions.assertNotNull(timeMeasurements.getStartTime());
 
-		// Deserialising works
+		Assertions.assertNotNull(timeMeasurements.getTimeMeasurementEntries());
+		Assertions.assertFalse(timeMeasurements.getTimeMeasurementEntries().isEmpty());
+		timeMeasurements.getTimeMeasurementEntries().forEach((e) -> {
+			Assertions.assertNotNull(e.getKey());
+			// TODO Assertions about the key
+			Assertions.assertNotNull(e.getTag());
+			Assertions.assertTrue(ITimeMeasurementTag.class.isAssignableFrom(e.getTag().getClass()));
+		});
 
-		var persistingStrat = new GSONPersistingStrategy(DateTimeFormatter.ISO_DATE_TIME,
-				DateTimeFormatter.ofPattern("dd-MM-yyyy_HH-mm-ss"));
+		Assertions.assertNotNull(timeMeasurements.getTimeUnit());
+	}
 
-		var newFile = new File("").getAbsoluteFile();
-		persistingStrat.save(formerJson, newFile.toPath());
-		newJsonPath = newFile.toPath().resolve("27-08-2025_21-07-21___27-08-2025_21-08-11.json");
+	@Test
+	public void loadAndSaveDataStructure() {
+		var formerFileContent = this.readTimeMeasurement(formerTimeMeasurementPath);
+		var formerTimeMeasurements = this.loadTimeMeasurement(formerTimeMeasurementPath);
 
-		String newFileContent = null;
-		try {
-			newFileContent = Files.readString(newJsonPath);
-//			newFileContent = newFileContent.replaceAll("\\.0,", ",");
-		} catch (IOException e) {
-			e.printStackTrace();
-			Assertions.fail(e);
-		}
+		var newFilePath = new File("").getAbsoluteFile().toPath();
+		// TODO Change to "newTimeMeasurementPath" after extracting file name from
+		// persisting strategy
+		this.persistTimeMeasurement(formerTimeMeasurements, newFilePath);
 
+		var newFileContent = this.readTimeMeasurement(newTimeMeasurementPath);
+		// Enable if GSON serialises Long instances with trailing zeroes (".0")
+		// newFileContent = newFileContent.replaceAll("\\.0,", ",");
+
+		// Ensure that serialising produces the same file
 		Assertions.assertEquals(formerFileContent, newFileContent);
 
-		// Serialising works and produces the same file
+		var newTimeMeasurements = this.loadTimeMeasurement(newTimeMeasurementPath);
 
-		var newJson = gson.fromJson(newFileContent, GSONDataStructure.class);
-
-		Assertions.assertTrue(formerJson.equals(newJson));
-
-		// Deserialising freshly serialised instance produces an equal instance
+		// Ensure that deserialising freshly serialised instance produces an equal
+		// instance
+		Assertions.assertTrue(formerTimeMeasurements.equals(newTimeMeasurements));
 	}
-
-	private JsonDeserializer<LocalDateTime> getDateDeserializer() {
-		return new JsonDeserializer<LocalDateTime>() {
-			@Override
-			public LocalDateTime deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context)
-					throws JsonParseException {
-				var date = json.getAsString();
-				return LocalDateTime.from(DateTimeFormatter.ISO_DATE_TIME.parse(date));
-			}
-		};
-	}
-
-	private JsonDeserializer<ITimeMeasurementTag> getTagDeserializer() {
-		return new JsonDeserializer<ITimeMeasurementTag>() {
-			private final List<ITimeMeasurementTag> possibleTags;
-
-			{
-				possibleTags = new ArrayList<ITimeMeasurementTag>();
-				for (var e : GeneralTimeMeasurementTag.values()) {
-					possibleTags.add(e);
-				}
-				for (var e : RepoTimeMeasurementTag.values()) {
-					possibleTags.add(e);
-				}
-			}
-
-			@Override
-			public ITimeMeasurementTag deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context)
-					throws JsonParseException {
-				var tagString = json.getAsString();
-				ITimeMeasurementTag tag = null;
-				for (var tagEnum : possibleTags) {
-					if (tagString.equals(tagEnum.toString())) {
-						tag = tagEnum;
-						break;
-					}
-				}
-				return tag;
-			}
-		};
-	}
-
-//	private JsonDeserializer<ITimeMeasurementDataStructureEntry> getEntryDeserializer() {
-//		return new JsonDeserializer<ITimeMeasurementDataStructureEntry>() {
-//			@Override
-//			public ITimeMeasurementDataStructureEntry deserialize(JsonElement json, Type typeOfT,
-//					JsonDeserializationContext context) throws JsonParseException {
-//				var entryObj = json.getAsJsonObject();
-//				var time = gson.fromJson(entryObj.get("timeElapsed"), Long.class);
-//
-////				var keyMap = gson.fromJson(entryObj.getAsJsonObject("key").get("keyMap"), HashMap.class);
-////				var key = new ParserTestTimeMeasurementKey().fromStringKeyMap(keyMap);
-//
-//				var key = gson.fromJson(entryObj.get("key"), ParserTestTimeMeasurementKey.class);
-//
-//				var tag = gson.fromJson(entryObj.get("tag"), ITimeMeasurementTag.class);
-//				var entry = new TimeMeasurementEntry(key, tag);
-//				entry.setTimeElapsed(time);
-//
-//				return entry;
-//			}
-//		};
-//	}
 }
---
+++ commit-based-cipm/fi-tests/cipm.consistency.fitests/src/cipm/consistency/fitests/similarity/jamopp/parser/timemeasurement/GSONDataStructureLoadingStrategy.java
@@ -0,0 +1,133 @@
+package cipm.consistency.fitests.similarity.jamopp.parser.timemeasurement;
+
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.HashSet;
+import java.util.Set;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParseException;
+
+/**
+ * TODO Commentary
+ */
+public class GSONDataStructureLoadingStrategy implements ITimeMeasurementLoadingStrategy {
+	/**
+	 * @see {@link #GSONLoadingStrategy(DateTimeFormatter, Class[])}
+	 */
+	private final DateTimeFormatter fileContentDateFormatter;
+	/**
+	 * @see {@link #GSONLoadingStrategy(DateTimeFormatter, Class[])}
+	 */
+	private final Set<ITimeMeasurementTag> possibleTags = new HashSet<ITimeMeasurementTag>();
+
+	/**
+	 * {@link #GSONDataStructureLoadingStrategy(DateTimeFormatter, Class[])} without
+	 * any class parameters
+	 */
+	public GSONDataStructureLoadingStrategy(DateTimeFormatter fileContentDateFormatter) {
+		this(fileContentDateFormatter, null);
+	}
+
+	/**
+	 * @param fileContentDateFormatter See {@link #getFileContentDateFormatter()}
+	 * @param possibleTagSubclasses    See {@link #addTagSubclass(Class)}
+	 */
+	public GSONDataStructureLoadingStrategy(DateTimeFormatter fileContentDateFormatter,
+			Class<ITimeMeasurementTag>[] possibleTagSubclasses) {
+		this.fileContentDateFormatter = fileContentDateFormatter;
+
+		if (possibleTagSubclasses != null) {
+			for (var ts : possibleTagSubclasses) {
+				this.addTagSubclass(ts);
+			}
+		}
+	}
+
+	/**
+	 * @return The {@link Gson} instance to use while parsing the data structure
+	 */
+	protected Gson buildGSON() {
+		return new GsonBuilder().registerTypeHierarchyAdapter(LocalDateTime.class, this.getDateDeserializer())
+				.registerTypeHierarchyAdapter(ITimeMeasurementTag.class, this.getTagDeserializer()).create();
+	}
+
+	/**
+	 * 
+	 * @param possibleTagSubclass A concrete (enum) sub-class of
+	 *                            {@link ITimeMeasurementTag} that should be
+	 *                            considered, while parsing the data structure.
+	 *                            These sub-classes have to be provided manually, as
+	 *                            there is no clean way to access all sub-types of a
+	 *                            given type programmatically
+	 */
+	public void addTagSubclass(Class<ITimeMeasurementTag> possibleTagSubclass) {
+		if (possibleTagSubclass != null) {
+			for (var ec : possibleTagSubclass.getEnumConstants()) {
+				this.possibleTags.add(ec);
+			}
+		}
+	}
+
+	/**
+	 * @implSpec Attempts to parse a {@link GSONDataStructure} instance from the
+	 *           file at the given absolute path. Throws
+	 *           {@link IllegalArgumentException} if an {@link IOException} occurs
+	 *           in the process.
+	 */
+	@Override
+	public GSONDataStructure load(Path pathToDataStructureFile) {
+		String fileContent;
+		try {
+			fileContent = Files.readString(pathToDataStructureFile);
+		} catch (IOException e) {
+			throw new IllegalArgumentException(e);
+		}
+
+		return this.buildGSON().fromJson(fileContent, GSONDataStructure.class);
+	}
+
+	/**
+	 * @return The date format, which will be used while parsing dates
+	 */
+	public DateTimeFormatter getFileContentDateFormatter() {
+		return this.fileContentDateFormatter;
+	}
+
+	private JsonDeserializer<LocalDateTime> getDateDeserializer() {
+		return new JsonDeserializer<LocalDateTime>() {
+			@Override
+			public LocalDateTime deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context)
+					throws JsonParseException {
+				var date = json.getAsString();
+				return LocalDateTime.from(getFileContentDateFormatter().parse(date));
+			}
+		};
+	}
+
+	private JsonDeserializer<ITimeMeasurementTag> getTagDeserializer() {
+		return new JsonDeserializer<ITimeMeasurementTag>() {
+			@Override
+			public ITimeMeasurementTag deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context)
+					throws JsonParseException {
+				var tagString = json.getAsString();
+				ITimeMeasurementTag tag = null;
+				for (var tagEnum : possibleTags) {
+					if (tagString.equals(tagEnum.toString())) {
+						tag = tagEnum;
+						break;
+					}
+				}
+				return tag;
+			}
+		};
+	}
+}
