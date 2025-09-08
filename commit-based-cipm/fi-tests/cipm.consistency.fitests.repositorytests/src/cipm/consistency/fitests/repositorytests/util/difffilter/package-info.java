69525d97e170493c9bc989223a682186f041de14
AlpTorac <a.torac@hotmail.com>
2025-09-08 14:30:21 +0200

    Edit commentary

--- commit-based-cipm/fi-tests/cipm.consistency.fitests.repositorytests/src/cipm/consistency/fitests/repositorytests/util/difffilter/package-info.java
+++ commit-based-cipm/fi-tests/cipm.consistency.fitests.repositorytests/src/cipm/consistency/fitests/repositorytests/util/difffilter/package-info.java
@@ -1,4 +1,5 @@
 /**
- * TODO Commentary
+ * Contains the means to filter GIT diff patches to get the actual changes from
+ * them. Currently only consists of {@link DiffFilter} and its unit test.
  */
 package cipm.consistency.fitests.repositorytests.util.difffilter;
