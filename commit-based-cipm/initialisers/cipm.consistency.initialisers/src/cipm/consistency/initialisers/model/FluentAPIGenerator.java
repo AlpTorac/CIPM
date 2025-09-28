b7875a309651d6d6d053e2b265fe351871d9aca3
AlpTorac <a.torac@hotmail.com>
2025-09-28 20:02:33 +0200

    (WIP) Add references, refactor

--- commit-based-cipm/initialisers/cipm.consistency.initialisers/src/cipm/consistency/initialisers/model/FluentAPIGenerator.java
+++ commit-based-cipm/initialisers/cipm.consistency.initialisers/src/cipm/consistency/initialisers/model/FluentAPIGenerator.java
@@ -15,6 +15,7 @@ import org.eclipse.emf.ecore.EModelElement;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EOperation;
 import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.emf.ecore.EReference;
 import org.eclipse.emf.ecore.EStructuralFeature;
 import org.eclipse.emf.ecore.EcoreFactory;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
@@ -66,16 +67,31 @@ public class FluentAPIGenerator {
 		javaPac.setNsPrefix(fluentAPIRootPacName);
 		javaPac.setNsURI(URI.createFileURI(fluentAPIRootPacName).toString());
 
+		// Add the super type for sub initialisation classes
+		var fluentAPISubClssSuperType = getFluentAPISuperType();
+		javaPac.getEClassifiers().add(fluentAPISubClssSuperType);
+
 		// Add the classes for individual model elements
-		javaPac.getEClassifiers().addAll(this.getFluentAPIClasses());
+		var fluentAPISubClss = this.getFluentAPIClasses(fluentAPISubClssSuperType);
+		fluentAPISubClss.forEach((cls) -> cls.getESuperTypes().add(fluentAPISubClssSuperType));
+		javaPac.getEClassifiers().addAll(fluentAPISubClss);
 
 		// Add the Class for composite Initialisation class
-		javaPac.getEClassifiers().add(this.generateFluentAPIGeneratorClass());
+		var fluentAPICls = this.getFluentAPIClass();
+		var subInitsRef = getSubInitialisationsReference(fluentAPISubClssSuperType);
+		fluentAPICls.getEStructuralFeatures().add(subInitsRef);
+		javaPac.getEClassifiers().add(fluentAPICls);
+
+		// Interconnected EReference setup
+		fluentAPISubClss.forEach((cls) -> {
+			var rootInitRef = getRootInitialisationReference(fluentAPICls);
+			cls.getEStructuralFeatures().add(rootInitRef);
+		});
 
 		return javaPac;
 	}
 
-	private List<EClass> getFluentAPIClasses() {
+	private List<EClass> getFluentAPIClasses(EClass fluentAPISubClssSuperType) {
 		var javaSubPackages = JavaPackageUtil.getAllSubPackages();
 		var fluentAPISubClss = new ArrayList<EClass>();
 
@@ -86,6 +102,16 @@ public class FluentAPIGenerator {
 		}
 
 		return fluentAPISubClss;
+	}
+
+	private static final String fluentAPISubClassesSuperTypeName = "IInitialisation";
+
+	private EClass getFluentAPISuperType() {
+		var superType = EcoreFactory.eINSTANCE.createEClass();
+		superType.setAbstract(true);
+		superType.setInterface(true);
+		superType.setName(fluentAPISubClassesSuperTypeName);
+		return superType;
 	}
 
 	private List<EClass> getFluentAPIClassesFor(EPackage javaSubPackage) {
@@ -103,25 +129,22 @@ public class FluentAPIGenerator {
 			apiSubCls.setName(eCls.getName() + fluentAPIClassSuffix);
 
 			// Add an EReference for covered model element types
-			var modelElementsToInitialiseRef = EcoreFactory.eINSTANCE.createEReference();
-			var modelElementsToInitialiseRefName = "initialisedClass";
-			modelElementsToInitialiseRef.setChangeable(true);
-			modelElementsToInitialiseRef.setContainment(false);
-			modelElementsToInitialiseRef.setEType(eCls.eClass().eClass());
-			modelElementsToInitialiseRef.setName(modelElementsToInitialiseRefName);
-			modelElementsToInitialiseRef.setLowerBound(1);
-			modelElementsToInitialiseRef.setUpperBound(1);
-			apiSubCls.getEStructuralFeatures().add(modelElementsToInitialiseRef);
+			apiSubCls.getEStructuralFeatures().add(getInitialisedClassReference(eCls));
+
+			// Add an EReference for all instantiated elements
+			apiSubCls.getEStructuralFeatures().add(getCreatedElementsReference(eCls));
+
 			// TODO Find a way to set the EReference above most likely through a protected
 			// init() method in constructor (?). apiSubCls.eSet(...) does not work
 
-			// Add initialisation operations
+			// Add initialisation operations "newX()"
 			var newOp = getNewOperationFor(eCls);
 			if (newOp != null) {
 				allNewOperations.add(newOp);
 				apiSubCls.getEOperations().add(newOp);
 			}
 
+			// Add modification methods "withX_Feat(obj, featValParam)"
 			var withOps = getWithOperationsFor(eCls);
 			if (withOps != null)
 				apiSubCls.getEOperations().addAll(withOps);
@@ -132,9 +155,72 @@ public class FluentAPIGenerator {
 		return clss;
 	}
 
+	private static final String rootInitialisationReferenceName = "rootInitialisation";
+	private static final String subInitialisationsReferenceName = "subInitialisations";
+
+	private static EReference getSubInitialisationsReference(EClass subInitialisationClsSuperType) {
+		var subInitialisationsRef = EcoreFactory.eINSTANCE.createEReference();
+		subInitialisationsRef.setChangeable(true);
+		subInitialisationsRef.setContainment(true);
+		subInitialisationsRef.setEType(subInitialisationClsSuperType);
+		subInitialisationsRef.setName(subInitialisationsReferenceName);
+		subInitialisationsRef.setLowerBound(0);
+		subInitialisationsRef.setUpperBound(EReference.UNBOUNDED_MULTIPLICITY);
+		return subInitialisationsRef;
+	}
+
+	private static EReference getRootInitialisationReference(EClass rootInitialisationCls) {
+		var rootInitialisationRef = EcoreFactory.eINSTANCE.createEReference();
+		rootInitialisationRef.setChangeable(true);
+		rootInitialisationRef.setContainment(false);
+		rootInitialisationRef.setEType(rootInitialisationCls);
+		rootInitialisationRef.setName(rootInitialisationReferenceName);
+		rootInitialisationRef.setLowerBound(1);
+		rootInitialisationRef.setUpperBound(1);
+		return rootInitialisationRef;
+	}
+
+	private static final String createdElementsReferenceName = "createdObjs";
+
+	/**
+	 * @return An EReference that is to reference the EObjects created via "newX()"
+	 *         methods
+	 */
+	private static EReference getCreatedElementsReference(EClass eClsToInit) {
+		var createdElementsRef = EcoreFactory.eINSTANCE.createEReference();
+		createdElementsRef.setChangeable(true);
+		createdElementsRef.setContainment(false);
+		createdElementsRef.setEType(eClsToInit);
+		createdElementsRef.setName(createdElementsReferenceName);
+		createdElementsRef.setLowerBound(0);
+		createdElementsRef.setUpperBound(EReference.UNBOUNDED_MULTIPLICITY);
+		return createdElementsRef;
+	}
+
+	private static final String initialisedClassReferenceName = "initialisedClass";
+
+	/**
+	 * @return An EReference that is to reference the EClass of the instantiated
+	 *         type
+	 */
+	private static EReference getInitialisedClassReference(EClass eClsToInit) {
+		var initialisedClassRef = EcoreFactory.eINSTANCE.createEReference();
+		initialisedClassRef.setChangeable(true);
+		initialisedClassRef.setContainment(false);
+		initialisedClassRef.setEType(eClsToInit.eClass());
+		initialisedClassRef.setName(initialisedClassReferenceName);
+		initialisedClassRef.setLowerBound(1);
+		initialisedClassRef.setUpperBound(1);
+		return initialisedClassRef;
+	}
+
 	private static boolean isConcrete(EClass elemToInit) {
 		return !elemToInit.isAbstract() && !elemToInit.isInterface();
 	}
+
+	private static final String newOperationNamePrefix = "new";
+
+	private static final String eOperationBodyKey = "body";
 
 	private static EOperation getNewOperationFor(EClass elemToInit) {
 		if (!isConcrete(elemToInit))
@@ -142,20 +228,26 @@ public class FluentAPIGenerator {
 
 		var op = EcoreFactory.eINSTANCE.createEOperation();
 		var elemInstanceName = elemToInit.getInstanceClass().getSimpleName();
-		op.setName("new" + elemInstanceName);
+		op.setName(newOperationNamePrefix + elemInstanceName);
 		op.setEType(elemToInit);
 
 		// Add the method body
 		var anno = EcoreFactory.eINSTANCE.createEAnnotation();
 		anno.setSource(genModelURL);
-		var bodyKey = "body";
 		// TODO Add hooks to creation methods (?)
-		var bodyValue = String.format("return %s.eINSTANCE.create%s();",
-				getFactoryNameForEPackage(elemToInit.getEPackage()), elemInstanceName);
-		anno.getDetails().put(bodyKey, bodyValue);
+		var bodyValue = getNewOperationBodyFor(elemToInit);
+		anno.getDetails().put(eOperationBodyKey, bodyValue);
 
 		op.getEAnnotations().add(anno);
 		return op;
+	}
+
+	private static String getNewOperationBodyFor(EClass elemToInit) {
+		// TODO Add the created element to getCreatedElementsReference(elemToInit) and
+		// return "this"
+
+		return String.format("return %s.eINSTANCE.create%s();", getFactoryNameForEPackage(elemToInit.getEPackage()),
+				elemToInit.getInstanceClass().getSimpleName());
 	}
 
 	private static List<EOperation> getWithOperationsFor(EClass elemToInit) {
@@ -169,6 +261,7 @@ public class FluentAPIGenerator {
 		// Java model elements directly
 		for (var feat : allFeats.stream()
 				.filter((attr) -> !attr.getEContainingClass().getInstanceClass().isAssignableFrom(Commentable.class))
+				// Enable if only features with EObject values are to be considered
 //				.filter((attr) -> EObject.class.isAssignableFrom(attr.getEType().getInstanceClass()))
 				.filter((attr) -> attr.isChangeable()).toArray(EStructuralFeature[]::new)) {
 
@@ -179,20 +272,23 @@ public class FluentAPIGenerator {
 
 			// Add obj param to operation, which is the element to initialise
 			var objParam = EcoreFactory.eINSTANCE.createEParameter();
+			var objParamName = "objToInit";
 			objParam.setEType(elemToInit);
-			objParam.setName("objToInit");
+			objParam.setName(objParamName);
 			objParam.setLowerBound(1);
 			objParam.setUpperBound(1);
 			op.getEParameters().add(objParam);
 
 			// Add feature param to operation, which will be the new value of obj.feat
 			var featValParam = EcoreFactory.eINSTANCE.createEParameter();
+			var featValParamName = "";
 			featValParam.setEType(feat.getEType());
 			if (!feat.isMany()) {
-				featValParam.setName("newFeatVal");
+				featValParamName = "newFeatVal";
 			} else {
-				featValParam.setName("addToFeatVal");
+				featValParamName = "addToFeatVal";
 			}
+			featValParam.setName(featValParamName);
 			featValParam.setLowerBound(1);
 			featValParam.setUpperBound(1);
 			op.getEParameters().add(featValParam);
@@ -201,16 +297,8 @@ public class FluentAPIGenerator {
 			var anno = EcoreFactory.eINSTANCE.createEAnnotation();
 			anno.setSource(genModelURL);
 			var bodyKey = "body";
-			// TODO Add hooks to creation methods (?)
-			var bodyValue = "";
-			if (!feat.isMany()) {
-				bodyValue = String.format(
-						"objToInit.eSet(objToInit.eClass().getEStructuralFeature(\"%s\"), newFeatVal);",
-						feat.getName());
-			} else {
-				bodyValue = String.format("var val = objToInit.eGet(objToInit.eClass().getEStructuralFeature(\"%s\"));"
-						+ System.lineSeparator() + "((EList) val).add(addToFeatVal);", feat.getName());
-			}
+			var bodyValue = getWithOperationBodyFor(objParamName, featValParamName, feat);
+
 			anno.getDetails().put(bodyKey, bodyValue);
 
 			op.getEAnnotations().add(anno);
@@ -220,11 +308,28 @@ public class FluentAPIGenerator {
 		return ops;
 	}
 
+	private static String getWithOperationBodyFor(String objParamName, String featureValParamName,
+			EStructuralFeature feat) {
+		/*
+		 * TODO Add hooks to creation methods for validation and assertions
+		 */
+
+		if (!feat.isMany()) {
+			return String.format("%s.eSet(%s.eClass().getEStructuralFeature(\"%s\"), %s);", objParamName, objParamName,
+					feat.getName(), featureValParamName);
+		} else {
+			return String.format(
+					"var val = %s.eGet(%s.eClass().getEStructuralFeature(\"%s\"));" + System.lineSeparator()
+							+ "((EList) val).add(%s);",
+					objParamName, objParamName, feat.getName(), featureValParamName);
+		}
+	}
+
 	private static String getFactoryNameForEPackage(EPackage pac) {
 		return pac.getEFactoryInstance().getClass().getName().replaceFirst(".impl", "").replace("Impl", "");
 	}
 
-	private EClass generateFluentAPIGeneratorClass() {
+	private EClass getFluentAPIClass() {
 		var fluentAPICls = EcoreFactory.eINSTANCE.createEClass();
 		fluentAPICls.setAbstract(false);
 		fluentAPICls.setInterface(false);
