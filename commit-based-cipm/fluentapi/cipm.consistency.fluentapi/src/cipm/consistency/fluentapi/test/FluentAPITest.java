ea13cdc4de887699d8f042b22698a55b4654213f
AlpTorac <a.torac@hotmail.com>
2025-10-28 19:05:51 +0100

    (WIP, TO BE TESTED FURTHER) Fix bidirectional references in fluent API
    (WIP) Add more tests for FluentAPI

--- commit-based-cipm/fluentapi/cipm.consistency.fluentapi/src/cipm/consistency/fluentapi/test/FluentAPITest.java
+++ commit-based-cipm/fluentapi/cipm.consistency.fluentapi/src/cipm/consistency/fluentapi/test/FluentAPITest.java
@@ -176,29 +176,86 @@ public class FluentAPITest {
 	}
 
 	@Test
-	public void apiTest_BidirectionalReferences_OneToMany() {
+	public void apiTest_BidirectionalReferences_OneToMany_OneContainmentPossible() {
 		var api = ApiFactory.eINSTANCE.createFluentEObjectAPI();
 
 		var modName = "modName";
-		var modNss = toEList("ns1", "ns2");
-
 		var pacName = "pacName";
 
-		var mod = api.newModule().withName(modName).withAddedNamespaces(modNss).createNow();
+		var mod = api.newModule().withName(modName).createNow();
 		var pac = api.newPackage().withName(pacName).withModule(mod).createNow();
 
 		Assertions.assertInstanceOf(org.emftext.language.java.containers.Module.class, mod);
 		Assertions.assertEquals(modName, mod.getName());
-		Assertions.assertEquals(modNss.size(), mod.getNamespaces().size());
-		Assertions.assertFalse(mod.getNamespaces().retainAll(modNss));
 
-		// TODO Check pac.module
-		
 		Assertions.assertEquals(1, mod.getPackages().size());
 		Assertions.assertEquals(pac, mod.getPackages().get(0));
 		Assertions.assertEquals(pacName, pac.getName());
-		Assertions.assertEquals(modNss.size(), pac.getNamespaces().size());
-		Assertions.assertFalse(pac.getNamespaces().retainAll(modNss));
+		Assertions.assertEquals(mod, pac.getModule());
+	}
+
+	@Test
+	public void apiTest_BidirectionalReferences_ManyToOne_OneContainmentPossible() {
+		var api = ApiFactory.eINSTANCE.createFluentEObjectAPI();
+
+		var modName = "modName";
+		var pacName = "pacName";
+
+		var pac = api.newPackage().withName(pacName).createNow();
+		var mod = api.newModule().withName(modName).withAddedPackages(pac).createNow();
+
+		Assertions.assertInstanceOf(org.emftext.language.java.containers.Module.class, mod);
+		Assertions.assertEquals(modName, mod.getName());
+
+		Assertions.assertEquals(1, mod.getPackages().size());
+		Assertions.assertEquals(pac, mod.getPackages().get(0));
+		Assertions.assertEquals(pacName, pac.getName());
+		Assertions.assertEquals(mod, pac.getModule());
+	}
+
+	// TODO Turn this into a test case
+//	class cls1 {
+//		class cls2 extends cls1 {
+//
+//		}
+//	}
+
+	@Test
+	public void apiTest_Nesting() {
+		var api = ApiFactory.eINSTANCE.createFluentEObjectAPI();
+
+		var innerClsName = "inner";
+		var outerClsName = "outer";
+
+		var innerCls = api.newClass().withName(innerClsName).createNow();
+		var outerCls = api.newClass().withName(outerClsName).withAddedMembers(innerCls).createNow();
+
+		Assertions.assertEquals(1, outerCls.getMembers().size());
+		Assertions.assertEquals(innerCls, outerCls.getMembers().get(0));
+		Assertions.assertEquals(0, outerCls.getDefaultMembers().size());
+		Assertions.assertNull(outerCls.eContainer());
+
+		Assertions.assertEquals(0, innerCls.getMembers().size());
+		Assertions.assertEquals(0, innerCls.getDefaultMembers().size());
+		Assertions.assertEquals(outerCls, innerCls.eContainer());
+	}
+
+	@Test
+	public void apiTest_TwoContainmentFeaturesWithSameType() {
+		var api = ApiFactory.eINSTANCE.createFluentEObjectAPI();
+
+		var arrDimBefore = api.newArrayDimension().createNow();
+		var arrDimAfter = api.newArrayDimension().createNow();
+		var ptr = api.newPrimitiveTypeReference().withAddedArrayDimensionsBefore(arrDimBefore)
+				.withAddedArrayDimensionsAfter(arrDimAfter).createNow();
+
+		Assertions.assertEquals(1, ptr.getArrayDimensionsBefore().size());
+		Assertions.assertEquals(arrDimBefore, ptr.getArrayDimensionsBefore().get(0));
+		Assertions.assertEquals(ptr, arrDimBefore.eContainer());
+
+		Assertions.assertEquals(1, ptr.getArrayDimensionsAfter().size());
+		Assertions.assertEquals(arrDimAfter, ptr.getArrayDimensionsAfter().get(0));
+		Assertions.assertEquals(ptr, arrDimAfter.eContainer());
 	}
 
 	@Test
