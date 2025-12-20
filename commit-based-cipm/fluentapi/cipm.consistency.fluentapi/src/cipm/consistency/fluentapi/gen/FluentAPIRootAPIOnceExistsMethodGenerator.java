ef10e6c651c21668cfdb885b9b2fb92ad436e58c
AlpTorac <a.torac@hotmail.com>
2025-12-20 14:39:04 +0100

    (WIP) Use Runnable instead of Consumer in onceExists
    Consumer requires messy type casting, whereas Runnable is more acceptable
    
    With the help of modifyMarkedX methods, this is the cleaner alternative for now

--- commit-based-cipm/fluentapi/cipm.consistency.fluentapi/src/cipm/consistency/fluentapi/gen/FluentAPIRootAPIOnceExistsMethodGenerator.java
+++ commit-based-cipm/fluentapi/cipm.consistency.fluentapi/src/cipm/consistency/fluentapi/gen/FluentAPIRootAPIOnceExistsMethodGenerator.java
@@ -12,18 +12,18 @@ public class FluentAPIRootAPIOnceExistsMethodGenerator {
 	private static final String genModelURL = "http://www.eclipse.org/emf/2002/GenModel";
 
 	private static final String markKeyParameterName = "markKey";
-	private static final String consumerParameterName = "toDoOnceExists";
+	private static final String runnableParameterName = "toDoOnceExists";
 
 	private static final String onceExistsMethodNameTemplate = "onceExists";
 	private static final String onceExistsMethodBodyTemplate = FluentAPIMethodsUtil.joinLOC(
 			// %s: Mark key parameter name
-			// %s: Consumer parameter name (what to do)
-			FluentAPIOnceExistsExtension.class.getName() + ".addOnceExists(this, %s, (java.util.function.Consumer) %s)",
+			// %s: Runnable parameter name (what to do)
+			FluentAPIOnceExistsExtension.class.getName() + ".addOnceExists(this, %s, (java.lang.Runnable) %s)",
 			"return this");
 
 	public EOperation generateOnceExistsMethod(EClass rootAPIECls) {
 		var keyParam = getMarkKeyParam();
-		var consumerParam = getConsumerParam();
+		var consumerParam = getRunnableParam();
 		return FluentAPIGenerationUtil.generateEOperationWithBody(onceExistsMethodNameTemplate, genModelURL,
 				rootAPIECls, String.format(onceExistsMethodBodyTemplate, keyParam.getName(), consumerParam.getName()),
 				keyParam, consumerParam);
@@ -34,8 +34,8 @@ public class FluentAPIRootAPIOnceExistsMethodGenerator {
 				EcorePackage.Literals.EJAVA_OBJECT);
 	}
 
-	private EParameter getConsumerParam() {
-		return FluentAPIGenerationUtil.generateSingleValuedEParameter(consumerParameterName,
+	private EParameter getRunnableParam() {
+		return FluentAPIGenerationUtil.generateSingleValuedEParameter(runnableParameterName,
 				EcorePackage.Literals.EJAVA_OBJECT);
 	}
 }
--- commit-based-cipm/fluentapi/cipm.consistency.fluentapi/src/cipm/consistency/fluentapi/test/FluentAPIOnceExistsTest.java
+++ commit-based-cipm/fluentapi/cipm.consistency.fluentapi/src/cipm/consistency/fluentapi/test/FluentAPIOnceExistsTest.java
@@ -1,7 +1,5 @@
 package cipm.consistency.fluentapi.test;
 
-import java.util.function.Consumer;
-
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -33,14 +31,14 @@ public class FluentAPIOnceExistsTest {
 		api.newClass().withName(outerClsName);
 
 		api.newClass().withName(cls1Name).markCurrent(cls1Name);
-		api.onceExists(cls2Name, (Consumer<org.emftext.language.java.classifiers.Class>) (cls2) -> api.modifyClass(cls2)
+		api.onceExists(cls2Name, (Runnable) () -> api.modifyMarkedClass(cls2Name)
 				.withExtends(api.newClassifierReference().withTarget(api.getMarkedClass(cls1Name)).createNow()));
 
 //		api.onceExists(cls2Name,
 //				toRunnable((org.emftext.language.java.classifiers.Class) api.getMarked(cls1Name), this::cons));
 
 		api.newClass().withName(cls2Name).markCurrent(cls2Name);
-		api.onceExists(cls1Name, (Consumer<org.emftext.language.java.classifiers.Class>) (cls1) -> api.modifyClass(cls1)
+		api.onceExists(cls1Name, (Runnable) () -> api.modifyMarkedClass(cls1Name)
 				.withExtends(api.newClassifierReference().withTarget(api.getMarkedClass(cls2Name)).createNow()));
 
 		api.continueOldestClass().withAddedMembers(FluentAPITestUtils.toEList(api.continueClassFromStart(1).createNow(),
@@ -59,52 +57,4 @@ public class FluentAPIOnceExistsTest {
 		Assertions.assertEquals(cls2Name, cls2.getName());
 		Assertions.assertEquals(cls1Name, cls2.getExtends().getPureClassifierReference().getTarget().getName());
 	}
-
-	@Test
-	public void delayedElementConstruction_Alternative() {
-		var api = ApiFactory.eINSTANCE.createFluentEObjectAPI();
-		var outerClsName = "OuterCls";
-		var cls1Name = "Cls1";
-		var cls2Name = "Cls2";
-
-		api.newClass().withName(outerClsName);
-
-		api.newClass().withName(cls1Name).markCurrent(cls1Name);
-		api.onceExists(cls2Name, (Consumer<org.emftext.language.java.classifiers.Class>) (cls2) -> api.modifyClass(cls2)
-				.withExtends(api.newClassifierReference().withTarget(api.getMarkedClass(cls1Name)).createNow()));
-
-//		api.onceExists(cls2Name, (Runnable) () -> api.modifyClass(api.getMarkedClass(cls2Name))
-//				.withExtends(api.newClassifierReference().withTarget(api.getMarkedClass(cls2Name)).createNow()));
-
-		api.newClass().withName(cls2Name).markCurrent(cls2Name);
-		api.onceExists(cls1Name, toRunnable(api.getMarkedClass(cls2Name), (cls1) -> api.modifyClass(cls1)
-				.withExtends(api.newClassifierReference().withTarget(api.getMarkedClass(cls2Name)).createNow())));
-
-		api.continueOldestClass().withAddedMembers(FluentAPITestUtils.toEList(api.continueClassFromStart(1).createNow(),
-				api.continueClassFromStart(1).createNow()));
-
-		var outerCls = api.continueOldestClass().createNow();
-
-		Assertions.assertInstanceOf(org.emftext.language.java.classifiers.Class.class, outerCls);
-		Assertions.assertEquals(2, outerCls.getMembers().size());
-
-		var cls1 = (org.emftext.language.java.classifiers.Class) outerCls.getMembers().get(0);
-		Assertions.assertEquals(cls1Name, cls1.getName());
-		Assertions.assertEquals(cls2Name, cls1.getExtends().getPureClassifierReference().getTarget().getName());
-
-		var cls2 = (org.emftext.language.java.classifiers.Class) outerCls.getMembers().get(1);
-		Assertions.assertEquals(cls2Name, cls2.getName());
-		Assertions.assertEquals(cls1Name, cls2.getExtends().getPureClassifierReference().getTarget().getName());
-	}
-
-	public <T> Consumer<T> toRunnable(Consumer<T> con) {
-		return con;
-	}
-
-	public <T> Consumer<T> toRunnable(T arg, Consumer<T> con) {
-		return con;
-	}
-
-	public void cons(org.emftext.language.java.classifiers.Class cls) {
-	};
 }
