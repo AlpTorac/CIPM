94492208b584ae80f67a183ea08da6057ccb697c
AlpTorac <a.torac@hotmail.com>
2026-01-15 18:50:39 +0100

    (WIP) Add overloading creation methods to fluent api class
    to reduce the length of method chain to create a basic EObject

--- commit-based-cipm/fluentapi/cipm.consistency.fluentapi/src/cipm/consistency/fluentapi/gen/FluentAPITargetMetamodelFeatureFilter.java
+++ commit-based-cipm/fluentapi/cipm.consistency.fluentapi/src/cipm/consistency/fluentapi/gen/FluentAPITargetMetamodelFeatureFilter.java
@@ -1,11 +1,24 @@
 package cipm.consistency.fluentapi.gen;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EStructuralFeature;
 
 public abstract class FluentAPITargetMetamodelFeatureFilter {
 	public abstract boolean isFeatureEligible(EClass holderOfFeat, EStructuralFeature feat);
+
+	public List<EStructuralFeature> getModifiableFeatures(EClass eObjEClass) {
+		return eObjEClass.getEAllStructuralFeatures().stream().filter((f) -> this.isFeatureEligible(eObjEClass, f))
+				.collect(Collectors.toList());
+	}
+
+	public long getModifiableFeatureCount(EClass eObjEClass) {
+		return eObjEClass.getEAllStructuralFeatures().stream().filter((f) -> this.isFeatureEligible(eObjEClass, f))
+				.count();
+	}
 
 	public boolean hasModifiableFeatures(EClass eObjEClass) {
 		return eObjEClass.getEAllStructuralFeatures().stream().anyMatch((f) -> this.isFeatureEligible(eObjEClass, f));
