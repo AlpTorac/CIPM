1441554ab81420f9135e5c794c74c4e5e53fc18e
AlpTorac <a.torac@hotmail.com>
2026-01-18 19:07:23 +0100

    (WIP) Adapt fitests to fluent api

--- commit-based-cipm/fi-tests/cipm.consistency.fitests/src/cipm/consistency/fitests/similarity/jamopp/unittests/impltests/SwitchTest.java
+++ commit-based-cipm/fi-tests/cipm.consistency.fitests/src/cipm/consistency/fitests/similarity/jamopp/unittests/impltests/SwitchTest.java
@@ -1,58 +1,50 @@
 package cipm.consistency.fitests.similarity.jamopp.unittests.impltests;
 
+import java.util.function.Supplier;
+
 import org.emftext.language.java.expressions.Expression;
 import org.emftext.language.java.statements.StatementsPackage;
-import org.emftext.language.java.statements.Switch;
 import org.emftext.language.java.statements.SwitchCase;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import cipm.consistency.fitests.similarity.jamopp.AbstractJaMoPPSimilarityTest;
-import cipm.consistency.fitests.similarity.jamopp.unittests.UsesSwitchCases;
-import cipm.consistency.initialisers.jamopp.statements.SwitchInitialiser;
 
-public class SwitchTest extends AbstractJaMoPPSimilarityTest implements UsesSwitchCases {
-	protected Switch initElement(SwitchCase[] cases, Expression var) {
-		var swInit = new SwitchInitialiser();
-		var sw = swInit.instantiate();
-		Assertions.assertTrue(swInit.addCases(sw, cases));
-		Assertions.assertTrue(swInit.setVariable(sw, var));
-		return sw;
-	}
+public class SwitchTest extends AbstractJaMoPPSimilarityTest {
+	private final Supplier<SwitchCase> case1 = () -> getAPI().createNewNormalSwitchCase();
+	private final Supplier<SwitchCase> case2 = () -> getAPI().createNewDefaultSwitchCase();
+
+	private final Supplier<Expression> variable1 = () -> getAPI().newDecimalIntegerLiteral(1);
+	private final Supplier<Expression> variable2 = () -> getAPI().newDecimalIntegerLiteral(2);
 
 	@Test
 	public void testCase() {
-		var objOne = this.initElement(new SwitchCase[] { this.createEmptyNSC() }, null);
-		var objTwo = this.initElement(new SwitchCase[] { this.createMinimalNSC() }, null);
-
-		this.testSimilarity(objOne, objTwo, StatementsPackage.Literals.SWITCH__CASES);
+		this.testSimilarity(getAPI().newSwitch().withAddedCases(case1.get()).createNow(),
+				getAPI().newSwitch().withAddedCases(case2.get()).createNow(), StatementsPackage.Literals.SWITCH__CASES);
 	}
 
 	@Test
 	public void testCaseSize() {
-		var objOne = this.initElement(new SwitchCase[] { this.createEmptyNSC(), this.createMinimalNSC() }, null);
-		var objTwo = this.initElement(new SwitchCase[] { this.createEmptyNSC() }, null);
-
-		this.testSimilarity(objOne, objTwo, StatementsPackage.Literals.SWITCH__CASES);
+		this.testSimilarity(
+				getAPI().newSwitch().withAddedCases(new SwitchCase[] { case1.get(), case2.get() }).createNow(),
+				getAPI().newSwitch().withAddedCases(case1.get()).createNow(), StatementsPackage.Literals.SWITCH__CASES);
 	}
 
 	@Test
 	public void testCaseNullCheck() {
-		this.testSimilarityNullCheck(this.initElement(new SwitchCase[] { this.createEmptyNSC() }, null),
-				new SwitchInitialiser(), false, StatementsPackage.Literals.SWITCH__CASES);
+		this.testSimilarityNullCheck(getAPI().newSwitch().withAddedCases(case1.get()).createNow(),
+				StatementsPackage.Literals.SWITCH__CASES);
 	}
 
 	@Test
 	public void testVariable() {
-		var objOne = this.initElement(null, this.createMinimalSR("str1"));
-		var objTwo = this.initElement(null, this.createMinimalSR("str2"));
-
-		this.testSimilarity(objOne, objTwo, StatementsPackage.Literals.SWITCH__VARIABLE);
+		this.testSimilarity(getAPI().newSwitch().withVariable(variable1.get()).createNow(),
+				getAPI().newSwitch().withVariable(variable2.get()).createNow(),
+				StatementsPackage.Literals.SWITCH__VARIABLE);
 	}
 
 	@Test
 	public void testVariableNullCheck() {
-		this.testSimilarityNullCheck(this.initElement(null, this.createMinimalSR("str1")), new SwitchInitialiser(),
-				false, StatementsPackage.Literals.SWITCH__VARIABLE);
+		this.testSimilarityNullCheck(getAPI().newSwitch().withVariable(variable1.get()).createNow(),
+				StatementsPackage.Literals.SWITCH__VARIABLE);
 	}
 }
--- commit-based-cipm/fi-tests/cipm.consistency.fitests/src/cipm/consistency/fitests/similarity/jamopp/unittests/impltests/SynchronizedBlockTest.java
+++ commit-based-cipm/fi-tests/cipm.consistency.fitests/src/cipm/consistency/fitests/similarity/jamopp/unittests/impltests/SynchronizedBlockTest.java
@@ -1,35 +1,27 @@
 package cipm.consistency.fitests.similarity.jamopp.unittests.impltests;
 
+import java.util.function.Supplier;
+
 import org.emftext.language.java.expressions.Expression;
 import org.emftext.language.java.statements.StatementsPackage;
-import org.emftext.language.java.statements.SynchronizedBlock;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import cipm.consistency.fitests.similarity.jamopp.AbstractJaMoPPSimilarityTest;
-import cipm.consistency.fitests.similarity.jamopp.unittests.UsesExpressions;
-import cipm.consistency.initialisers.jamopp.statements.SynchronizedBlockInitialiser;
 
-public class SynchronizedBlockTest extends AbstractJaMoPPSimilarityTest implements UsesExpressions {
-	protected SynchronizedBlock initElement(Expression lockProvider) {
-		var sbInit = new SynchronizedBlockInitialiser();
-		var sb = sbInit.instantiate();
-		Assertions.assertTrue(sbInit.setLockProvider(sb, lockProvider));
-		return sb;
-	}
+public class SynchronizedBlockTest extends AbstractJaMoPPSimilarityTest {
+	private final Supplier<Expression> lockProvider1 = () -> getAPI().newDecimalIntegerLiteral(1);
+	private final Supplier<Expression> lockProvider2 = () -> getAPI().newDecimalIntegerLiteral(2);
 
 	@Test
 	public void testLockProvider() {
-		var objOne = this.initElement(this.createDecimalIntegerLiteral(1));
-		var objTwo = this.initElement(this.createDecimalIntegerLiteral(2));
-
-		this.testSimilarity(objOne, objTwo, StatementsPackage.Literals.SYNCHRONIZED_BLOCK__LOCK_PROVIDER);
+		this.testSimilarity(getAPI().newSynchronizedBlock().withLockProvider(lockProvider1.get()).createNow(),
+				getAPI().newSynchronizedBlock().withLockProvider(lockProvider2.get()).createNow(),
+				StatementsPackage.Literals.SYNCHRONIZED_BLOCK__LOCK_PROVIDER);
 	}
 
 	@Test
 	public void testLockProviderNullCheck() {
-		this.testSimilarityNullCheck(this.initElement(this.createDecimalIntegerLiteral(1)),
-				new SynchronizedBlockInitialiser(), false,
+		this.testSimilarityNullCheck(getAPI().newSynchronizedBlock().withLockProvider(lockProvider1.get()).createNow(),
 				StatementsPackage.Literals.SYNCHRONIZED_BLOCK__LOCK_PROVIDER);
 	}
 }
