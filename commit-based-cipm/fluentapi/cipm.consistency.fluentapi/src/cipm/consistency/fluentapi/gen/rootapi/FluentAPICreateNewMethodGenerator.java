1bba34f3a04ca87cf549f076475ac72af9ac128c
AlpTorac <a.torac@hotmail.com>
2026-01-18 21:06:45 +0100

    Add overloading method to newX
    Add createNewX method

--- commit-based-cipm/fluentapi/cipm.consistency.fluentapi/src/cipm/consistency/fluentapi/gen/rootapi/FluentAPICreateNewMethodGenerator.java
+++ commit-based-cipm/fluentapi/cipm.consistency.fluentapi/src/cipm/consistency/fluentapi/gen/rootapi/FluentAPICreateNewMethodGenerator.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EOperation;
+import org.eclipse.emf.ecore.EcoreFactory;
+import org.eclipse.emf.ecore.EcorePackage;
 
 import cipm.consistency.fluentapi.gen.FluentAPIGenerationUtil;
 import cipm.consistency.fluentapi.gen.methods.FluentAPIMethodsUtil;
@@ -23,8 +25,17 @@ public class FluentAPICreateNewMethodGenerator {
 	private static final String createNewXMethodBodyTemplate = FluentAPIMethodsUtil
 			.joinLOC("return (%s) this.getInitialisationForX(%s.class).createNow()");
 
+	private static final String createNewXWithClassParamMethodName = "createNewX";
+	private static final String createNewXWithClassParamTypeParamName = "T";
+	private static final String createNewXWithClassParamParamName = "eObjCls";
+
+	private static final String createNewXWithClassParamMethodBodyTemplate = FluentAPIMethodsUtil
+			.joinLOC("return (%s) this.getInitialisationForX(%s).createNow()");
+
 	public List<EOperation> generateAllCreateNewMethods(List<EClass> eObjEClss) {
 		var ops = new ArrayList<EOperation>();
+
+		ops.add(generateGenericCreateNewMethod());
 
 		for (var eObjEClass : eObjEClss) {
 			ops.add(generateCreateNewMethod(eObjEClass));
@@ -38,5 +49,41 @@ public class FluentAPICreateNewMethodGenerator {
 				String.format(createNewXMethodNameTemplate, eObjEClass.getInstanceClass().getSimpleName()), eObjEClass,
 				String.format(createNewXMethodBodyTemplate, eObjEClass.getInstanceClass().getName(),
 						eObjEClass.getInstanceClass().getName()));
+	}
+
+	private EOperation generateGenericCreateNewMethod() {
+		// TODO Clean up
+
+		// Goal: <T> T createNewX(Class<T> createNewXWithClassParamParamName)
+
+		var typeParam = EcoreFactory.eINSTANCE.createETypeParameter();
+		typeParam.setName(createNewXWithClassParamTypeParamName);
+
+		// Make sure to create 2 generic types, one for the method parameter (Class<T>)
+		// and one for the return type of the method (T)
+
+		var genericParamTypeForJavaClass = EcoreFactory.eINSTANCE.createEGenericType();
+		genericParamTypeForJavaClass.setETypeParameter(typeParam);
+
+		var genericClassType = EcoreFactory.eINSTANCE.createEGenericType();
+		genericClassType.setEClassifier(EcorePackage.Literals.EJAVA_CLASS);
+		genericClassType.getETypeArguments().add(genericParamTypeForJavaClass);
+
+		var genericParamTypeForOp = EcoreFactory.eINSTANCE.createEGenericType();
+		genericParamTypeForOp.setETypeParameter(typeParam);
+
+		var param = EcoreFactory.eINSTANCE.createEParameter();
+		param.setName(createNewXWithClassParamParamName);
+		param.setEGenericType(genericClassType);
+		param.setLowerBound(1);
+		param.setUpperBound(1);
+
+		var op = FluentAPIGenerationUtil.generateEOperationWithBody(createNewXWithClassParamMethodName, null,
+				String.format(createNewXWithClassParamMethodBodyTemplate, typeParam.getName(), param.getName()));
+		op.setEGenericType(genericParamTypeForOp);
+		op.getETypeParameters().add(typeParam);
+		op.getEParameters().add(param);
+
+		return op;
 	}
 }
