c26660f00e20d37d6e4d0bf0a7c59dabf8f4cf82
AlpTorac <a.torac@hotmail.com>
2026-01-20 15:45:31 +0100

    Adjust JaMoPPArguments method signatures

--- commit-based-cipm/fi-tests/cipm.consistency.fitests/src/cipm/consistency/fitests/similarity/jamopp/JaMoPPArguments.java
+++ commit-based-cipm/fi-tests/cipm.consistency.fitests/src/cipm/consistency/fitests/similarity/jamopp/JaMoPPArguments.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import org.eclipse.emf.ecore.EObject;
 import org.junit.jupiter.params.provider.Arguments;
 
 import cipm.consistency.fluentapi.api.ApiFactory;
@@ -12,14 +13,16 @@ import cipm.consistency.fluentapi.api.FluentEObjectAPI;
 
 public final class JaMoPPArguments {
 	private static final FluentEObjectAPI api = ApiFactory.eINSTANCE.createFluentEObjectAPI();
+	@SuppressWarnings("unchecked")
 	private static final List<Class<?>> supportedClasses = api.getAllSupportedClasses();
 
-	public static List<Class<?>> getAllConcreteClasses() {
-		return List.copyOf(supportedClasses);
+	@SuppressWarnings("unchecked")
+	public static List<Class<? extends EObject>> getAllConcreteClasses() {
+		return supportedClasses.stream().map((cls) -> (Class<? extends EObject>) cls).collect(Collectors.toList());
 	}
 
 	@SuppressWarnings("unchecked")
-	public static <T> List<Class<? extends T>> getAllConcreteClassesBySuper(Class<T> superType) {
+	public static <T extends EObject> List<Class<? extends T>> getAllConcreteClassesBySuper(Class<T> superType) {
 		return supportedClasses.stream().filter((cls) -> superType.isAssignableFrom(cls))
 				.map((cls) -> (Class<? extends T>) cls).collect(Collectors.toList());
 	}
@@ -28,11 +31,11 @@ public final class JaMoPPArguments {
 		return toArgStream(getAllConcreteClasses());
 	}
 
-	public static Stream<Arguments> getAllConcreteClassesBySuperAsArgs(Class<?> superType) {
+	public static Stream<Arguments> getAllConcreteClassesBySuperAsArgs(Class<? extends EObject> superType) {
 		return toArgStream(getAllConcreteClassesBySuper(superType));
 	}
 
-	private static <T, Cls extends Class<? extends T>, C extends Collection<Cls>> Stream<Arguments> toArgStream(C col) {
+	private static <T, C extends Collection<Class<? extends T>>> Stream<Arguments> toArgStream(C col) {
 		return col.stream().map((c) -> Arguments.of(c, generateDisplayNameForInit(c)));
 	}
 
