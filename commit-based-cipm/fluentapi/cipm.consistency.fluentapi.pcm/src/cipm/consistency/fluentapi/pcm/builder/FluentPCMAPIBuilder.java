e8d26e6f431aba8680bb86319224238a059d20ba
AlpTorac <a.torac@hotmail.com>
2026-05-04 18:19:57 +0200

    Refactor fluent api builders to spare code duplication
    Generation methods are left as is. Even though they could be unified, model generation in general would need them to stay separated. Unifying them would just complicate the generation process

--- commit-based-cipm/fluentapi/cipm.consistency.fluentapi.pcm/src/cipm/consistency/fluentapi/pcm/builder/FluentPCMAPIBuilder.java
+++ commit-based-cipm/fluentapi/cipm.consistency.fluentapi.pcm/src/cipm/consistency/fluentapi/pcm/builder/FluentPCMAPIBuilder.java
@@ -1,23 +1,16 @@
 package cipm.consistency.fluentapi.pcm.builder;
 
-import java.io.IOException;
-import java.nio.file.Path;
 import java.util.ArrayList;
 
 import org.eclipse.emf.codegen.ecore.genmodel.GenModel;
 import org.eclipse.emf.codegen.ecore.genmodel.GenModelFactory;
-import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EPackage;
 import org.eclipse.emf.ecore.resource.Resource;
-import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
 
 import cipm.consistency.fluentapi.builder.FluentAPIAbstractBuilder;
 import cipm.consistency.fluentapi.gen.FluentAPIGenerationContext;
 import cipm.consistency.fluentapi.gen.FluentAPIGenerator;
-import cipm.consistency.fluentapi.gen.ModelConstants;
 import cipm.consistency.fluentapi.metamodel.FluentAPITargetMetamodelFeatureFilter;
 import cipm.consistency.fluentapi.metamodel.FluentAPITargetMetamodelPackageProvider;
 import cipm.consistency.fluentapi.pcm.metamodel.FluentAPIPcmMetamodelFeatureFilter;
@@ -30,48 +23,21 @@ public class FluentPCMAPIBuilder extends FluentAPIAbstractBuilder {
 	private static final FluentAPITargetMetamodelPackageProvider provider = new FluentAPIPcmMetamodelPackageProvider();
 	private static final FluentAPITargetMetamodelFeatureFilter filter = new FluentAPIPcmMetamodelFeatureFilter();
 
-	@Test
-	public void generateModelFiles() {
-		cleanPreviousModelFiles();
-
-		var context = new FluentAPIGenerationContext();
-		context.setTargetMetamodelPackageProvider(getTargetMetamodelPackageProvider());
-		context.setTargetMetamodelFeatureFilter(getTargetMetamodelFeatureFilter());
-		context.setBasePackageName(ModelConstants.BASE_PACKAGE_NAME.getFor(provider.getTargetMetamodelName()));
-
-		var modelResSet = new ResourceSetImpl();
-		var ecoreRes = modelResSet.createResource(URI.createFileURI(getEcoreModelFilePath().toString()));
-		var genModelRes = modelResSet.createResource(URI.createFileURI(getGenModelFilePath().toString()));
-
-		generateEcoreModel(ecoreRes, context);
-		generateGenModel(genModelRes, ecoreRes, context);
-
-		try {
-			ecoreRes.save(null);
-			genModelRes.save(null);
-		} catch (IOException e) {
-			e.printStackTrace();
-			Assertions.fail(e);
-		}
-	}
-
-	private GenModel generateGenModel(Resource genModelRes, Resource ecoreRes, FluentAPIGenerationContext context) {
-		var pluginName = "cipm.consistency.fluentapi.pcm";
-		var relativeModelDirPath = Path.of(pluginName, "src-gen");
-
+	@Override
+	protected GenModel generateGenModel(Resource genModelRes, Resource ecoreRes, FluentAPIGenerationContext context) {
 		var genModel = GenModelFactory.eINSTANCE.createGenModel();
 		genModelRes.getContents().add(genModel);
 
-		genModel.setModelDirectory("/" + relativeModelDirPath.toString());
+		genModel.setModelDirectory("/" + getModelDirectoryPath().toString());
 		genModel.setOperationReflection(true);
 		genModel.setImportOrganizing(true);
 		genModel.setComplianceLevel(getJDKVersion());
 		genModel.setModelName(getModelName());
-		genModel.setModelPluginID(pluginName);
+		genModel.setModelPluginID(getModelPluginID());
 		genModel.getForeignModel().add(ecoreRes.getURI().lastSegment());
 
-		var javaGenModel = provider.getTargetMetamodelGenModels().get(0);
-		genModel.getUsedGenPackages().addAll(javaGenModel.getGenPackages());
+		var targetMetamodelGenModel = provider.getTargetMetamodelGenModels().get(0);
+		genModel.getUsedGenPackages().addAll(targetMetamodelGenModel.getGenPackages());
 
 		var initEPacs = new ArrayList<EPackage>();
 		var toGen = (EPackage) ecoreRes.getContents().get(0);
@@ -89,7 +55,8 @@ public class FluentPCMAPIBuilder extends FluentAPIAbstractBuilder {
 		return genModel;
 	}
 
-	private void generateEcoreModel(Resource ecoreRes, FluentAPIGenerationContext context) {
+	@Override
+	protected void generateEcoreModel(Resource ecoreRes, FluentAPIGenerationContext context) {
 		new FluentAPIGenerator().generateRootAPIPackages(context);
 		new FluentAPIGenerationBigNumberParameterPostProcessor(context.getAllInitEClss()).apply();
 
--- commit-based-cipm/fluentapi/cipm.consistency.fluentapi/src/cipm/consistency/fluentapi/builder/FluentAPIAbstractBuilder.java
+++ commit-based-cipm/fluentapi/cipm.consistency.fluentapi/src/cipm/consistency/fluentapi/builder/FluentAPIAbstractBuilder.java
@@ -1,23 +1,87 @@
 package cipm.consistency.fluentapi.builder;
 
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.Path;
 
 import org.eclipse.emf.codegen.ecore.genmodel.GenJDKLevel;
+import org.eclipse.emf.codegen.ecore.genmodel.GenModel;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.osgi.framework.FrameworkUtil;
 
+import cipm.consistency.fluentapi.gen.FluentAPIGenerationContext;
+import cipm.consistency.fluentapi.gen.ModelConstants;
 import cipm.consistency.fluentapi.metamodel.FluentAPITargetMetamodelFeatureFilter;
 import cipm.consistency.fluentapi.metamodel.FluentAPITargetMetamodelPackageProvider;
 
 public abstract class FluentAPIAbstractBuilder {
+	/**
+	 * The suffix all generated fluent api model files (i.e. the ecore and genmodel
+	 * files) should have
+	 */
 	private static final String commonModelSuffix = "fluentapi";
+	/**
+	 * The name of the directory, where the fluent api model files will be generated
+	 */
 	private static final String commonModelDirName = "metamodel";
+	/**
+	 * The name of the ecore file associated with the fluent api
+	 */
 	private static final String commonEcoreModelFileName = commonModelSuffix + ".ecore";
+	/**
+	 * The name of the genmodel file associated with the fluent api
+	 */
 	private static final String commonGenModelFileName = commonModelSuffix + ".genmodel";
+	/**
+	 * The name of the directory, where the fluent api itself will be generated
+	 * (i.e. the classes that can be used to construct models of the targeted
+	 * metamodel)
+	 */
+	private static final String modelGenerationTargetDirName = "src-gen";
 
-	protected String getCommonModelSuffix() {
-		return commonModelSuffix;
+	/**
+	 * Generates the fluent API model files (i.e. the ecore and genmodel files).
+	 * <p>
+	 * Can be overridden in concrete implementors to change how the model files are
+	 * generated. If overridden, the overriding method should have the {@code @Test}
+	 * annotation for JUnit to detect it as a test case. Otherwise, the overriding
+	 * method will not be recognised as a test method and no model files will be
+	 * generated.
+	 */
+	@Test
+	public void generateModelFiles() {
+		cleanPreviousModelFiles();
+
+		var context = new FluentAPIGenerationContext();
+		context.setTargetMetamodelPackageProvider(getTargetMetamodelPackageProvider());
+		context.setTargetMetamodelFeatureFilter(getTargetMetamodelFeatureFilter());
+		context.setBasePackageName(
+				ModelConstants.BASE_PACKAGE_NAME.getFor(getTargetMetamodelPackageProvider().getTargetMetamodelName()));
+
+		var modelResSet = new ResourceSetImpl();
+		var ecoreRes = modelResSet.createResource(URI.createFileURI(getEcoreModelFilePath().toString()));
+		var genModelRes = modelResSet.createResource(URI.createFileURI(getGenModelFilePath().toString()));
+
+		generateEcoreModel(ecoreRes, context);
+		generateGenModel(genModelRes, ecoreRes, context);
+
+		try {
+			ecoreRes.save(null);
+			genModelRes.save(null);
+		} catch (IOException e) {
+			e.printStackTrace();
+			Assertions.fail(e);
+		}
 	}
 
+	/**
+	 * @return The value of the "model name" property of the genmodel of the fluent
+	 *         api.
+	 */
 	protected String getModelName() {
 		return getTargetMetamodelPackageProvider().getTargetMetamodelName() + "-" + commonModelSuffix;
 	}
@@ -55,7 +119,8 @@ public abstract class FluentAPIAbstractBuilder {
 	}
 
 	/**
-	 * @return The absolute path to the ecore model file
+	 * @return The absolute path to the ecore model file associated with the fluent
+	 *         api
 	 */
 	protected Path getEcoreModelFilePath() {
 		return new File(getModelDirName()).getAbsoluteFile().toPath().resolve(getEcoreModelFileName());
@@ -76,6 +141,10 @@ public abstract class FluentAPIAbstractBuilder {
 		}
 	}
 
+	/**
+	 * @return The value of the "compliance level" property of the genmodel of
+	 *         fluent api.
+	 */
 	protected static GenJDKLevel getJDKVersion() {
 		var runtimeVer = Runtime.version().version().get(0);
 		GenJDKLevel lvl = null;
@@ -88,7 +157,43 @@ public abstract class FluentAPIAbstractBuilder {
 		return lvl;
 	}
 
+	/**
+	 * @return The value of the "model plugin ID" property of the genmodel of fluent
+	 *         api.
+	 */
+	protected String getModelPluginID() {
+		return ModelConstants.BASE_PACKAGE_NAME.getFor(getTargetMetamodelPackageProvider().getTargetMetamodelName());
+	}
+
+	/**
+	 * Returns The path, at which the fluent API will be generated.
+	 * <p>
+	 * Can be overridden in concrete implementors.
+	 * 
+	 * @return The value of the "model directory" property of the genmodel of fluent
+	 *         api as Path.
+	 */
+	protected Path getModelDirectoryPath() {
+		return Path.of(getCurrentPluginName(), modelGenerationTargetDirName);
+	}
+
+	/**
+	 * Meant to be used by {@link #getModelDirectoryPath()}.
+	 * 
+	 * @return The name of the current plug-in.
+	 */
+	private String getCurrentPluginName() {
+		var bundle = FrameworkUtil.getBundle(getClass());
+		var name = bundle.getSymbolicName();
+		return name;
+	}
+
 	protected abstract FluentAPITargetMetamodelPackageProvider getTargetMetamodelPackageProvider();
 
 	protected abstract FluentAPITargetMetamodelFeatureFilter getTargetMetamodelFeatureFilter();
+
+	protected abstract GenModel generateGenModel(Resource genModelRes, Resource ecoreRes,
+			FluentAPIGenerationContext context);
+
+	protected abstract void generateEcoreModel(Resource ecoreRes, FluentAPIGenerationContext context);
 }
