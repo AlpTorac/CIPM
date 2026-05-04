28cc9198fc3cb3f1e66ac5050bf418eed66aefd8
AlpTorac <a.torac@hotmail.com>
2026-05-04 18:22:58 +0200

    Remove unused import

--- commit-based-cipm/fluentapi/cipm.consistency.fluentapi/src/cipm/consistency/fluentapi/test/metamodel/AbstractFluentAPIInitialisationGenerationTest.java
+++ commit-based-cipm/fluentapi/cipm.consistency.fluentapi/src/cipm/consistency/fluentapi/test/metamodel/AbstractFluentAPIInitialisationGenerationTest.java
@@ -10,7 +10,6 @@ import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EClassifier;
 import org.eclipse.emf.ecore.EOperation;
 import org.eclipse.emf.ecore.EStructuralFeature;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInfo;
