c30e5dc01a5ccb9e7f3f590ede260877e10093d3
AlpTorac <a.torac@hotmail.com>
2026-05-21 18:15:50 +0200

    Remove unused method, add documentary

--- commit-based-cipm/fluentapi/cipm.consistency.fluentapi/src/cipm/consistency/fluentapi/metamodel/FluentAPITargetMetamodelFeatureFilter.java
+++ commit-based-cipm/fluentapi/cipm.consistency.fluentapi/src/cipm/consistency/fluentapi/metamodel/FluentAPITargetMetamodelFeatureFilter.java
@@ -7,33 +7,59 @@ import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EStructuralFeature;
 
 /**
- * 
- * TODO Add proper commentary
- * 
+ * An abstract class meant to be implemented by classes, which filter the
+ * features of (EMF-based) metamodels. If constraints and invariants are
+ * considered within the metamodel, they have to be explicitly checked.
+ * <p>
+ * <p>
  * Note: Do not use the original metamodel packages while type-checking or
  * filtering, because {@link FluentAPITargetMetamodelPackageProvider} does not
  * use the original metamodel packages. Attempting to use
  * {@code originalECls.isSuperTypeOf(givenECls)} or vice versa will always
  * result in false, due to the original EClass and the given EClass being in
- * different models entirely. Instead, use their EAttributes for type-checking
- * (such as their name); excluding {@code eCls.getInstanceClass()} and related
- * methods, since they are not guaranteed to exist in parsed models.
+ * different Resources entirely. Instead, use their EAttributes for
+ * type-checking (such as their name); excluding {@code eCls.getInstanceClass()}
+ * and related methods, since they are not guaranteed to exist in parsed models.
  * 
  * @author Alp Torac Genc
  */
 public abstract class FluentAPITargetMetamodelFeatureFilter {
+	/**
+	 * @param holderOfFeat The EClass that contains the feat. Must be specified,
+	 *                     since feat may also be declared in a super EClass.
+	 * @param feat         A feature of the metamodel
+	 * @return Whether the given feature should be considered in fluent api for the
+	 *         given EClass (holderOfFeat)
+	 */
 	public abstract boolean isFeatureEligible(EClass holderOfFeat, EStructuralFeature feat);
 
+	/**
+	 * @param eObjEClass An EClass within the metamodel.
+	 * @return All features of the given EClass that are considered in fluent api
+	 *         (according to {@link #isFeatureEligible(EClass, EStructuralFeature)})
+	 */
 	public List<EStructuralFeature> getModifiableFeatures(EClass eObjEClass) {
 		return eObjEClass.getEAllStructuralFeatures().stream().filter((f) -> this.isFeatureEligible(eObjEClass, f))
 				.collect(Collectors.toList());
 	}
 
+	/**
+	 * @param eObjEClass An EClass within the metamodel.
+	 * @return The number of features of the given EClass that are considered in
+	 *         fluent api (according to
+	 *         {@link #isFeatureEligible(EClass, EStructuralFeature)})
+	 */
 	public long getModifiableFeatureCount(EClass eObjEClass) {
 		return eObjEClass.getEAllStructuralFeatures().stream().filter((f) -> this.isFeatureEligible(eObjEClass, f))
 				.count();
 	}
 
+	/**
+	 * @param eObjEClass An EClass within the metamodel.
+	 * @return Whether the given EClass has any features that are considered in
+	 *         fluent api (according to
+	 *         {@link #isFeatureEligible(EClass, EStructuralFeature)})
+	 */
 	public boolean hasModifiableFeatures(EClass eObjEClass) {
 		return eObjEClass.getEAllStructuralFeatures().stream().anyMatch((f) -> this.isFeatureEligible(eObjEClass, f));
 	}
@@ -41,18 +67,11 @@ public abstract class FluentAPITargetMetamodelFeatureFilter {
 	/**
 	 * Not declared as static, because the underlying metamodel could introduce
 	 * constraints regarding this. This is a default implementation.
+	 * 
+	 * @param feat A feature within the metamodel
+	 * @return Whether the given feat can be modified
 	 */
 	public boolean isFeatureChangeable(EStructuralFeature feat) {
 		return feat.isChangeable() && !feat.isDerived();
-	}
-
-	/**
-	 * Not declared as static, because the underlying metamodel could introduce
-	 * constraints regarding this. This is a default implementation.
-	 */
-	public boolean isContainmentReferenceFor(EClass elemToInit, EStructuralFeature potentialContainmentFeat) {
-		var refType = potentialContainmentFeat.getEType();
-		var refTypeCls = refType.getInstanceClass();
-		return refType instanceof EClass && refTypeCls.isAssignableFrom(elemToInit.getInstanceClass());
 	}
 }
