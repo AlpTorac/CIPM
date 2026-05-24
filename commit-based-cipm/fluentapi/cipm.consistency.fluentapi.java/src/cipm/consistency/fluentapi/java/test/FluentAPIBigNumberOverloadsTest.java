3a69805d43b01162e1831e1d1610e8525e008a23
AlpTorac <a.torac@hotmail.com>
2026-05-24 10:43:14 +0200

    add documentation

--- commit-based-cipm/fluentapi/cipm.consistency.fluentapi.java/src/cipm/consistency/fluentapi/java/test/FluentAPIBigNumberOverloadsTest.java
+++ commit-based-cipm/fluentapi/cipm.consistency.fluentapi.java/src/cipm/consistency/fluentapi/java/test/FluentAPIBigNumberOverloadsTest.java
@@ -9,6 +9,13 @@ import org.junit.jupiter.api.Test;
 import cipm.consistency.fluentapi.java.api.ApiFactory;
 import cipm.consistency.fluentapi.test.AbstractFluentAPITest;
 
+/**
+ * A test class for checking whether fluent api model has overloading methods
+ * for operations that consider {@link BigInteger} parameters and that they work
+ * as intended.
+ * 
+ * @author Alp Torac Genc
+ */
 public class FluentAPIBigNumberOverloadsTest extends AbstractFluentAPITest {
 	/**
 	 * Ensures that overloading methods for BigInteger are generated
@@ -20,20 +27,27 @@ public class FluentAPIBigNumberOverloadsTest extends AbstractFluentAPITest {
 		final var lit = new DecimalIntegerLiteral[3];
 
 		BigInteger bigIntVal = BigInteger.valueOf(1);
+
+		// Ensure that the original method for BigInteger is generated
 		Assertions.assertDoesNotThrow(
 				() -> lit[0] = api.newDecimalIntegerLiteral().withDecimalValue(bigIntVal).createNow());
+		// Ensure that the original method for BigInteger works as intended
 		Assertions.assertEquals(bigIntVal, lit[0].getDecimalValue());
 
 		int intVal = bigIntVal.intValue();
 		Assertions.assertEquals(1, intVal);
+		// Ensure that the overloading method is generated (with int parameter)
 		Assertions
 				.assertDoesNotThrow(() -> lit[1] = api.newDecimalIntegerLiteral().withDecimalValue(intVal).createNow());
+		// Ensure that the overloading method works as intended
 		Assertions.assertEquals(intVal, lit[1].getDecimalValue().intValue());
 
 		long longVal = bigIntVal.longValue();
 		Assertions.assertEquals(1, longVal);
+		// Ensure that the overloading method is generated (with long parameter)
 		Assertions.assertDoesNotThrow(
 				() -> lit[2] = api.newDecimalIntegerLiteral().withDecimalValue(longVal).createNow());
+		// Ensure that the overloading method works as intended
 		Assertions.assertEquals(longVal, lit[2].getDecimalValue().longValue());
 	}
 
@@ -47,12 +61,16 @@ public class FluentAPIBigNumberOverloadsTest extends AbstractFluentAPITest {
 		final var obj = new DecimalIntegerLiteral[2];
 
 		int intVal = 1;
+		// Ensure that the overloading method is generated (with int parameter)
 		Assertions.assertDoesNotThrow(() -> obj[0] = api.newDecimalIntegerLiteral(intVal));
+		// Ensure that the overloading method works as intended
 		Assertions.assertInstanceOf(DecimalIntegerLiteral.class, obj[0]);
 		Assertions.assertEquals(intVal, obj[0].getDecimalValue().intValue());
 
 		long longVal = 1;
+		// Ensure that the overloading method is generated (with long parameter)
 		Assertions.assertDoesNotThrow(() -> obj[1] = api.newDecimalIntegerLiteral(longVal));
+		// Ensure that the overloading method works as intended
 		Assertions.assertInstanceOf(DecimalIntegerLiteral.class, obj[1]);
 		Assertions.assertEquals(longVal, obj[1].getDecimalValue().longValue());
 	}
