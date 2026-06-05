30f130fd04fe02c172902991d5129eece97f0186
AlpTorac <a.torac@hotmail.com>
2026-06-05 16:39:16 +0200

    Add todo

--- commit-based-cipm/tests/cipm.consistency.vsum.test/src/cipm/consistency/vsum/test/pcm/preprocessing/test/EObjectWrapperTest.java
+++ commit-based-cipm/tests/cipm.consistency.vsum.test/src/cipm/consistency/vsum/test/pcm/preprocessing/test/EObjectWrapperTest.java
@@ -3,6 +3,7 @@ package cipm.consistency.vsum.test.pcm.preprocessing.test;
 import java.util.List;
 
 import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -27,7 +28,7 @@ public class EObjectWrapperTest {
 	public static void startUpBeforeAll() {
 //		Resource.Factory.Registry.INSTANCE.getContentTypeToFactoryMap().put("*", new RepositoryResourceFactoryImpl());
 	}
-	
+
 	@Test
 	public void a() {
 		var wrapper = new EObjectDependencyTracker();
@@ -62,9 +63,34 @@ public class EObjectWrapperTest {
 
 	@Test
 	public void b() {
-		
+
 		// TODO Check how change IDs actually are in PCM changes and adjust if needed
-		
+
+		/*
+		 * TODO
+		 * 
+		 * If computed PCM changes do not utilise the UUIDs, mention it in the next
+		 * meeting. Currently, the PCM changes computed here use the structural IDs,
+		 * even though the UUIDs are present.
+		 * 
+		 * Tested whether UUIDs can actually be used in PCM changes and get resolved,
+		 * turns out that they can. The following test code runs without failures:
+		 * 
+		 * var expectedIDAfterSetting = (String) replaceIDChange.getNewValue();
+		 * 
+		 * var copier = new EcoreUtil.Copier();
+		 * 
+		 * var change = (RemoveRootEObject<?>) copier.copy(removeRootChange);
+		 * 
+		 * copier.copyReferences();
+		 * 
+		 * change.setOldValueID(expectedIDAfterSetting);
+		 * 
+		 * ChangePreprocessingTestAssertions.assertChangeSequencesHaveSameEffect(
+		 * changes, List.of(createChange, insertRootChange, replaceIDChange, change,
+		 * deleteChange));
+		 */
+
 		var wrapper = new EObjectDependencyTracker();
 		wrapper.setIDAdjustingStrategy(new UUIDAdjustingStrategy());
 		final var repo = RepositoryFactory.eINSTANCE.createRepository();
@@ -97,7 +123,7 @@ public class EObjectWrapperTest {
 				wrapper.getContainingFeatInChange(insertRootChange));
 
 		var expectedIDAfterInsert = expectedRes.getURI().appendFragment("/0").toString();
-		
+
 		Assertions.assertEquals(expectedIDAfterInsert, wrapper.getIDInChange(replaceIDChange));
 		Assertions.assertEquals(FeaturePackage.Literals.FEATURE_ECHANGE__AFFECTED_EOBJECT,
 				wrapper.getContainingFeatInChange(replaceIDChange));
@@ -105,7 +131,7 @@ public class EObjectWrapperTest {
 		// FIXME Enable or clean once fixed
 //		var expectedIDAfterSetting = (String) replaceIDChange.getNewValue();
 //		Assertions.assertEquals(expectedIDAfterSetting, wrapper.getIDInChange(removeRootChange));
-		
+
 		Assertions.assertEquals(expectedIDAfterInsert, wrapper.getIDInChange(removeRootChange));
 		Assertions.assertEquals(EobjectPackage.Literals.EOBJECT_SUBTRACTED_ECHANGE__OLD_VALUE,
 				wrapper.getContainingFeatInChange(removeRootChange));
