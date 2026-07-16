d509d0d5d5c84bcb11d38caed2e7f4d7b4ed6a34
AlpTorac <a.torac@hotmail.com>
2026-07-16 15:28:38 +0200

    Fix move change detection

--- commit-based-cipm/tests/cipm.consistency.vsum.test/src/cipm/consistency/vsum/test/changedetection/MoveSingleElementMatcher.java
+++ commit-based-cipm/tests/cipm.consistency.vsum.test/src/cipm/consistency/vsum/test/changedetection/MoveSingleElementMatcher.java
@@ -3,12 +3,36 @@ package cipm.consistency.vsum.test.changedetection;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.util.EcoreUtil;
 
+import cipm.consistency.cpr.pcmjava.preprocessing.ChangeUtil;
 import tools.vitruv.change.atomic.EChange;
 import tools.vitruv.change.atomic.feature.reference.AdditiveReferenceEChange;
 import tools.vitruv.change.atomic.feature.reference.SubtractiveReferenceEChange;
 
+/**
+ * 
+ * 4 possibilities in total:
+ * <ul>
+ * <li>ReplaceSingleValuedEReference E newVal -> ReplaceSingleValuedEReference
+ * oldVal E (E.ID is a cache ID)
+ * <li>ReplaceSingleValuedEReference E newVal -> InsertEReference E (E.ID is a
+ * cache ID)
+ * <li>RemoveEReference E ->ReplaceSingleValuedEReference oldVal E (E.ID is a
+ * cache ID)
+ * <li>RemoveEReference E -> InsertEReference E (E.ID is a cache ID)
+ * </ul>
+ * 
+ * => If no EObjects are present within EChanges (i.e. they are unresolved), the
+ * only thing to check for is: (1) is a SubtractiveReferenceEChange followed by
+ * a AdditiveReferenceEChange, (2) is AdditiveReferenceEChange.newValueID a
+ * cache ID.
+ * 
+ * => Assume that E will be stored in the same feature, so that its purpose
+ * remains the same and therefore moving it is less prone to side-effects
+ * 
+ * @author Alp Torac Genc
+ */
 public class MoveSingleElementMatcher implements ICompositeChangeMatcher {
 	private List<EChange> changeSeq;
 
@@ -39,10 +63,11 @@ public class MoveSingleElementMatcher implements ICompositeChangeMatcher {
 	private boolean matchAdditiveChange(EChange ar) {
 		if (ar instanceof AdditiveReferenceEChange) {
 			var castedAR = (AdditiveReferenceEChange) ar;
-			var typeAR = (EClass) castedAR.getAffectedFeature().getEType();
-			var typeSR = (EClass) sr.getAffectedFeature().getEType();
-
-			if (typeAR.isSuperTypeOf(typeSR) || typeSR.isSuperTypeOf(typeAR)) {
+			if (
+			// Make sure that the affected features are the same
+			EcoreUtil.equals(sr.getAffectedFeature(), castedAR.getAffectedFeature()) &&
+			// Check whether the newValueID is a cache ID
+					castedAR.getNewValueID() != null && ChangeUtil.isCacheURI(castedAR.getNewValueID())) {
 				this.ar = castedAR;
 				return true;
 			}
